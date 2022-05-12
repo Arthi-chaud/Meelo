40be023d5ff9ec40bda733875f5ff5a0eba08d45
Arthi-chaud <arthur.jamet@gmail.com>
2022-05-12 10:22:53 +0200

    Library: now holds files instead of tracks

--- src/library/models/library.model.ts
+++ src/library/models/library.model.ts
@@ -1,4 +1,5 @@
 import { AutoIncrement, BeforeCreate, BeforeUpdate, BelongsTo, Column, Default, ForeignKey, HasMany, HasOne, Is, Model, Table, Unique } from 'sequelize-typescript';
+import { File } from 'src/file/models/file.model';
 import { Track } from 'src/track/models/track.model';
 
 @Table({ tableName: 'libraries' })
@@ -10,6 +11,6 @@ export class Library extends Model {
 	@Column
 	path: string;
 
-	@HasMany(() => Track)
-	tracks: Track[];
+	@HasMany(() => File)
+	files: File[];
 }
