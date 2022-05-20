e7d3d8375a982d095f2fb53e7bd024d3d2b968e9
Arthi-chaud <arthur.jamet@gmail.com>
2022-05-20 21:44:50 +0200

    Settings Module: Add Hot Reload Feature

---
+++ src/settings/settings.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, HttpException, HttpStatus, Redirect, Req, Res } from '@nestjs/common';
+import { SettingsService } from './settings.service';
+
+@Controller('settings')
+export class SettingsController {
+	constructor(private settingsService: SettingsService) {}
+	
+	@Get()
+	getSettings(): object {
+		return this.settingsService;
+	}
+
+	@Get('reload')
+	@Redirect('/settings', HttpStatus.FOUND)
+	reload(@Res() res) {
+		try {
+			this.settingsService.loadFromFile();
+		} catch (exception) {
+			throw new HttpException({
+				error: exception.message
+			}, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+}
--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -3,10 +3,19 @@ import * as fs from 'fs';
 
 @Injectable()
 export class SettingsService {
-	readonly dataFolder: string;
-	readonly trackRegexes: string[];
+	protected dataFolder: string;
+	protected trackRegexes: string[];
+	/// TODO shoud be set from constructor's parameter
+	private readonly configPath: string = '/meelo/settings.json';
+
 	constructor() {
-		let settings = JSON.parse(fs.readFileSync('/meelo/settings.json','utf8'));
+		this.loadFromFile();
+	}
+	/**
+	 * Loading Settings configuration from a JSON file
+	 */
+	loadFromFile() {
+		let settings = JSON.parse(fs.readFileSync(this.configPath,'utf8'));
 		this.dataFolder = settings.dataFolder;
 		this.trackRegexes = settings.trackRegex;
 	}
