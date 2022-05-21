2b02f1a7584f027a24c57cd8d8ee49e71716858a
Arthi-chaud <arthur.jamet@gmail.com>
2022-05-21 13:10:12 +0200

    File Manager Service: Add function to compute a file's MD5 Checksum

--- src/file-manager/file-manager.service.ts
+++ src/file-manager/file-manager.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { SettingsService } from 'src/settings/settings.service';
+import md5File from 'md5-file';
 
 @Injectable()
 export class FileManagerService {
 	constructor(private settingsService: SettingsService) {}
+
+	/**
+	 * Compute the MD5 checksum of a file
+	 * @param filePath The Path to a file, whose MD5 checksum will be computed
+	 * @returns the MD5 Checksum as a string
+	 */
+	getMd5Checksum(filePath: string) {
+		return md5File.sync(filePath);
+	}
 }
