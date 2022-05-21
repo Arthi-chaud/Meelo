123361f6f0a726598ff889198427cbe9c5aa98ef
Arthi-chaud <arthur.jamet@gmail.com>
2022-05-21 17:16:55 +0200

    File Module: Import File Manger Module

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { File } from './models/file.model';
+import { FileService } from './file.service';
+import { FileManagerModule } from 'src/file-manager/file-manager.module';
 
 @Module({
 	imports: [
 		SequelizeModule.forFeature([File]),
-	]
+		FileManagerModule
+	],
+	providers: [FileService]
 })
 export class FileModule {}
