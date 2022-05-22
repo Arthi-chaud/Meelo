6f5aee7e06680e3abdec1dacf20a413f2cc29ca8
Arthi-chaud <arthur.jamet@gmail.com>
2022-05-22 11:57:22 +0200

    File Manager: Fix Registration flow

--- src/file-manager/file-manager.service.ts
+++ src/file-manager/file-manager.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { SettingsService } from 'src/settings/settings.service';
 import md5File from 'md5-file';
 import * as fs from 'fs';
@@ -25,7 +25,7 @@ export class FileManagerService {
 		const baseFolder = this.settingsService.getDataFolder();
 		const libraryPath = `${baseFolder}/${libraryBaseDirectory}`;
 		return this.getCandidateInFolder(libraryPath).map(
-			(candidateFullPath) => candidateFullPath.substring(0, libraryPath.length)
+			(candidateFullPath) => candidateFullPath.substring(libraryPath.length + 1)
 		);
 	}
 
@@ -35,22 +35,31 @@ export class FileManagerService {
 	 */
 	private getCandidateInFolder(folderPath: string): string[] {
 		let directoryContent = fs.readdirSync(folderPath, { withFileTypes: true });
-		let candidates = [];
+		let candidates: string[] = [];
 
 		directoryContent.forEach(
 			(dirEntry) => {
 				const entryFullPath = `${folderPath}/${dirEntry.name}`;
-				if (dirEntry.isDirectory)
-					candidates.concat(this.getCandidateInFolder(entryFullPath));
-				else if (dirEntry.isFile) {
-					let matchingRegexes = this.settingsService.getTrackRegexes().filter(
-						(regex) => entryFullPath.match(regex) != null
-					);
-					if (matchingRegexes.length > 0)
+				if (dirEntry.isDirectory())
+				candidates = candidates.concat(this.getCandidateInFolder(entryFullPath));
+				else if (dirEntry.isFile()) {
+					if (this.fileIsCandidate(entryFullPath))
 						candidates.push(entryFullPath);
 				}
 			}
 		);
 		return candidates;
+	}
+
+	/**
+	 * Checks if a file matches one of the RegExp from the settings
+	 * @param filepath The Full path of a file
+	 * @returns true if any of the RegExp matches
+	 */
+	private fileIsCandidate(filepath: string): boolean {
+		let matchingRegexes = this.settingsService.getTrackRegexes().filter(
+			(regex) => filepath.match(regex) != null
+		);
+		return matchingRegexes.length > 0;
 	}
 }
