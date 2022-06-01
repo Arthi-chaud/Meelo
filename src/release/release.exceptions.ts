b0b3071daf5c87aa03438bcdded5734ee28f6dc8
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-01 21:23:57 +0200

    Release: Add related exceptions

--- src/release/release.exceptions.ts
+++ src/release/release.exceptions.ts
@@ -2,19 +2,19 @@ import { AlreadyExistsException, NotFoundException } from "src/exceptions/meelo-
 import { Slug } from "src/slug/slug";
 
 export class ReleaseNotFoundException extends NotFoundException {
-	constructor(releaseName: string, albumSlug: Slug, artistSlug: Slug) {
-		super(`Release '${releaseName}' of ${albumSlug.toString()} by ${artistSlug.toString()} not found`);
+	constructor(releaseName: string, albumSlug: Slug, artistSlug?: Slug) {
+		super(`Release '${releaseName}' of ${albumSlug.toString()} ${artistSlug ? `by ${artistSlug.toString()}`: ''} not found`);
 	}
 }
 
 export class ReleaseAlreadyExists extends AlreadyExistsException {
-	constructor(releaseTitle: string, artistSlug: Slug) {
-		super(`Release ${releaseTitle} by ${artistSlug.toString()} already exists`);
+	constructor(releaseTitle: string, artistSlug?: Slug) {
+		super(`Release ${releaseTitle}${artistSlug ? `by ${artistSlug.toString()}`: ''} already exists`);
 	}
 }
 
 export class MasterReleaseNotFoundException extends NotFoundException {
-	constructor(albumSlug: Slug, artistSlug: Slug) {
-		super(`Master Release of ${albumSlug.toString()} by ${artistSlug.toString()} not found`);
+	constructor(albumSlug: Slug, artistSlug?: Slug) {
+		super(`Master Release of ${albumSlug.toString()}${artistSlug ? `by ${artistSlug.toString()}`: ''} not found`);
 	}
 }
