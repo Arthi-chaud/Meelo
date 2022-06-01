643d51fc153b68f2ecdf811a95d112a2770c32eb
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-01 12:05:12 +0200

    Album: Add related exceptions

--- src/album/album.exceptions.ts
+++ src/album/album.exceptions.ts
@@ -1,8 +1,14 @@
-import { NotFoundException } from "src/exceptions/meelo-exception";
+import { AlreadyExistsException, NotFoundException } from "src/exceptions/meelo-exception";
 import { Slug } from "src/slug/slug";
 
 export class AlbumNotFoundException extends NotFoundException {
 	constructor(albumSlug: Slug, artistSlug: Slug) {
 		super(`${albumSlug.toString()} by ${artistSlug.toString()}: No such album`);
+	}
+}
+
+export class AlbumAlreadyExistsException extends AlreadyExistsException {
+	constructor(albumSlug: Slug, artistSlug: Slug) {
+		super(`${albumSlug.toString()} by ${artistSlug.toString()} already exists`);
 	}
 }
--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Track } from './models/track.model';
+import { TrackService } from './track.service';
 
 @Module({
 	imports: [
 		SequelizeModule.forFeature([
 			Track
 		])
-	]
+	],
+	providers: [TrackService]
 })
 export class TrackModule {}
