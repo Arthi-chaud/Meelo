cbc5ec704c3c1922cbacf0ab6531d41c32d6678e
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-01 21:43:10 +0200

    Album Service: Method to get, create, getorcreate album

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Album } from './models/album.model';
 import { AlbumService } from './album.service';
+import { ArtistModule } from 'src/artist/artist.module';
 
 @Module({
 	imports: [
-		SequelizeModule.forFeature([Album])
+		SequelizeModule.forFeature([Album]),
+		ArtistModule
 	],
+	exports: [AlbumService],
 	providers: [AlbumService]
 })
 export class AlbumModule {}
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { ArtistService } from 'src/artist/artist.service';
 import { Artist } from 'src/artist/models/artist.model';
 import { Release } from 'src/release/models/release.model';
 import { Slug } from 'src/slug/slug';
-import { AlbumNotFoundException } from './album.exceptions';
+import { AlbumAlreadyExistsException, AlbumNotFoundException } from './album.exceptions';
 import { Album } from './models/album.model';
 
 @Injectable()
 export class AlbumService {
 	constructor(
 		@InjectModel(Album)
-		private albumModel: typeof Album 
+		private albumModel: typeof Album,
+		private artistServce: ArtistService
 	) {}
 
 	/**
@@ -18,11 +20,11 @@ export class AlbumService {
 	 * @param albumSlug the slug of the album to find
 	 * @param artistSlug the slug of the artist of the album
 	 */
-	async getAlbumBySlug(albumSlug: Slug, artistSlug: Slug): Promise<Album> {
+	async getAlbumBySlug(albumSlug: Slug, artistSlug?: Slug): Promise<Album> {
 		return await this.albumModel.findOne({
 			where: {
 				slug: albumSlug.toString(),
-				'$Artist.slug$': artistSlug.toString()
+				'$Artist.slug$': artistSlug?.toString()
 			},
 			rejectOnEmpty: true,
 			include: [
@@ -30,9 +32,55 @@ export class AlbumService {
 					model: Artist,
 					as: 'Artist',
 				},
+				Release
 			]
 		}).catch(() => {
 			throw new AlbumNotFoundException(albumSlug, artistSlug);
 		});
+	}
+
+	async saveAlbum(album: Album): Promise<Album> {
+		return await album.save();
+	}
+
+	getAlbumTypeFromName(albumName: string): AlbumType {
+		albumName = albumName.toLowerCase();
+		if (albumName.search(/.+(live).*/g) ||
+			albumName.includes(' tour')) {
+			return AlbumType.LiveRecording
+		}
+		if (albumName.endsWith('- single')) {
+			return AlbumType.Single
+		}
+		if (albumName.includes('best of')) {
+			return AlbumType.Compilation
+		}
+		return AlbumType.StudioRecording;
+	}
+
+	async createAlbum(albumName: string, artistName?: string, releaseDate?: Date): Promise<Album> {
+		let albumSlug: Slug = new Slug(albumName);
+		try {
+			let album: Album = Album.build({
+				name: albumName,
+				slug: albumSlug,
+				artist: artistName ? await this.artistServce.getOrCreateArtist(artistName) : null,
+				releaseDate: releaseDate,
+				releases: [],
+				type: this.getAlbumTypeFromName(albumName)
+			});
+			return await album.save();
+		} catch {
+			throw new AlbumAlreadyExistsException(albumSlug, new Slug(artistName ?? 'Unkown'));
+		}
+
+	}
+
+	async findOrCreate(albumName: string, artistName?: string): Promise<Album> {
+		try {
+			return await this.getAlbumBySlug(new Slug(albumName), artistName ? new Slug(artistName) : undefined);
+		} catch {
+			return await this.createAlbum(albumName, artistName);
+		}
 	}
 }
