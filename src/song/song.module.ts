280dbd362626dc03ff8da628ffc96b84ef905a7b
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-01 11:09:31 +0200

    Song: Module imports ArtistModule

--- src/song/song.module.ts
+++ src/song/song.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ArtistModule } from 'src/artist/artist.module';
 import { Song } from './models/song.model';
+import { SongService } from './song.service';
 
 @Module({
 	imports: [
 		SequelizeModule.forFeature([
 			Song,
-		])
-	]
+		]),
+		ArtistModule
+	],
+	providers: [SongService]
 })
 export class SongModule {}
