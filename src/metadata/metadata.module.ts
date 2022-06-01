3c2a0edb38c910526f1f55391390c3def51154c9
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-01 12:05:39 +0200

    Metadata: Module imports Track & song module

--- src/metadata/metadata.module.ts
+++ src/metadata/metadata.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FileManagerModule } from 'src/file-manager/file-manager.module';
 import { SettingsModule } from 'src/settings/settings.module';
+import { SongModule } from 'src/song/song.module';
+import { TrackModule } from 'src/track/track.module';
 import { MetadataService } from './metadata.service';
 
 @Module({
-  imports: [SettingsModule, FileManagerModule],
+  imports: [SettingsModule, FileManagerModule, TrackModule, SongModule],
   providers: [MetadataService],
   exports: [MetadataService]
 })
