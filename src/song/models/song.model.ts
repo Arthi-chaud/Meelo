5e9a3b53eb58fadd6cf358d0e1333e5301c8c4d9
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-01 12:06:22 +0200

    Song: Model has a slug

--- src/song/models/song.model.ts
+++ src/song/models/song.model.ts
@@ -1,12 +1,13 @@
 import { AutoIncrement, BeforeCreate, BeforeUpdate, BelongsTo, Column, Default, ForeignKey, HasMany, HasOne, Is, Model, Table, Unique } from 'sequelize-typescript';
 import { Artist } from 'src/artist/models/artist.model';
+import { SluggedModel } from 'src/slug/slugged-model';
 import { Track } from 'src/track/models/track.model';
 
 /**
  * A song is described by a title, an artist, and 'instanciated' by Tracks
  */
 @Table({ tableName: 'songs' })
-export class Song extends Model {
+export class Song extends SluggedModel {
 	/**
 	 * The artist of the song
 	 */
@@ -30,4 +31,8 @@ export class Song extends Model {
 	 */
 	@Column({ allowNull: false })
 	name: string;
+	
+	get slugSource(): string {
+		return this.name;
+	}
 }
