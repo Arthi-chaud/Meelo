5130720f9fd94b83903173333739475a65e9b467
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-07 20:16:58 +0200

    Artist module: add controller

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ArtistController } from './artist.controller';
 
 @Module({
 	imports: [
 		PrismaModule,
 	],
 	exports: [ArtistService],
-	providers: [ArtistService]
+	providers: [ArtistService],
+	controllers: [ArtistController]
 })
 export class ArtistModule {}
