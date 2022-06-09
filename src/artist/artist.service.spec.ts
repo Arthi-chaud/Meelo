b78b60f4d381ba503e2f76023ec74c18ee6e0c99
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-09 15:33:07 +0200

    Artist Service: Unit tests

--- src/artist/artist.service.spec.ts
+++ src/artist/artist.service.spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { FileManagerService } from "src/file-manager/file-manager.service";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { PrismaService } from "src/prisma/prisma.service";
-import { SettingsModule } from "src/settings/settings.module";
+import { Slug } from "src/slug/slug";
 import { FakeFileManagerService } from "test/FakeFileManagerModule";
+import { ArtistalreadyExistsException } from "./artist.exceptions";
 import { ArtistModule } from "./artist.module";
 import { ArtistService } from "./artist.service"
 
@@ -22,14 +23,67 @@ describe('Artist Service', () => {
 	it('should be defined', () => {
 		expect(artistService).toBeDefined();
 	});
+	let artistName = 'My name';
 
-	it(('should create a new artist'), async () => {
-		let artistName = 'My name';
-		let artist = await artistService.createArtist(artistName);
-		expect(artist.songs).toBeUndefined();
-		expect(artist.albums).toBeUndefined();
-		expect(artist.name).toBe(artistName);
-		expect(artist.slug).toBe('my-name');
-		expect(artist.id).toBeDefined();
+	describe('Create Artist', () => {
+		it(('should create a new artist'), async () => {
+			let artist = await artistService.createArtist(artistName);
+			expect(artist.songs).toBeUndefined();
+			expect(artist.albums).toBeUndefined();
+			expect(artist.name).toBe(artistName);
+			expect(artist.slug).toBe('my-name');
+			expect(artist.id).toBeDefined();
+		})
+	
+		it(('should throw as alrtist already exists'), () => {
+			const test = async () => {
+				await artistService.createArtist(artistName);
+			};
+			expect(test()).rejects.toThrow(ArtistalreadyExistsException);
+		})
+	})
+
+	describe('Get Artist', () => {
+		it(('should return an existing artist, without relations'), async () => {
+			let artist = await artistService.getArtist(new Slug(artistName));
+			expect(artist.songs).toBeUndefined();
+			expect(artist.albums).toBeUndefined();
+			expect(artist.name).toBe(artistName);
+			expect(artist.slug).toBe('my-name');
+			expect(artist.id).toBeDefined();
+		})
+	
+		it(('should return an existing artist, with relations'), async () => {
+			let artist = await artistService.getArtist(new Slug(artistName), {
+				albums: true,
+				songs: true
+			});
+			expect(artist.songs).toStrictEqual([]);
+			expect(artist.albums).toStrictEqual([]);
+			expect(artist.name).toBe(artistName);
+			expect(artist.slug).toBe('my-name');
+			expect(artist.id).toBeDefined();
+		})
+	});
+
+	describe('Get or Create Artist', () => {
+		it(('should get the existing artist'), async () => {
+			let artist = await artistService.getArtist(new Slug(artistName));
+			let artistGet = await artistService.getOrCreateArtist(artistName);
+			expect(artistGet.songs).toBeUndefined();
+			expect(artistGet.albums).toBeUndefined();
+			expect(artistGet.name).toBe(artist.name);
+			expect(artistGet.slug).toBe(artist.slug);
+			expect(artistGet.id).toBe(artist.id);
+		})
+	
+		it(('should create a new artist, as it does not exists'), async () => {
+			let artist = await artistService.getOrCreateArtist('My Artist2');
+			expect(artist.songs).toBeUndefined();
+			expect(artist.albums).toBeUndefined();
+			expect(artist.name).toBe('My Artist2');
+			expect(artist.slug).toBe('my-artist2');
+			expect(artist.id).toBe(artist.id);
+		})
 	})
 })
