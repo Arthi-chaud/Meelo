4c6e3efdb1f94472c644326dbd9952ffd9bc54bc
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-22 09:01:42 +0200

    Illustration Exceptions: Add new exceptions classes

--- src/illustration/illustration.exceptions.ts
+++ src/illustration/illustration.exceptions.ts
@@ -1,5 +1,5 @@
 import { HttpStatus } from "@nestjs/common"
-import { MeeloException } from "src/exceptions/meelo-exception"
+import { InvalidRequestException, MeeloException } from "src/exceptions/meelo-exception"
 import { Slug } from "src/slug/slug"
 
 class NoIllustrationFolderException extends MeeloException {
@@ -21,12 +21,12 @@ export class NoAlbumFolderIllustrationException extends NoIllustrationFolderExce
 }
 
 export class NoReleaseFolderIllustrationException extends NoIllustrationFolderException {
-	constructor(albumSlug: Slug, releaseId: number) {
-		super(`No illustration folder found for release '${releaseId}' of ${albumSlug.toString()}`)
+	constructor(albumSlug: Slug, releaseSlug: Slug) {
+		super(`No illustration folder found for release '${releaseSlug.toString()}' of ${albumSlug.toString()}`)
 	}
 }
 
-class NoIllustrationException extends MeeloException {
+export class NoIllustrationException extends MeeloException {
 	constructor(message: string) {
 		super(HttpStatus.NOT_FOUND, message)
 	}
@@ -45,7 +45,19 @@ export class NoAlbumIllustrationException extends NoIllustrationException {
 }
 
 export class NoReleaseIllustrationException extends NoIllustrationException {
-	constructor(albumSlug: Slug, releaseId: number) {
-		super(`No illustration found for release '${releaseId}' of ${albumSlug.toString()}`)
+	constructor(albumSlug: Slug, releaseSlug: Slug) {
+		super(`No illustration found for release '${releaseSlug.toString()}' of ${albumSlug.toString()}`)
+	}
+}
+
+export class CantDownloadIllustrationException extends InvalidRequestException {
+	constructor(illustrationURL: string) {
+		super(`Illustration from could not be downloaded from '${illustrationURL}'`);
+	}
+}
+
+export class IllustrationNotExtracted extends InvalidRequestException {
+	constructor(trackSourceFileName: string) {
+		super(`Illustration from file '${trackSourceFileName}' could not be extracted`);
 	}
 }
