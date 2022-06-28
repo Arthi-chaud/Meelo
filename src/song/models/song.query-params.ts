628b18debc099d0538874b89825bf873c9d08a44
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-28 16:37:52 +0200

    Song: Redo Query Parameters

--- src/song/models/song.query-params.ts
+++ src/song/models/song.query-params.ts
@@ -1,26 +1,64 @@
+import { Album, Song } from "@prisma/client";
+import { ArtistQueryParameters } from "src/artist/models/artist.query-parameters";
 import { Slug } from "src/slug/slug"
+import { OmitId } from "src/utils/omit-id";
+import { OmitReleaseDate } from "src/utils/omit-release-date";
 import { RequireAtLeastOne } from "src/utils/require-at-least-one";
 import { RequireOnlyOne } from "src/utils/require-only-one"
 import { SearchStringInput } from "src/utils/search-string-input";
 
-/**
- * Query paraeters to find a song
- */
-export type SongWhereInput = RequireOnlyOne<{
-	byId: { id: number },
-	bySlug: { slug: Slug, artistSlug: Slug }
-}>;
 
-/**
- * Query params to find multiple songs
- */
-export type SongsWhereInput = RequireAtLeastOne<{
-	byName: SearchStringInput,
-	byArtist: RequireOnlyOne<{ artistSlug?: Slug, artistId: number }>,
-	byPlayCount: RequireOnlyOne<{ below: number, exact: number, moreThan: number }>,
-}>;
+export namespace SongQueryParameters {
+	type OmitArtistId<T> = Omit<T, 'artistId'>;
+	type OmitPlayCount<T> = Omit<T, 'playCount'>;
+	/**
+	 * The input required to save a song in the database
+	 */
+	export type CreateInput = OmitPlayCount<OmitReleaseDate<OmitId<OmitArtistId<Song>>>>
+		& { releaseDate?: Date }
+		& { artist?: ArtistQueryParameters.WhereInput };
+	/**
+	 * The input required to update a song in the database
+	 */
+	export type UpdateInput = Partial<OmitId<OmitArtistId<Song>>>
+		& { artist?: ArtistQueryParameters.WhereInput };
+	/**
+	 * The input to find or create a song
+	 */
+	 export type GetOrCreateInput = CreateInput;
+	
+	/**
+	 * Query paraeters to find a song
+	 */
+	export type WhereInput = RequireOnlyOne<{
+		byId: { id: number },
+		bySlug: { slug: Slug, artist?: ArtistQueryParameters.WhereInput }
+	}>;
 
-export type SongRelationInclude = Partial<{
-	instances: boolean,
-	artist: boolean
-}>;
+	/**
+	 * Query params to find multiple songs
+	 */
+	export type ManyWhereInput = RequireAtLeastOne<{
+		name: SearchStringInput,
+		artist?: ArtistQueryParameters.WhereInput,
+		playCount: RequireOnlyOne<{ below: number, exact: number, moreThan: number }>,
+	}>;
+	/**
+	 * Defines what relations to include in query
+	 */
+	export type RelationInclude = Partial<{
+		instances: boolean,
+		artist: boolean
+	}>;
+
+	/**
+	 * Build the query parameters for ORM to include relations
+	 * @returns the ORM-ready query parameters
+	 */
+	 export function buildIncludeParameters(include?: RelationInclude) {
+		return {
+			instances: include?.instances ?? false,
+			artist: include?.artist ?? false
+		};
+	}
+}
