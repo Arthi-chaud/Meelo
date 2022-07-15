c70fb3f9a4aa36a31d008d6e422cf224ad0b71cf
Arthi-chaud <arthur.jamet@gmail.com>
2022-07-15 10:55:58 +0200

    Parse Id Pipe: Remove debug log

--- src/id/id.pipe.ts
+++ src/id/id.pipe.ts
@@ -3,7 +3,6 @@ import InvalidIdParsingInput from './id.exceptions';
 @Injectable()
 export class ParseIdPipe implements PipeTransform<string> {
 	transform(value: string, _metadata: ArgumentMetadata): number {
-		console.log(value);
 		let parsedId: number = parseInt(value);
 		if (isNaN(parsedId)) {
 			throw new InvalidIdParsingInput(value);
