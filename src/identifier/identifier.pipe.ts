fda92a33c0bbf5b6ea8db39b611bd9fdad269334
Arthi-chaud <arthur.jamet@gmail.com>
2022-07-15 13:02:17 +0200

    Identifier Pipe: Fix type compatibility

--- src/identifier/identifier.pipe.ts
+++ src/identifier/identifier.pipe.ts
@@ -1,9 +1,8 @@
 import type { ArgumentMetadata, PipeTransform } from "@nestjs/common";
 import { ParseIdPipe } from "src/identifier/id.pipe";
 import Slug from "src/slug/slug";
-import type RequireOnlyOne from "src/utils/require-only-one";
 
-export default class ParseResourceIdentifierPipe<W extends RequireOnlyOne<{ id: number, slug: Slug}>> implements PipeTransform {
+export default class ParseResourceIdentifierPipe<W extends Partial<{ id: number, slug: Slug}>> implements PipeTransform {
 	transform<T extends { id: any }>(value: T, _metadata: ArgumentMetadata): W {
 		try {
 			const id = new ParseIdPipe().transform(value.id, _metadata);
