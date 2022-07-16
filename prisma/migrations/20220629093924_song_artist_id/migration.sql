6a3cd2206923a13972997a0d7f7864eafd3793e6
Arthi-chaud <arthur.jamet@gmail.com>
2022-07-16 13:29:58 +0200

    Prisma schema: Remove cascade deletion + reset migrations

--- prisma/migrations/20220629093924_song_artist_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `artistId` on table `Song` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Song" ALTER COLUMN "artistId" SET NOT NULL;
