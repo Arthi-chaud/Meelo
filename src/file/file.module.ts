daade3a8f126357dbdc9351692d969f5a341b4a5
Arthi-chaud <arthur.jamet@gmail.com>
2022-07-18 14:15:19 +0200

    File Module: Import Settings Service

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import FileService from './file.service';
 import FileManagerModule from 'src/file-manager/file-manager.module';
 import PrismaModule from 'src/prisma/prisma.module';
+import FileController from './file.controller';
+import SettingsModule from 'src/settings/settings.module';
 
 @Module({
 	imports: [
 		PrismaModule,
-		FileManagerModule
+		FileManagerModule,
+		SettingsModule
 	],
 	providers: [FileService],
-	exports: [FileService]
+	exports: [FileService],
+	controllers: [FileController]
 })
 export default class FileModule {}
