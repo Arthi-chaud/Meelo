c98e27df871bdc1e72cabae59dcf5e58f9535dbc
Arthi-chaud <arthur.jamet@gmail.com>
2022-07-20 16:38:03 +0200

    Genre Module: Forward Refenrece to album & artist dependencies

--- src/genre/genre.module.ts
+++ src/genre/genre.module.ts
@@ -1,13 +1,19 @@
 import { Module, forwardRef } from '@nestjs/common';
+import AlbumModule from 'src/album/album.module';
+import ArtistModule from 'src/artist/artist.module';
 import PrismaModule from 'src/prisma/prisma.module';
 import SongModule from 'src/song/song.module';
+import { GenreController } from './genre.controller';
 import GenreService from './genre.service';
 
 @Module({
 	imports: [
 		PrismaModule,
-		forwardRef(() => SongModule)
+		forwardRef(() => SongModule),
+		forwardRef(() => ArtistModule),
+		forwardRef(() => AlbumModule),
 	],
+	controllers: [GenreController],
 	providers: [GenreService],
 	exports: [GenreService]
 })
