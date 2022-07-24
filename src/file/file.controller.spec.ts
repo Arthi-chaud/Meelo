8e51674b7be80ff95f21f35073a8b6b8273e43e1
Arthi-chaud <arthur.jamet@gmail.com>
2022-07-24 07:56:21 +0200

    File Module's Tests: Use Common Dummy Data

--- src/file/file.controller.spec.ts
+++ src/file/file.controller.spec.ts
@@ -1,56 +1,42 @@
 import type { INestApplication } from "@nestjs/common";
 import type { TestingModule } from "@nestjs/testing";
-import type { File, Library } from "@prisma/client";
+import type { File } from "@prisma/client";
 import FileManagerService from "src/file-manager/file-manager.service";
 import LibraryModule from "src/library/library.module";
-import LibraryService from "src/library/library.service";
 import PrismaModule from "src/prisma/prisma.module";
 import PrismaService from "src/prisma/prisma.service";
 import { FakeFileManagerService } from "test/fake-file-manager.module";
 import SetupApp from "test/setup-app";
 import { createTestingModule } from "test/test-module";
 import FileModule from "./file.module";
-import FileService from "./file.service";
 import request from 'supertest';
+import TestPrismaService from "test/test-prisma.service";
 
 describe('File Controller', () => {
-	let libraryService: LibraryService;
-	let fileService: FileService;
 	let app: INestApplication;
 
-	let library1: Library;
-	let file1: File;
+	let dummyRepository: TestPrismaService
 
 	beforeAll(async () => {
 		const module: TestingModule = await createTestingModule({
 			imports: [FileModule, LibraryModule, PrismaModule]
-		}).overrideProvider(FileManagerService).useClass(FakeFileManagerService).compile();
+		}).overrideProvider(FileManagerService).useClass(FakeFileManagerService)
+		.overrideProvider(PrismaService).useClass(TestPrismaService).compile();
 		app = await SetupApp(module);
-		await module.get<PrismaService>(PrismaService).onModuleInit();
-		libraryService = module.get<LibraryService>(LibraryService);
-		fileService = module.get<FileService>(FileService);
-		library1 = await libraryService.createLibrary({
-			path: "&",
-			name: "&"
-		});
-		file1 = await fileService.createFile({
-			path: "",
-			md5Checksum: "",
-			registerDate: new Date(),
-			libraryId: library1.id
-		});
+		dummyRepository = module.get(PrismaService);
+		await dummyRepository.onModuleInit();
 	});
 
 	describe('Get File', () => {
 		it("should find the file", async () => {
 			return request(app.getHttpServer())
-				.get(`/files/${file1.id}`)
+				.get(`/files/${dummyRepository.fileA1_1.id}`)
 				.expect(200)
 				.expect((res) => {
 					let file: File = res.body;
 					expect(file).toStrictEqual({
-						...file1,
-						registerDate: file1.registerDate.toISOString()
+						...dummyRepository.fileA1_1,
+						registerDate: dummyRepository.fileA1_1.registerDate.toISOString()
 					});
 				});
 		});
@@ -64,7 +50,7 @@ describe('File Controller', () => {
 	describe('Stream File File', () => {
 		it("should return an error, as the source file does not exist", async () => {
 			return request(app.getHttpServer())
-				.get(`/files/${file1.id}/stream`)
+				.get(`/files/${dummyRepository.fileA1_1.id}/stream`)
 				.expect(404);
 		});
 	})
