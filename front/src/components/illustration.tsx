762905ab54e4c961d90cc2eba68fecab41691fc7
Arthi-chaud <arthur.jamet@gmail.com>
2022-09-04 08:54:55 +0200

    Front: Add Wrapper component for illustrations

---
+++ front/src/components/illustration.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import FadeIn from "react-fade-in";
+import { useQuery } from "react-query";
+import API from "../api";
+import LoadingComponent, { WideLoadingComponent } from "./loading/loading";
+
+type IllustrationProps = {
+	/**
+	 * URL of the illustration to display
+	 * Must be an URL from an API response
+	 */
+	url: string;
+} & React.ImgHTMLAttributes<HTMLImageElement>
+
+const Illustration = (props: IllustrationProps) => {
+	const [illustrationURLReady, setIllustrationURLReady] = useState(!props.url.startsWith('/songs'))
+	const [illustrationURL, setIllustrationURL] = useState(API.getIllustrationURL(props.url));
+	useEffect(() => {
+		if (!illustrationURLReady)
+			fetch(illustrationURL, { redirect: 'follow' }).then((response) => {
+				setIllustrationURLReady(true);
+				if (response.redirected) {
+					setIllustrationURL(API.getIllustrationURL(new URL(response.url).pathname));
+				}
+			});
+	}, []);
+	const { isLoading, data } = useQuery(illustrationURL, () => fetch(illustrationURL).then((response) => response.blob()));
+	if (illustrationURLReady == false || isLoading)
+		return <></>;
+	return <FadeIn>
+		<img {...props} src={URL.createObjectURL(data!)}/>
+	</FadeIn>
+}
+
+export default Illustration;
