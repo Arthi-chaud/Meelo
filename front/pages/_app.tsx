ded072cef10fcae6f78504c60e4861260168305f
Arthi-chaud <arthur.jamet@gmail.com>
2022-09-11 17:04:03 +0200

    Front: Wrap App with basinc error boundary

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
+import { QueryClient, QueryClientProvider, Hydrate, QueryCache } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import "../styles/global.css";
 import { Box } from "@mui/material";
 import MeeloAppBar from "../src/components/appbar/appbar";
+import { ErrorBoundary } from 'react-error-boundary'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const [queryClient] = React.useState(() => new QueryClient());
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Hydrate state={pageProps.dehydratedState}>
-				<Box>
-					<MeeloAppBar/>
-					<Component {...pageProps} />
-				</Box>
-			</Hydrate>
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		<ErrorBoundary FallbackComponent={() => <Box>{"Hello"}</Box>}>
+			<QueryClientProvider client={queryClient}>
+				<Hydrate state={pageProps.dehydratedState}>
+					<Box>
+						<MeeloAppBar/>
+						<Component {...pageProps} />
+					</Box>
+				</Hydrate>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
+		</ErrorBoundary>
 	);
 }
 
