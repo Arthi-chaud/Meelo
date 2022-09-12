d6a0bcdf70c5dc7cd3223af606e1f28bdeab6fa4
Arthi-chaud <arthur.jamet@gmail.com>
2022-09-12 15:57:05 +0200

    Front: Basic Error Boundary with toast

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -6,11 +6,15 @@ import "../styles/global.css";
 import { Box } from "@mui/material";
 import MeeloAppBar from "../src/components/appbar/appbar";
 import { ErrorBoundary } from 'react-error-boundary'
+import toast, { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const [queryClient] = React.useState(() => new QueryClient());
 	return (
-		<ErrorBoundary FallbackComponent={() => <Box>{"Hello"}</Box>}>
+		<ErrorBoundary fallbackRender={(error) => {
+			toast.error(error.error.message, { duration: 3600, position: 'bottom-center' });
+			return <Toaster/>
+		}}>
 			<QueryClientProvider client={queryClient}>
 				<Hydrate state={pageProps.dehydratedState}>
 					<Box>
