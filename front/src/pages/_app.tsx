5df9bff184727fd2be60fab04a8b9a98b5b11611
Arthur Jamet <60505370+Arthi-chaud@users.noreply.github.com>
2022-09-19 20:25:15 +0100

    Front: Make Production Ready (#195)
    * Front: Add title to page + fix api routes
    
    * Front: Fix Illustration sourcing
    
    * Front: Add route to API on SSR

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -7,11 +7,16 @@ import { Box } from "@mui/material";
 import MeeloAppBar from "../components/appbar/appbar";
 import { ErrorBoundary } from 'react-error-boundary'
 import toast, { Toaster } from 'react-hot-toast';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const [queryClient] = useState(() => new QueryClient());
 	return (
 		<Box>
+			<Head>
+    			<title>Meelo</title>
+    			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    		</Head>
 			<QueryClientProvider client={queryClient}>
 				<MeeloAppBar/>
 				<ErrorBoundary
