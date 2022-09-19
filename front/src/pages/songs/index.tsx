4eb5b01d5ae0abcd4152eb7f3723a035f9633784
Arthi-chaud <arthur.jamet@gmail.com>
2022-09-19 14:36:51 +0100

    Front: Song Page: Sorting

--- front/src/pages/songs/index.tsx
+++ front/src/pages/songs/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MeeloAppBar from "../../components/appbar/appbar";
 import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from "next";
 import { Box, Divider, List } from '@mui/material';
@@ -15,24 +15,29 @@ import { dehydrate, QueryClient } from 'react-query';
 import { prepareMeeloInfiniteQuery } from '../../query';
 import { Page } from '../../components/infinite/infinite-scroll';
 import InfiniteView from '../../components/infinite/infinite-view';
+import { getOrderParams, getSortingFieldParams, SortingParameters } from '../../utils/sorting';
 
-const songsQuery = () => ({
-	key: ["songs"],
-	exec: (lastPage: Page<SongWithArtist>) => API.getAllSongs<SongWithArtist>(lastPage, ['artist'])
+const SongSortingFields: (keyof Song)[] = ['name', 'playCount']
+
+const songsQuery = (sort: SortingParameters<Song>) => ({
+	key: ["songs", sort],
+	exec: (lastPage: Page<SongWithArtist>) => API.getAllSongs<SongWithArtist>(lastPage, sort, ['artist'])
 });
 
-const librarySongsQuery = (slugOrId: string | number) => ({
-	key: ["library", slugOrId, "songs"],
-	exec: (lastPage: Page<SongWithArtist>) => API.getAllSongsInLibrary<SongWithArtist>(slugOrId, lastPage, ['artist'])
+const librarySongsQuery = (slugOrId: string | number, sort: SortingParameters<Song>) => ({
+	key: ["library", slugOrId, "songs", sort],
+	exec: (lastPage: Page<SongWithArtist>) => API.getAllSongsInLibrary<SongWithArtist>(slugOrId, lastPage, sort, ['artist'])
 });
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
 	const queryClient = new QueryClient();
 	const librarySlug = getLibrarySlug(context.req.url!) ?? null;
+	const order = getOrderParams(context.query.order);
+	const sortBy = getSortingFieldParams(context.query.sortBy, SongSortingFields);
 	if (librarySlug) {
-		await queryClient.prefetchInfiniteQuery(prepareMeeloInfiniteQuery(librarySongsQuery, librarySlug));
+		await queryClient.prefetchInfiniteQuery(prepareMeeloInfiniteQuery(librarySongsQuery, librarySlug, { sortBy, order }));
 	} else {
-		await queryClient.prefetchInfiniteQuery(prepareMeeloInfiniteQuery(songsQuery));
+		await queryClient.prefetchInfiniteQuery(prepareMeeloInfiniteQuery(songsQuery, { sortBy, order }));
 	}
 
 	return {
@@ -44,14 +49,18 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 }
 
 const LibrarySongsPage = ({ librarySlug }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-	const query = librarySlug ? librarySongsQuery(librarySlug) : songsQuery();
-
+	const router = useRouter();
+	const [order, setOrder] = useState(getOrderParams(router.query.order));
+	const [sortBy, setSortBy] = useState(getSortingFieldParams(router.query.sortBy, SongSortingFields));
 	return (
 		<InfiniteView
+			sortingFields={SongSortingFields}
 			view='list'
-			query={() => query}
+			query={() => librarySlug ? librarySongsQuery(librarySlug, { sortBy, order }) : songsQuery({ sortBy, order })}
 			renderListItem={(item: SongWithArtist) => <SongItem song={item} key={item.id}/> }
 			renderGridItem={(item: SongWithArtist) => <></> }
+			onSortingFieldSelect={(newField) => setSortBy(newField)}
+			onSortingOrderSelect={(newOrder) => setOrder(newOrder)}
 		/>
 	);
 }
