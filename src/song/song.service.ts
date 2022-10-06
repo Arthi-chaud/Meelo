c898f650b3938b62d88b0ffcf131eae6725e5567
Arthi-chaud <arthur.jamet@gmail.com>
2022-10-06 11:32:32 +0100

    Song Service: Avoid when possible setting a Video track as master

--- src/song/song.service.ts
+++ src/song/song.service.ts
@@ -180,18 +180,18 @@ export default class SongService extends RepositoryService<
 	 */
 	async updateSongMaster(where: SongQueryParameters.WhereInput): Promise<Track | null> {
 		let tracks = await this.trackService.getSongTracks(where, {}, { release: true });
+		const currentMaster = tracks.find((track) => track.master);
 		const sortedTracks = tracks
 			.filter((track) => track.release.releaseDate !== null)
-			.sort((trackA, trackB) => trackA.release.releaseDate!.getTime() - trackB.release.releaseDate!.getTime())
-		if (sortedTracks.length !== 0) {
-			const newMaster = sortedTracks.at(0)!
-			await this.trackService.setTrackAsMaster({ trackId: newMaster.id, song: where });
-			return { ...newMaster, master: true };
-		} else if (tracks.length !== 0) {
-			await this.trackService.setTrackAsMaster({ trackId: tracks[0].id, song: where });
-			return { ...tracks[0], master: true };
-		}
-		return null;
+			.sort((trackA, trackB) => trackA.release.releaseDate!.getTime() - trackB.release.releaseDate!.getTime());
+		const sortedAudioTracks = sortedTracks.filter((track) => track.type == 'Audio');
+		const newMaster = sortedAudioTracks.at(0) ?? sortedTracks.at(0) ?? tracks.at(0);
+		if (newMaster == null)
+			return null;
+		if (newMaster.id === currentMaster?.id)
+			return newMaster;
+		await this.trackService.setTrackAsMaster({ trackId: newMaster.id, song: where });
+		return { ...newMaster, master: true };
 	}
 
 	/**
