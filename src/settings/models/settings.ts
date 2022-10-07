865b059609f4d14c84791384f061932e421d745f
Arthi-chaud <arthur.jamet@gmail.com>
2022-10-07 20:41:46 +0100

    Settings: Fix model for swagger

--- src/settings/models/settings.ts
+++ src/settings/models/settings.ts
@@ -1,23 +1,39 @@
+import { ApiProperty } from "@nestjs/swagger";
 
 export const metadataSourceValue = ["path", "embedded"] as const;
 export const metadataOrderValue = ["only", "preferred"] as const;
+
+class MetadataSettings {
+	/**
+	 * Use the path or the embedded metadata as main metadata source
+	 */
+	@ApiProperty({ enum: metadataSourceValue })
+	source: typeof metadataSourceValue[number];
+	/**
+	 * Exclude the other source, or use is as a fallback
+	 */
+	@ApiProperty({ enum: metadataOrderValue })
+	order: typeof metadataOrderValue[number];
+}
 /**
  * Global settings of the Meelo server
  */
-export default interface Settings {
+export default class Settings {
 	/**
 	 * The base folder where every libraries must be located
 	 */
+	@ApiProperty()
 	dataFolder: string;
 	/**
 	 * Array of RegExp string, used to match track files
 	 */
+	@ApiProperty()
 	trackRegex: string[];
 	/**
 	 * Defines the metadata parsing policy
 	 */
-	metadata: {
-		source: typeof metadataSourceValue[number],
-		order: typeof metadataOrderValue[number],
-	};
+	@ApiProperty({
+		type: MetadataSettings
+	})
+	metadata: MetadataSettings;
 }
