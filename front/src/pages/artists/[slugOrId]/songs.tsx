c9fac33a6638edef76c2fca8f837eb62787e2ac2
Arthi-chaud <arthur.jamet@gmail.com>
2022-10-12 17:01:38 +0100

    Front: Artist's Songs Page

--- front/src/pages/artists/[slugOrId]/songs.tsx
+++ front/src/pages/artists/[slugOrId]/songs.tsx
@@ -5,26 +5,24 @@ import { dehydrate, QueryClient, useQuery } from "react-query";
 import API from "../../../api";
 import InfiniteAlbumView from "../../../components/infinite/infinite-album-view";
 import { Page } from "../../../components/infinite/infinite-scroll";
+import InfiniteSongView from "../../../components/infinite/infinite-song-view";
 import Album, { AlbumSortingKeys, AlbumWithArtist } from "../../../models/album";
+import Song, { SongSortingKeys, SongWithArtist } from "../../../models/song";
 import { prepareMeeloInfiniteQuery, prepareMeeloQuery } from "../../../query";
 import { SortingParameters } from "../../../utils/sorting";
 
-const artistAlbumsQuery = (artistSlugOrId: number | string, sort?: SortingParameters<typeof AlbumSortingKeys>) => ({
-	key: ["artist", artistSlugOrId, "albums", sort ?? {}],
-	exec: (lastPage: Page<Album>) => API.getArtistAlbums<AlbumWithArtist>(artistSlugOrId, lastPage, sort, ["artist"])
+const artistSongsQuery = (artistSlugOrId: number | string, sort?: SortingParameters<typeof SongSortingKeys>) => ({
+	key: ["artist", artistSlugOrId, "songs", sort ?? {}],
+	exec: (lastPage: Page<SongWithArtist>) => API.getArtistSongs<SongWithArtist>(artistSlugOrId, lastPage, sort, ['artist'])
 });
 
-const artistQuery = (slugOrId: string | number) => ({
-	key: ['artist', slugOrId],
-	exec: () => API.getArtist(slugOrId),
-});
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
 	const artistIdentifier = context.params!.slugOrId as string;
 	const queryClient = new QueryClient()
   
 	await Promise.all([
-		await queryClient.prefetchInfiniteQuery(prepareMeeloInfiniteQuery(artistAlbumsQuery, artistIdentifier))
+		await queryClient.prefetchInfiniteQuery(prepareMeeloInfiniteQuery(artistSongsQuery, artistIdentifier))
 	]);
   
 	return {
@@ -38,11 +36,10 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 const ArtistAlbumsPage = ({ artistIdentifier }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
 	const router = useRouter();
 	artistIdentifier ??= router.query.slugOrId as string;
-	return <InfiniteAlbumView
-		initialSortingField={'releaseDate'}
-		initialSortingOrder={'desc'}
-		initialView={'grid'}
-		query={(sort) => artistAlbumsQuery(artistIdentifier, sort)}
+	return <InfiniteSongView
+		initialSortingField={'name'}
+		initialSortingOrder={'asc'}
+		query={(sort) => artistSongsQuery(artistIdentifier, sort)}
 	/>
 }
 export default ArtistAlbumsPage;
