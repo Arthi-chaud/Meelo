dcb87802ee41ec6edda0c46d62285b108d9d2f14
Arthi-chaud <arthur.jamet@gmail.com>
2022-10-25 12:49:52 +0100

    Relation Include: Filter function to get only atomic relations

---
+++ src/relation-include/atomic-relation-include.filter.ts
@@ -0,0 +1,10 @@
+/**
+ * Filter an array of relation includes keys based on their names
+ * @param keys the keys to filter
+ * @param toKeep the keys to keep anyway
+ * @returns the filtered keys
+ */
+export function filterAtomicRelationInclude<Key extends string>(keys: readonly Key[], toKeep?: Key[]) {
+	let keysCopy = Array.from(keys);
+	return keysCopy.filter((key) => toKeep?.includes(key) || !key.endsWith('s'));
+}
