58393dd4b3956b210777ace243a0e3e9315bc9e0
Arthi-chaud <arthur.jamet@gmail.com>
2022-11-14 15:23:16 +0000

    CI: Single Build workflow

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 import { ApiExcludeController } from "@nestjs/swagger";
 
 @ApiExcludeController()
