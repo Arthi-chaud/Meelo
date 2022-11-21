5eb893463ab2be5836c273984bc87b35e9507b5d
Arthi-chaud <arthur.jamet@gmail.com>
2022-11-21 08:46:45 +0000

    Front: Unit Tests

---
+++ front/__tests__/utils/formatDuration.ts
@@ -0,0 +1,10 @@
+import formatDuration from "../../src/utils/formatDuration";
+
+describe('Format Duration', () => {
+	it('Should format duration correctly', () => {
+		const duration = 61;
+		const formatted = formatDuration(duration);
+
+		expect(formatted).toBe("1:01");
+	});
+});
