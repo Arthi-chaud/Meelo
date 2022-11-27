2b66b52f6b82e85c2daf1c53b9ac93d6a6540dee
Arthi-chaud <arthur.jamet@gmail.com>
2022-11-27 16:25:40 +0000

    Front: Settings Page: Style Tabs

--- front/src/pages/settings/[[...panel]].tsx
+++ front/src/pages/settings/[[...panel]].tsx
@@ -1,11 +1,7 @@
 import {
-	Box, Tab
-} from "@mui/material";
-import {
-	TabContext, TabList, TabPanel
-} from '@mui/lab';
+	Box, Tab, Tabs
+} from '@mui/material';
 import { useEffect, useState } from "react";
-import prepareSSR, { InferSSRProps } from "../../ssr";
 import { useRouter } from "next/router";
 import LibrariesSettings from "../../components/settings/libraries-settings";
 import UsersSettings from "../../components/settings/users-settings copy";
@@ -13,43 +9,37 @@ import UsersSettings from "../../components/settings/users-settings copy";
 const AvailablePanels = ['libraries', 'users'];
 
 const getPanelFromQuery = (query?: string): string => {
-	if (!AvailablePanels.includes(query?.toString().toLowerCase() ?? '')) {
+	if (!AvailablePanels.includes(query?.toLowerCase() ?? '')) {
 		return 'libraries';
 	}
 	return query!;
 };
 
-export const getServerSideProps = prepareSSR((context) => {
-	const panel = getPanelFromQuery(context.query.panel as string);
-
-	return {
-		additionalProps: { panel }
-	};
-});
-
-const SettingsPage = (props: InferSSRProps<typeof getServerSideProps>) => {
+const SettingsPage = () => {
 	const router = useRouter();
-	const [panel, setPanel] = useState(getPanelFromQuery(props.panel ?? router.query.panel));
+	const [panel, setPanel] = useState(getPanelFromQuery(router.query.panel?.at(0)));
 
 	useEffect(() => {
 		router.push(`/settings/${panel}`, undefined, { shallow: true });
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [panel]);
 	return <>
-		<TabContext value={panel}>
-			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-				<TabList onChange={(__, newPanel) => setPanel(AvailablePanels[newPanel])}>
-					{AvailablePanels.map((panelName) => <Tab key={panelName} label={panelName}/>)}
-				</TabList>
-			</Box>
-			{AvailablePanels.map((panelName) =>
-				<TabPanel key={panelName} value={panelName}>
-					{panelName == 'libraries'
-						? <LibrariesSettings/>
-						: <UsersSettings/>
-					}
-				</TabPanel>)}
-		</TabContext>
+		<Tabs
+			value={panel}
+			onChange={(__, panelName) => setPanel(panelName)}
+			indicatorColor='secondary'
+			textColor='secondary'
+			centered
+		>
+			{AvailablePanels.map((panelName, index) =>
+				<Tab key={index} value={panelName} label={panelName}/>)
+			}
+		</Tabs>
+		{AvailablePanels.map((panelName) => panelName == panel &&
+			<Box key={panelName}>
+				{panelName == 'libraries' ? <LibrariesSettings/> : <UsersSettings/>}
+			</Box>)
+		}
 	</>;
 };
 
