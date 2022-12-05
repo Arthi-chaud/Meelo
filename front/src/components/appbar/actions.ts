9e997130e37c10b3be08e22f9bc11e73721d6c6c
Arthi-chaud <arthur.jamet@gmail.com>
2022-12-05 11:15:47 +0000

    Front: AppBar: Extends Actions

--- front/src/components/appbar/actions.ts
+++ front/src/components/appbar/actions.ts
@@ -1,42 +1,23 @@
-import Action from "../contextual-menu/actions";
-import SearchIcon from '@mui/icons-material/Search';
-import AutoModeIcon from '@mui/icons-material/AutoMode';
-import API from "../../api/api";
-import toast from "react-hot-toast";
-import SettingsIcon from '@mui/icons-material/Settings';
-import { createElement } from "react";
-import LogoutIcon from '@mui/icons-material/Logout';
+import Action from "../actions/action";
 import store from "../../state/store";
-import { unsetAccessToken } from "../../state/userSlice";
+import { ScanAllLibrariesAction } from "../actions/library-task";
+import { GoToSearchAction, GoToSettingsAction } from "../actions/link";
+import { LogoutAction } from "../actions/auth";
 
 /**
  * Collections of actions that are accessible from appbar and drawer
  */
 const getAppBarActions: () => Action[] = () => [
+	GoToSearchAction,
 	{
-		label: 'Search',
-		icon: createElement(SearchIcon),
-		href: '/search',
+		...ScanAllLibrariesAction,
+		disabled: store.getState().user.user?.admin !== true
 	},
 	{
-		label: 'Refresh Libraries',
-		icon: createElement(AutoModeIcon),
-		disabled: store.getState().user.user?.admin !== true,
-		onClick: () => API.scanLibraries()
-			.then(({ status }) => toast.success(status, { duration: 4000 })),
+		...GoToSettingsAction,
+		disabled: store.getState().user.user?.admin !== true
 	},
-	{
-		label: 'Settings',
-		icon: createElement(SettingsIcon),
-		disabled: store.getState().user.user?.admin !== true,
-		href: '/settings'
-	},
-	{
-		label: 'Logout',
-		icon: createElement(LogoutIcon),
-		href: '/',
-		onClick: () => store.dispatch(unsetAccessToken())
-	}
+	LogoutAction
 ];
 
 export default getAppBarActions;
