cc32cb6f567601f077be7599ceceea3b03251dea
Arthi-chaud <arthur.jamet@gmail.com>
2022-12-06 19:28:52 +0000

    Front: Release/Album Contextual Menu: Add Archive Button

--- front/src/components/actions/download.tsx
+++ front/src/components/actions/download.tsx
@@ -1,4 +1,4 @@
-import { Download } from "@mui/icons-material";
+import { Archive, Download } from "@mui/icons-material";
 import Action from "./action";
 import { useConfirm } from "material-ui-confirm";
 import API from "../../api/api";
@@ -7,7 +7,7 @@ import confirmDownloadAction from "../confirm-download-action";
 export const DownloadAction = (
 	confirm: ReturnType<typeof useConfirm>, streamURL: string
 ): Action => ({
-	icon: <Download/>,
+	icon: <Download />,
 	label: "Download",
 	onClick: () => confirmDownloadAction(confirm, API.getStreamURL(streamURL))
 });
@@ -15,11 +15,31 @@ export const DownloadAction = (
 export const DownloadAsyncAction = (
 	confirm: ReturnType<typeof useConfirm>, streamURL: () => PromiseLike<string>
 ): Action => ({
-	icon: <Download/>,
+	icon: <Download />,
 	label: "Download",
 	onClick: () => streamURL()
 		.then((url) => confirmDownloadAction(
 			confirm,
 			API.getStreamURL(url)
+		))
+});
+
+export const DownloadReleaseAction = (
+	confirm: ReturnType<typeof useConfirm>, releaseId: number | string
+): Action => ({
+	icon: <Archive />,
+	label: "Archive",
+	onClick: () => confirmDownloadAction(confirm, API.getReleaseArchiveURL(releaseId))
+});
+
+export const DownloadReleaseAsyncAction = (
+	confirm: ReturnType<typeof useConfirm>, releaseId: () => PromiseLike<number | string>
+): Action => ({
+	icon: <Archive />,
+	label: "Archive",
+	onClick: () => releaseId()
+		.then((id) => confirmDownloadAction(
+			confirm,
+			API.getReleaseArchiveURL(id)
 		))
 });
