be448eb79dc6fa83186d4d61c86bf4735043e7e8
Arthi-chaud <arthur.jamet@gmail.com>
2022-12-24 00:16:17 +0100

    Relation Include Decorator: Use Reflection to Apply OpenAPI decorator

--- src/relation-include/relation-include-query.decorator.ts
+++ src/relation-include/relation-include-query.decorator.ts
@@ -1,10 +1,27 @@
 import { Query } from "@nestjs/common";
 import ParseBaseRelationIncludePipe from 'src/relation-include/relation-include.pipe';
+import "reflect-metadata";
+import { ApiRelationInclude } from "./relation-include-route.decorator";
+
 /**
  * Query parame decorator to parse relation includes
  * @param keys
  * @returns
  */
 export default function RelationIncludeQuery(keys: readonly string[]) {
-	return Query('with', new ParseBaseRelationIncludePipe(keys));
+	return function (target: any, functionName: string, parameterIndex: number) {
+		if (keys.length != 0) {
+			const descriptor = Reflect.getOwnPropertyDescriptor(target, functionName)!;
+
+			if (descriptor) {
+				const method = descriptor.value;
+
+				if (method instanceof Function) {
+					descriptor.value = ApiRelationInclude(keys)(target, functionName, descriptor);
+				}
+				Object.defineProperty(target, functionName, descriptor);
+			}
+		}
+		return Query('with', new ParseBaseRelationIncludePipe(keys))(target, functionName, parameterIndex);
+	};
 }
