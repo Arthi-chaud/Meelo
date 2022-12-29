55a694caae637128573902f96c2b13d9697cbb40
Arthur Jamet <60505370+Arthi-chaud@users.noreply.github.com>
2022-12-29 11:56:04 +0100

    Swagger: Custom css (#370)

--- server/src/exceptions/all-exceptions.filter.ts
+++ server/src/exceptions/all-exceptions.filter.ts
@@ -6,16 +6,25 @@ import type { Response } from 'express';
 
 @Catch()
 export default class AllExceptionsFilter extends BaseExceptionFilter {
-	catch(exception: Error, host: ArgumentsHost) {
+	catch(exception: any, host: ArgumentsHost) {
 		const ctx = host.switchToHttp();
 		const response = ctx.getResponse<Response>();
 
-		Logger.error(exception);
-		response
-			.status(HttpStatus.INTERNAL_SERVER_ERROR)
-			.json({
-				statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-				message: "An error occured, Try again.",
-			});
+		if (exception.code === 'ENOENT') {
+			response
+				.status(HttpStatus.NOT_FOUND)
+				.json({
+					statusCode: HttpStatus.NOT_FOUND,
+					message: "Not found."
+				});
+		} else {
+			Logger.error(exception);
+			response
+				.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.json({
+					statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+					message: "An error occured, Try again.",
+				});
+		}
 	}
 }
--- server/src/swagger/bootstrap.ts
+++ server/src/swagger/bootstrap.ts
@@ -1,16 +1,18 @@
-
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 export default async function bootstrapSwagger(app: INestApplication) {
 	const config = new DocumentBuilder()
-		.setTitle('Meelo')
-		.setDescription('API Documentation')
-		.setVersion('1.0')
+		.setTitle('API Documentation')
 		.addServer("/api", "API Path (for Production use)")
 		.addServer("/", "Application Path (for Dev use)")
 		.build();
-	const document = SwaggerModule.createDocument(app, config);
+	const document = SwaggerModule.createDocument(app, config,);
 
-	SwaggerModule.setup('/docs', app, document);
+	SwaggerModule.setup('/docs', app, document, {
+		customSiteTitle: 'Swagger - Meelo',
+		customfavIcon: './favicon.ico',
+		customCssUrl: './swagger/styles.css',
+		customJs: './swagger/script.js'
+	});
 }
