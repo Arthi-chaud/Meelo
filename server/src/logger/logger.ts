ef6ef664d4aa5c9469064d1cc328fc44b904667c
Arthi-chaud <arthur.jamet@gmail.com>
2023-01-03 18:19:23 +0000

    Logger: Format

--- server/src/logger/logger.ts
+++ server/src/logger/logger.ts
@@ -1,3 +1,14 @@
-import { ConsoleLogger } from '@nestjs/common';
+import { ConsoleLogger, LogLevel } from '@nestjs/common';
 
-export default class Logger extends ConsoleLogger {}
+export default class Logger extends ConsoleLogger {
+	override formatMessage(
+		logLevel: LogLevel, message: unknown,
+		_pidMessage: string, formattedLogLevel: string,
+		contextMessage: string, _timestampDiff: string
+	): string {
+		const output = this.stringifyMessage(message, logLevel);
+
+		formattedLogLevel = this.colorize(formattedLogLevel, logLevel);
+		return `${this.getTimestamp()} ${formattedLogLevel} ${contextMessage}: ${output}\n`;
+	}
+}
