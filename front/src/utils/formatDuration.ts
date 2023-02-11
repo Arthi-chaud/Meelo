8c83831416b8a8419244a29dce0f886a5c3029da
Arthi-chaud <arthur.jamet@gmail.com>
2023-02-11 17:58:45 +0000

    Front: Fix Linter rule

--- front/src/utils/formatDuration.ts
+++ front/src/utils/formatDuration.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-restricted-imports
 import formatMilliSecondsDuration from "format-duration";
 
 const formatDuration = (seconds?: number) => seconds ? formatMilliSecondsDuration(seconds * 1000) : '--:--';
