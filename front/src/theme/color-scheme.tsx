d6027cefe10be8cf72ace76750dfb91c38ecd552
Arthur Jamet <arthur.jamet@gmail.com>
2023-02-22 10:28:33 +0000

    Front: Create ColorScheme Hook

---
+++ front/src/theme/color-scheme.tsx
@@ -0,0 +1,22 @@
+import { useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "../state/store";
+
+/**
+ * Hook to get the current color shceme (light or dark)
+ * Needs the store
+ */
+const useColorScheme = () => {
+	const colorSetting = useSelector((state: RootState) => state.settings.colorScheme);
+	const systemPrefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+	if (colorSetting == 'system') {
+		if (systemPrefersDarkMode) {
+			return 'dark';
+		}
+		return 'light';
+	}
+	return colorSetting;
+};
+
+export default useColorScheme;
--- front/src/theme/provider.tsx
+++ front/src/theme/provider.tsx
@@ -1,34 +1,30 @@
 import {
 	// eslint-disable-next-line no-restricted-imports
 	CssBaseline, GlobalStyles, ThemeProvider as MUIThemeProvider,
-	createTheme, responsiveFontSizes, useMediaQuery
-} from "@mui/material";
+	createTheme, responsiveFontSizes} from "@mui/material";
 import Styles from "./style";
-import { useSelector } from "react-redux";
-import { RootState } from "../state/store";
 import { useMemo } from "react";
 import {
 	DarkTheme, GlobalTheme, LightTheme
 } from "./theme";
+import useColorScheme from "./color-scheme";
 
+/**
+ * Provides the Theme
+ */
 const ThemeProvider = (props: { children: any }) => {
-	const colorSetting = useSelector((state: RootState) => state.settings.colorScheme);
-	const systemPrefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+	const colorScheme = useColorScheme();
 	const theme = useMemo(
 		() => {
-			const scheme = colorSetting == 'system' ?
-				systemPrefersDarkMode ? 'dark' : 'light' :
-				colorSetting;
-
 			return responsiveFontSizes(createTheme({
 				palette: {
-					mode: scheme,
-					...scheme == 'light' ? LightTheme : DarkTheme
+					mode: colorScheme,
+					...colorScheme == 'light' ? LightTheme : DarkTheme
 				},
 				...GlobalTheme
 			}));
 		},
-		[colorSetting, systemPrefersDarkMode]
+		[colorScheme]
 	);
 
 	return <MUIThemeProvider theme={theme}>
