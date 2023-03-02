ca4e7bbdeaf3dbd058862d2980f3349aff1ffa42
Arthur Jamet <arthur.jamet@gmail.com>
2023-03-02 10:10:44 +0000

    Provider Service: Centralisation of action run

--- server/src/providers/provider.exception.ts
+++ server/src/providers/provider.exception.ts
@@ -1,4 +1,4 @@
-import { InvalidRequestException } from "src/exceptions/meelo-exception";
+import { InvalidRequestException, NotFoundException } from "src/exceptions/meelo-exception";
 
 export class ProviderMethodNotAvailableError extends InvalidRequestException {
 	constructor(providerName: string) {
@@ -9,5 +9,11 @@ export class ProviderMethodNotAvailableError extends InvalidRequestException {
 export class UnknownProviderError extends InvalidRequestException {
 	constructor(providerName: string) {
 		super(`Provider ${providerName}: Provider is unknown`);
+	}
+}
+
+export class AllProvidersFailedError extends NotFoundException {
+	constructor(actionName: string) {
+		super(`All providers failed action '${actionName}'`);
 	}
 }
--- server/src/providers/provider.service.ts
+++ server/src/providers/provider.service.ts
@@ -3,7 +3,8 @@ import MusixMatchProvider from "./musixmatch/musixmatch.provider";
 import IProvider from "./iprovider";
 import SettingsService from "src/settings/settings.service";
 import ProviderActions from "./provider-actions";
-import { AlbumType } from "@prisma/client";
+import { AllProvidersFailedError } from "./provider.exception";
+import Logger from "src/logger/logger";
 
 /**
  * Orchestrates of Providers
@@ -11,6 +12,7 @@ import { AlbumType } from "@prisma/client";
 @Injectable()
 export default class ProviderService implements ProviderActions {
 	private readonly enabledProviders: IProvider<unknown>[];
+	private readonly logger = new Logger(ProviderService.name);
 
 	constructor(
 		musixmatchProvider: MusixMatchProvider,
@@ -21,37 +23,58 @@ export default class ProviderService implements ProviderActions {
 
 		providers.forEach((provider) => {
 			if (providersSettings[provider.name].enabled === true) {
+				this.logger.log(`Provider '${provider.name}' enabled`);
 				provider.settings = providersSettings[provider.name];
 				this.enabledProviders.push(provider);
+			} else {
+				this.logger.warn(`Provider '${provider.name}' disabled`);
 			}
 		});
 	}
 
-	getArtistIdentifier(artistName: string, songName?: string | undefined): Promise<string> {
-		throw new Error("Method not implemented.");
+	getArtistIdentifier(...parameters: Parameters<ProviderActions['getArtistIdentifier']>) {
+		return this.runAction('getArtistIdentifier', parameters);
 	}
 
-	getSongIdentifier(songName: string, artistName: string): Promise<string> {
-		throw new Error("Method not implemented.");
+	getSongIdentifier(...parameters: Parameters<ProviderActions['getSongIdentifier']>) {
+		return this.runAction('getSongIdentifier', parameters);
 	}
 
-	getAlbumIdentifier(albumName: string, artistName?: string | undefined): Promise<string> {
-		throw new Error("Method not implemented.");
+	getAlbumIdentifier(...parameters: Parameters<ProviderActions['getAlbumIdentifier']>) {
+		return this.runAction('getAlbumIdentifier', parameters);
 	}
 
-	getAlbumType(albumIdentifer: string): Promise<AlbumType> {
-		throw new Error("Method not implemented.");
+	getAlbumType(...parameters: Parameters<ProviderActions['getAlbumType']>) {
+		return this.runAction('getAlbumType', parameters);
 	}
 
-	getArtistIllustrationUrl(artistIdentifer: string): Promise<string> {
-		throw new Error("Method not implemented.");
+	getArtistIllustrationUrl(...parameters: Parameters<ProviderActions['getArtistIllustrationUrl']>) {
+		return this.runAction('getArtistIllustrationUrl', parameters);
 	}
 
-	getSongLyrics(songIdentifier: string): Promise<string> {
-		throw new Error("Method not implemented.");
+	getSongLyrics(...parameters: Parameters<ProviderActions['getSongLyrics']>) {
+		return this.runAction('getSongLyrics', parameters);
 	}
 
-	getSongGenres(songIdentifier: string): Promise<string[]> {
-		throw new Error("Method not implemented.");
+	getSongGenres(...parameters: Parameters<ProviderActions['getSongGenres']>) {
+		return this.runAction('getSongGenres', parameters);
+	}
+
+	/**
+	 * Calls action method on each enabled provider, until one suceeds
+	 * If all fails, rejects
+	 */
+	private async runAction<ActionName extends keyof ProviderActions = keyof ProviderActions>(
+		actionName: ActionName,
+		parameters: Parameters<ProviderActions[ActionName]>
+	): Promise<Awaited<ReturnType<ProviderActions[ActionName]>>> {
+		for (const provider of this.enabledProviders) {
+			try {
+				return await provider[actionName].call(provider, ...parameters);
+			} catch {
+				continue;
+			}
+		}
+		throw new AllProvidersFailedError(actionName);
 	}
 }
