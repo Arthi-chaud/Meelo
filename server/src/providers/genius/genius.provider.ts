072cbe2e2591cee6ec9a994b43cf8a310d73fafa
Arthur Jamet <arthur.jamet@gmail.com>
2023-03-04 19:21:47 +0000

    Genius Provider: Parse by hand, remove dependencies

--- server/src/providers/genius/genius.provider.ts
+++ server/src/providers/genius/genius.provider.ts
@@ -2,31 +2,57 @@ import { Injectable, OnModuleInit } from "@nestjs/common";
 import IProvider from "../iprovider";
 import GeniusSettings from "./genius.settings";
 import SettingsService from "src/settings/settings.service";
-import * as Genius from 'genius-lyrics';
 import Slug from "src/slug/slug";
 import levenshtein from "damerau-levenshtein";
 import { ProviderActionFailedError } from "../provider.exception";
 import { name, version } from 'package.json';
+import { HttpService } from "@nestjs/axios";
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export default class GeniusProvider extends IProvider<GeniusSettings, number> implements OnModuleInit {
-	private geniusClient: Genius.Client;
 	constructor(
+		private httpService: HttpService,
 		private settingsService: SettingsService
 	) {
 		super("genius");
 	}
 
-	onModuleInit() {
-		this._settings = this.settingsService.settingsValues.providers.genius;
-		this.geniusClient = new Genius.Client(
-			process.env.NODE_ENV == 'test' ? process.env.GENIUS_ACCESS_TOKEN : this._settings.apiKey
-		);
-		this.geniusClient.config.requestOptions = {
-			headers: {
+	private fetchAPI(route: string) {
+		return this._fetch(route, 'https://api.genius.com')
+			.then((res) => res.response);
+	}
+
+	private fetchWebPage(route: string) {
+		return this._fetch(route, 'https://genius.com');
+	}
+
+	private _fetch(route: string, baseURL: string) {
+		const accessToken = process.env.NODE_ENV == 'test'
+			? process.env.GENIUS_ACCESS_TOKEN
+			: this._settings.apiKey;
+
+		return this.httpService.axiosRef.get(route, {
+			baseURL, headers: {
+				"Authorization": "Bearer " + accessToken,
 				"User-Agent": `${name}, ${version}`
 			}
-		};
+		}).then((res) => res.data);
+	}
+
+	// Thanks to https://github.com/farshed/genius-lyrics-api/blob/9634a6e99b8b0f96cf82a53e8f1ff13d2d604b8e/lib/utils/index.js#L11
+	private sanitizeQuery(query: string) {
+		return query
+			.toLowerCase()
+			.replace(/ *\([^)]*\) */g, "")
+			.replace(/ *\[[^\]]*]/, "")
+			.replace(/feat\.|ft\./g, "")
+			.replace(/\s+/g, " ")
+			.trim();
+	}
+
+	onModuleInit() {
+		this._settings = this.settingsService.settingsValues.providers.genius;
 	}
 
 	getProviderBannerUrl(): string {
@@ -40,20 +66,18 @@ export default class GeniusProvider extends IProvider<GeniusSettings, number> im
 	async getArtistIdentifier(artistName: string, songName?: string): Promise<number> {
 		try {
 			const sluggedArtistName = new Slug(artistName).toString();
-			const searchResults = await this.geniusClient.songs.search(
-				`${artistName} ${songName ?? ''}`,
-				{ sanitizeQuery: true }
-			);
+			const searchResults = await this.fetchAPI('/search?q=' + this.sanitizeQuery(`${artistName} ${songName ?? ''}`))
+				.then((res) => res.hits.map((hit: any) => hit.result));
 
 			return searchResults
-				.map((song) => ({
+				.map((song: any) => ({
 					similarity: levenshtein(
-						new Slug(song.artist.name).toString(),
+						new Slug(song.primary_artist.name).toString(),
 						sluggedArtistName
 					).similarity,
-					id: song.artist.id
+					id: song.primary_artist.id
 				}))
-				.sort((artistA, artistB) => artistB.similarity - artistA.similarity)
+				.sort((artistA: any, artistB: any) => artistB.similarity - artistA.similarity)
 				.at(0)!.id;
 		} catch (err) {
 			throw new ProviderActionFailedError(this.name, 'getArtistIdentifier', err.message);
@@ -63,21 +87,19 @@ export default class GeniusProvider extends IProvider<GeniusSettings, number> im
 	async getSongIdentifier(songName: string, artistIdentifer: number): Promise<number> {
 		try {
 			const sluggedSongName = new Slug(songName).toString();
-			const searchResults = await this.geniusClient.songs.search(
-				songName,
-				{ sanitizeQuery: true }
-			);
+			const searchResults = await this.fetchAPI(`/search?q=${this.sanitizeQuery(songName)}`)
+				.then((res) => res.hits.map((hit: any) => hit.result));
 
 			return searchResults
-				.filter((song) => song.artist.id == artistIdentifer)
-				.map((song) => ({
+				.filter((song: any) => song.primary_artist.id == artistIdentifer)
+				.map((song: any) => ({
 					similarity: levenshtein(
 						new Slug(song.title).toString(),
 						sluggedSongName
 					).similarity,
 					id: song.id
 				}))
-				.sort((songA, songB) => songB.similarity - songA.similarity)
+				.sort((songA: any, songB: any) => songB.similarity - songA.similarity)
 				.at(0)!.id;
 		} catch (err) {
 			throw new ProviderActionFailedError(this.name, 'getSongIdentifier', err.message);
@@ -85,23 +107,46 @@ export default class GeniusProvider extends IProvider<GeniusSettings, number> im
 	}
 
 	async getArtistIllustrationUrl(artistIdentifer: number): Promise<string> {
-		const artist = await this.geniusClient.artists.get(artistIdentifer).catch((err) => {
-			throw new ProviderActionFailedError(this.name, 'getSongIdentifier', err.message);
-		});
+		const artist = await this.fetchAPI(`/artists/${artistIdentifer}`).catch((err) => {
+			throw new ProviderActionFailedError(this.name, 'getArtistIllustrationUrl', err.message);
+		}).then((res) => res.artist);
 
-		if (artist.image.includes("default_avatar")) {
-			throw new ProviderActionFailedError(this.name, 'getSongIdentifier', "No Image");
+		if (artist.image_url.includes("default_avatar")) {
+			throw new ProviderActionFailedError(this.name, 'getArtistIllustrationUrl', "No Image");
 		}
-		return artist.image;
+		return artist.image_url;
 	}
 
 	async getSongLyrics(songIdentifier: number): Promise<string> {
 		try {
-			const song = await this.geniusClient.songs.get(songIdentifier);
+			const song = await this.fetchAPI(`/songs/${songIdentifier}`).catch((err) => {
+				throw new ProviderActionFailedError(this.name, 'getSongLyrics', err.message);
+			}).then((res) => res.song);
+			const songPage = await this.fetchWebPage(song.url);
+			const parsedPage = cheerio.load(songPage);
+			// Inspired by https://github.com/zyrouge/node-genius-lyrics/blob/8499a0aeb91fd5eba296f96c19a24db28a2177d5/lib/songs/song.ts#L76
+			const primaryLyricsContainer = parsedPage(".lyrics").text().trim();
 
-			return await song.lyrics();
+			if (primaryLyricsContainer.length) {
+				return primaryLyricsContainer;
+			}
+			const secondaryLyricsContainer = parsedPage("div[class*='Lyrics__Container']")
+				.toArray()
+				.map((lyricContainer: any) => {
+					const parsedLyrics = parsedPage(lyricContainer as any);
+
+					parsedLyrics.find("br").replaceWith("\n");
+					return parsedLyrics.text();
+				})
+				.join("\n")
+				.trim();
+
+			if (secondaryLyricsContainer.length) {
+				return secondaryLyricsContainer;
+			}
 		} catch (err) {
-			throw new ProviderActionFailedError(this.name, 'getSongLyrics', err);
+			throw new ProviderActionFailedError(this.name, 'getSongLyrics', err.message);
 		}
+		throw new ProviderActionFailedError(this.name, 'getSongLyrics', "No Lyrics Found");
 	}
 }
