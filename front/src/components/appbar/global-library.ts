4597e32a4e2d68f96043b254300755d704b9af46
Arthur Jamet <arthur.jamet@gmail.com>
2023-04-29 19:00:53 +0100

    Front: Pseudo Global Library: Fix typing

--- front/src/components/appbar/global-library.ts
+++ front/src/components/appbar/global-library.ts
@@ -3,6 +3,6 @@ import Library from "../../models/library";
 /**
  * Dummy library object for 'all' library
  */
-const globalLibrary: Library = { name: 'All', slug: '', id: -1 };
+const globalLibrary: Library = { name: 'All', slug: '', id: -1, path: '' };
 
 export default globalLibrary;
