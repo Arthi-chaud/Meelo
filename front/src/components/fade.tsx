9bff290ff903cb05162288d0370749edc74d1b95
Arthur Jamet <arthur.jamet@gmail.com>
2023-09-10 18:11:05 +0200

    Front: Introduce fade wrapping

---
+++ front/src/components/fade.tsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-restricted-imports
+import { Fade as MUIFade } from '@mui/material';
+import { isClientSideRendering } from '../ssr';
+/**
+ * Wrapper around MUI's Fade.
+ * Fade is the main animation used in the app.
+ * However, when SSR, the opacity of the child node will be stuck at 0, and no animation is applied
+ * This component fixes it.
+ */
+
+type FadeProps = Parameters<typeof MUIFade>[0];
+
+const Fade = (props: FadeProps) => {
+	return <MUIFade {...props} appear={(props.appear ?? true) && isClientSideRendering()}/>;
+};
+
+export default Fade;
