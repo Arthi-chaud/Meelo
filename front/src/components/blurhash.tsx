5f0fc2e46b2af68bdbec798a69747510896ce11d
Arthur Jamet <arthur.jamet@gmail.com>
2023-10-26 19:06:58 +0200

    Front: Fix blurhash transitions

--- front/src/components/blurhash.tsx
+++ front/src/components/blurhash.tsx
@@ -1,4 +1,4 @@
-import { Box, createStyles, useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { blurHashToDataURL } from "../utils/blurhashToDataUrl";
 import { isSSR } from "../ssr";
 import { Blurhash as RBlurhash } from "react-blurhash";
@@ -12,7 +12,7 @@ const Blurhash = ({ blurhash, ...props }: BlurhashProps) => {
 	const theme = useTheme();
 	const fadeIn = {
 		'opacity': 1,
-		'animation': `fadeIn 0.2s ease-in`,
+		'animation': `fadeIn 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms`,
 		"@keyframes fadeIn": { '0%': { opacity: 0 } }
 	};
 	const ssrProps = () => ({
@@ -30,22 +30,24 @@ const Blurhash = ({ blurhash, ...props }: BlurhashProps) => {
 		sx: {
 			borderRadius: theme.shape.borderRadius,
 			...props.sx,
-			...fadeIn
+			// ...fadeIn
 		},
 	} as const);
 
-	return <Box
-		{...(isSSR() ? ssrProps() : csrProps())}
-		suppressHydrationWarning
-	>
-		{ isSSR() && blurhash
-			? <RBlurhash
-				hash={blurhash}
-				style={{ width: '100%', height: '100%' }}
-			/>
-			: <></>
-		}
-	</Box>;
+	return <Fade in={blurhash !== undefined} suppressHydrationWarning>
+		<Box
+			{...(isSSR() ? ssrProps() : csrProps())}
+			suppressHydrationWarning
+		>
+			{ !isSSR() && blurhash
+				? <RBlurhash
+					hash={blurhash}
+					style={{ width: '100%', height: '100%' }}
+				/>
+				: <></>
+			}
+		</Box>
+	</Fade>;
 };
 
 export default Blurhash;
