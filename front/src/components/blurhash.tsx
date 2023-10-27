4a333a88d38ff0b31e7f9642cf3c418fa6e424e9
Arthur Jamet <arthur.jamet@gmail.com>
2023-10-27 10:14:55 +0200

    Front: Blurhash: transitions only for bg, not for image placeholders

--- front/src/components/blurhash.tsx
+++ front/src/components/blurhash.tsx
@@ -1,8 +1,9 @@
-import { Box, useTheme } from "@mui/material";
+import {
+	Box, NoSsr, useTheme
+} from "@mui/material";
 import { blurHashToDataURL } from "../utils/blurhashToDataUrl";
 import { isSSR } from "../ssr";
 import { Blurhash as RBlurhash } from "react-blurhash";
-import Fade from "./fade";
 
 type BlurhashProps = Parameters<typeof Box>['0'] & {
 	blurhash?: string
@@ -10,11 +11,7 @@ type BlurhashProps = Parameters<typeof Box>['0'] & {
 
 const Blurhash = ({ blurhash, ...props }: BlurhashProps) => {
 	const theme = useTheme();
-	const fadeIn = {
-		'opacity': 1,
-		'animation': `fadeIn 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms`,
-		"@keyframes fadeIn": { '0%': { opacity: 0 } }
-	};
+
 	const ssrProps = () => ({
 		...props,
 		sx: {
@@ -22,7 +19,6 @@ const Blurhash = ({ blurhash, ...props }: BlurhashProps) => {
 			backgroundRepeat: 'no-repeat',
 			backgroundSize: 'cover',
 			...props.sx,
-			...fadeIn
 		},
 	});
 	const csrProps = () => ({
@@ -30,24 +26,20 @@ const Blurhash = ({ blurhash, ...props }: BlurhashProps) => {
 		sx: {
 			borderRadius: theme.shape.borderRadius,
 			...props.sx,
-			// ...fadeIn
 		},
 	} as const);
 
-	return <Fade in={blurhash !== undefined} suppressHydrationWarning>
-		<Box
-			{...(isSSR() ? ssrProps() : csrProps())}
-			suppressHydrationWarning
-		>
-			{ !isSSR() && blurhash
-				? <RBlurhash
-					hash={blurhash}
-					style={{ width: '100%', height: '100%' }}
-				/>
-				: <></>
-			}
-		</Box>
-	</Fade>;
+	return <Box
+		suppressHydrationWarning
+		{...(isSSR() ? ssrProps() : csrProps())}
+	>
+		<NoSsr>
+			{ blurhash && <RBlurhash
+				hash={blurhash}
+				style={{ width: '100%', height: '100%' }}
+			/> }
+		</NoSsr>
+	</Box>;
 };
 
 export default Blurhash;
