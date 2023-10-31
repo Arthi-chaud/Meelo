a6390013de4da17834bedaca38ab1b6264fae000
Arthur Jamet <arthur.jamet@gmail.com>
2023-10-31 11:23:40 +0100

    Front: Toast fits theme

--- front/src/components/toaster.tsx
+++ front/src/components/toaster.tsx
@@ -1,3 +1,6 @@
+
+import { useMediaQuery, useTheme } from '@mui/material';
+import hexToRgba from 'hex-to-rgba';
 import { useEffect } from 'react';
 import * as Toasts from 'react-hot-toast';
 
@@ -5,6 +8,9 @@ import * as Toasts from 'react-hot-toast';
 const Toaster = () => {
 	const { toasts } = Toasts.useToasterStore();
 	const TOAST_LIMIT = 2;
+	const theme = useTheme();
+	const themePaperColor = hexToRgba(theme.palette.background.paper, 0.90);
+	const viewPortIsSmall = useMediaQuery(theme.breakpoints.down('md'));
 
 	useEffect(() => {
 		toasts
@@ -12,7 +18,14 @@ const Toaster = () => {
 			.filter((item, index) => index >= TOAST_LIMIT)
 			.forEach((toast) => Toasts.toast.dismiss(toast.id));
 	}, [toasts]);
-	return <Toasts.Toaster toastOptions={{ duration: 2500 }} position='bottom-center' />;
+	return <Toasts.Toaster
+		toastOptions={{ duration: 2500, style: {
+			borderRadius: theme.shape.borderRadius,
+			backgroundColor: themePaperColor,
+			color: theme.palette.text.primary,
+		} }}
+		position={viewPortIsSmall ? 'top-center' : 'top-right'}
+	/>;
 };
 
 export default Toaster;
