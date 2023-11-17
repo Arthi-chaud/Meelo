7fddc682e8e83884306a58e1bf0c1517f383241b
Arthur Jamet <arthur.jamet@gmail.com>
2023-11-17 13:56:41 +0100

    Discogs Provider: Add Methods to fetch artist and album metadata

--- server/src/providers/discogs/discogs.provider.ts
+++ server/src/providers/discogs/discogs.provider.ts
@@ -1,14 +1,26 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
-import IProvider from "../iprovider";
+import IProvider, { ArtistMetadata } from "../iprovider";
 import DiscogsSettings from "./discogs.settings";
+import { HttpService } from "@nestjs/axios";
+import { ProviderActionFailedError } from "../provider.exception";
+import {
+	homepage, name, version
+} from 'package.json';
+import { isNumber, isString } from "class-validator";
+import SettingsService from "src/settings/settings.service";
 
 @Injectable()
 export default class DiscogsProvider extends IProvider<DiscogsSettings> implements OnModuleInit {
-	constructor() {
+	constructor(
+		private readonly httpService: HttpService,
+		private settingsService: SettingsService
+	) {
 		super('discogs');
 	}
 
-	onModuleInit() {}
+	onModuleInit() {
+		this._settings = this.settingsService.settingsValues.providers.discogs;
+	}
 
 	getProviderHomepage(): string {
 		return 'https://www.discogs.com';
@@ -22,7 +34,82 @@ export default class DiscogsProvider extends IProvider<DiscogsSettings> implemen
 		return 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Discogs_record_icon.svg/256px-Discogs_record_icon.svg.png';
 	}
 
+	getArtistURL(artistIdentifier: string): string {
+		return `${this.getProviderHomepage()}/artist/${artistIdentifier}`;
+	}
+
+	getAlbumURL(albumIdentifier: string): string {
+		return `${this.getProviderHomepage()}/master/${albumIdentifier}`;
+	}
+
 	getReleaseURL(releaseIdentifier: string): string {
 		return `${this.getProviderHomepage()}/release/${releaseIdentifier}`;
+	}
+
+	async getArtistMetadataByIdentifier(artistIdentifier: string): Promise<ArtistMetadata> {
+		try {
+			const artist = await this.fetch(`/artists/${artistIdentifier}`);
+
+			if (isNumber(artist.id) && isString(artist.profile_plaintext)) {
+				return {
+					value: artist.id.toString(),
+					description: artist.profile_plaintext
+				};
+			}
+			throw new ProviderActionFailedError(this.name, 'getArtistMetadataByIdentifier', 'Invalid Data Type');
+		} catch {
+			throw new ProviderActionFailedError(this.name, 'getArtistMetadataByIdentifier', 'Artist Not Found');
+		}
+	}
+
+	async getAlbumMetadataByIdentifier(masterIdentifier: string): Promise<ArtistMetadata> {
+		try {
+			const album = await this.fetch(`/masters/${masterIdentifier}`);
+
+			if (isNumber(album.id) && isString(album.notes_plaintext)) {
+				return {
+					value: album.id.toString(),
+					description: album.notes_plaintext
+				};
+			}
+			throw new ProviderActionFailedError(this.name, 'getAlbumMetadataByIdentifier', 'Invalid Data Type');
+		} catch {
+			throw new ProviderActionFailedError(this.name, 'getAlbumMetadataByIdentifier', 'Album Not Found');
+		}
+	}
+
+	async getReleaseMetadataByIdentifier(releaseIdentifier: string): Promise<ArtistMetadata> {
+		try {
+			const release = await this.fetch(`/releases/${releaseIdentifier}`);
+
+			if (isNumber(release.id) && isString(release.notes_plaintext)) {
+				return {
+					value: release.id.toString(),
+					description: release.notes_plaintext
+				};
+			}
+			throw new ProviderActionFailedError(this.name, 'getReleaseMetadataByIdentifier', 'Invalid Data Type');
+		} catch {
+			throw new ProviderActionFailedError(this.name, 'getReleaseMetadataByIdentifier', 'Release Not Found');
+		}
+	}
+
+	async fetch(route: string): Promise<any> {
+		const accessToken = process.env.NODE_ENV == 'test'
+			? process.env.DISCOGS_ACCESS_TOKEN
+			: this.settings.apiKey;
+
+		try {
+			return this.httpService.axiosRef.get(route, {
+				baseURL: 'https://api.discogs.com',
+				headers: {
+					"Authorization": "Bearer " + accessToken,
+					"Accept": "application/vnd.discogs.v2.plaintext+json",
+					"User-Agent": `${name}/${version} +${homepage}`
+				}
+			}).then((res) => res.data);
+		} catch {
+			throw new ProviderActionFailedError(this.name, 'getArtistMetadataByIdentifier', 'Artist Not Found');
+		}
 	}
 }
