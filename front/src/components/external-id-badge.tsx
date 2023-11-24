24cf4451656d69c4f6c3ee063e1e6b3a34766119
Arthur Jamet <arthur.jamet@gmail.com>
2023-11-24 08:55:04 +0100

    Front: Song Page: add tab, and move genres, external ids there + add description

--- front/src/components/external-id-badge.tsx
+++ front/src/components/external-id-badge.tsx
@@ -1,21 +1,25 @@
-import {
-	Box, Button, Link
-} from "@mui/material";
+import { Box, Button } from "@mui/material";
 import ExternalId from "../models/external-id";
 import Illustration from "./illustration";
+import Link from "next/link";
 
 type ExternalIdBadgeProps = {
 	externalId: ExternalId
 }
 
 const ExternalIdBadge = ({ externalId }: ExternalIdBadgeProps) => {
-	return <Link href={externalId.url ?? undefined} rel="noopener noreferrer" target="_blank">
-		<Button variant="outlined" startIcon={<Box sx={{ width: 30 }}>
-			<Illustration url={externalId.provider.icon} quality="original"/>
-		</Box>}>
-			{externalId.provider.name}
-		</Button>
-	</Link>;
+	const badge = <Button variant="outlined" startIcon={<Box sx={{ width: 30 }}>
+		<Illustration url={externalId.provider.icon} quality="original"/>
+	</Box>}>
+		{externalId.provider.name}
+	</Button>;
+
+	if(externalId.url) {
+		return <Link href={externalId.url ?? undefined} rel="noopener noreferrer" target="_blank">
+			{badge}
+		</Link>;
+	}
+	return badge;
 };
 
 export default ExternalIdBadge;
