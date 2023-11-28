8ee0231003fd338ef997bd44ce22f2d3fa2363e5
Arthur Jamet <arthur.jamet@gmail.com>
2023-11-28 09:36:14 +0100

    Front: Release Page: Recover Star ratings

--- front/src/pages/releases/[slugOrId].tsx
+++ front/src/pages/releases/[slugOrId].tsx
@@ -1,6 +1,6 @@
 import {
 	Button, Container, Divider, Grid, IconButton,
-	ListSubheader, Typography, useMediaQuery, useTheme
+	ListSubheader, Rating, Typography, useMediaQuery, useTheme
 } from "@mui/material";
 import { Box } from "@mui/system";
 import { useRouter } from "next/router";
@@ -34,6 +34,7 @@ import getYear from "../../utils/getYear";
 import Fade from "../../components/fade";
 import BackgroundBlurhash from "../../components/blurhash-background";
 import ResourceDescriptionExpandable from "../../components/resource-description-expandable";
+import { Star1 } from "iconsax-react";
 
 const releaseQuery = (releaseIdentifier: string | number) => API.getRelease(releaseIdentifier, ['album', 'externalIds']);
 const releaseTracklistQuery = (releaseIdentifier: string | number) => API.getReleaseTrackList(releaseIdentifier, ['artist', 'featuring']);
@@ -144,6 +145,10 @@ const ReleasePage = (props: InferSSRProps<typeof getServerSideProps>) => {
 	const illustration = useMemo(() => release.data?.illustration, [release]);
 	const externalIdWithDescription = album.data?.externalIds
 		.find(({ description }) => description !== null);
+	const albumRating = album.data?.externalIds
+		.map(({ rating }) => rating)
+		.filter((rating) => rating !== null)
+		.sort().at(-1) ?? null;
 
 	// eslint-disable-next-line no-extra-parens
 	if (!release.data || !album.data || !artists.data || !trackList) {
@@ -177,12 +182,23 @@ const ReleasePage = (props: InferSSRProps<typeof getServerSideProps>) => {
 							</Link>
 						</Grid>
 					}
-					<Grid item>
-						<Typography sx={{ color: 'text.disabled' }}>
+					<Grid item style={{ alignItems: 'center', display: 'inline-flex' }}>
+						<Typography sx={{ color: 'text.disabled' }} component={'span'}>
 							{(release.data.releaseDate || release.data.album.releaseDate) &&
 								`${new Date(release.data.releaseDate ?? release.data.album.releaseDate!).getFullYear()} - `}
 							{formatDuration(totalDuration ?? undefined)}
 						</Typography>
+						{albumRating && <Rating
+							sx={{ paddingLeft: 1.5 }}
+							readOnly
+							value={albumRating / 20}
+							icon={<Star1 size={18} style={{ marginTop: -3 }} color={
+								Array.of(...illustration?.colors ?? []).sort().at(4)
+							} />}
+							emptyIcon={<Star1 size={18} style={{ marginTop: -3 }}
+								color={theme.palette.text.disabled} opacity={0.2}
+							/>}
+						/>}
 					</Grid>
 				</Grid>
 				<Grid item container lg={3}
