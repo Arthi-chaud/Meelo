931e471da1064298955a0e54370d9ea8d29f6906
Arthur Jamet <arthur.jamet@gmail.com>
2023-12-16 19:58:08 +0100

    License: Add Header + ESLint Rule

--- front/src/utils/blurhashToDataUrl.ts
+++ front/src/utils/blurhashToDataUrl.ts
@@ -1,6 +1,23 @@
-/* eslint-disable no-shadow */
+/*
+ * Meelo is a music server and application to enjoy your personal music files anywhere, anytime you want.
+ * Copyright (C) 2023
+ *
+ * Meelo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Meelo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* eslint-disable max-len */
-/* eslint-disable id-length */
+/* eslint-disable no-shadow */
 /* eslint-disable init-declarations */
 // From https://gist.github.com/mattiaz9/53cb67040fa135cb395b1d015a200aff.
 // Thanks to the author
