88d7ce5971783cc3e6a5b504a150922eda0d97ec
Arthur Jamet <arthur.jamet@gmail.com>
2024-01-09 09:17:39 +0100

    Prisma Service: Add Shutdown Hook + Move 'flushingDb' method to test module

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -16,45 +16,29 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
+import {
+	INestApplication,
+	Injectable,
+	OnModuleDestroy,
+	OnModuleInit,
+} from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
-import Logger from "src/logger/logger";
 
 @Injectable()
 export default class PrismaService
 	extends PrismaClient
-	implements OnModuleInit
+	implements OnModuleInit, OnModuleDestroy
 {
-	private readonly logger = new Logger(PrismaService.name);
 	async onModuleInit() {
 		await this.$connect();
-		// if (process.env.NODE_ENV === 'development') {
-		// 	await this.flushDatabase();
-		// }
+	}
+	async onModuleDestroy() {
+		await this.$disconnect();
 	}
 
 	async enableShutdownHooks(app: INestApplication) {
 		this.$on("beforeExit", async () => {
 			await app.close();
 		});
-	}
-
-	protected async flushDatabase() {
-		this.logger.warn("Flushing database");
-		const tablenames = await this.$queryRaw<
-			Array<{ tablename: string }>
-		>`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
-
-		for (const { tablename } of tablenames) {
-			if (tablename !== "_prisma_migrations") {
-				try {
-					await this.$executeRawUnsafe(
-						`TRUNCATE TABLE "public"."${tablename}" CASCADE;`,
-					);
-				} catch (error) {
-					this.logger.error(`Flushing table '${tablename}' failed`);
-				}
-			}
-		}
 	}
 }
