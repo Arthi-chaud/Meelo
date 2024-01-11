e3c0cae233c2276c808c45341a334966ae26a967
Arthur Jamet <arthur.jamet@gmail.com>
2024-01-11 13:56:38 +0100

    Prisma: Add hash field to release illustrations + remove track_illustration model

---
+++ server/prisma/migrations/20240111124042_release_illustration/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `track_illustrations` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `hash` to the `release_illustrations` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "track_illustrations" DROP CONSTRAINT "track_illustrations_trackId_fkey";
+
+-- AlterTable
+ALTER TABLE "release_illustrations" ADD COLUMN     "hash" TEXT NOT NULL;
+ALTER TABLE "release_illustrations" ADD COLUMN     "track" INTEGER;
+
+-- DropTable
+DROP TABLE "track_illustrations";
