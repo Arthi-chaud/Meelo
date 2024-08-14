30def999586c9901253b6074e887f6556a7bbf92
Arthur Jamet <arthur.jamet@gmail.com>
2024-08-14 20:28:24 +0200

    Scanner: Connect to API

--- scanner/app/main.go
+++ scanner/app/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"os"
+	"time"
+
 	_ "github.com/Arthi-chaud/Meelo/scanner/app/docs"
+	"github.com/Arthi-chaud/Meelo/scanner/internal/api"
+	"github.com/Arthi-chaud/Meelo/scanner/internal/config"
 	"github.com/kpango/glg"
 	"github.com/labstack/echo/v4"
 	"github.com/swaggo/echo-swagger"
 )
+
+const ApiHealthckechAttemptCount = 5
+
+// @title Meelo's Scanner API
+// @description The scanner is responsible for file parsing and registration.
+func main() {
+	setupLogger()
+	c := config.GetConfig()
+	e := setupEcho()
+
+	waitForApi(c)
+	e.Logger.Fatal(e.Start(":8133"))
+}
 
 func setupLogger() {
 	glg.Get().
@@ -17,14 +35,29 @@ func setupLogger() {
 		SetLineTraceMode(glg.TraceLineNone)
 }
 
-// @title Meelo's Scanner API
-// @description The scanner is responsible for file parsing and registration.
-func main() {
-	setupLogger()
+// Sets up echo endpoints
+func setupEcho() *echo.Echo {
 	e := echo.New()
-	glg.Log("Hello World!")
+	e.HideBanner = true
+	e.HidePort = true
 
 	e.GET("/", Status)
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
-	e.Logger.Fatal(e.Start(":8133"))
+	return e
+}
+
+// hangs while API is not reachable.
+// after ApiHealthckechAttemptCount attempts, exits
+func waitForApi(c config.Config) {
+	for i := 0; i < ApiHealthckechAttemptCount; i++ {
+		if err := api.HealthCheck(c); err != nil {
+			glg.Failf("Failed connecting to API: %s", err)
+			time.Sleep(5 * time.Second)
+		} else {
+			glg.Success("Connected to API 🥳")
+			return
+		}
+	}
+	glg.Fatal("Could not connect to the API. Exiting...")
+	os.Exit(1)
 }
