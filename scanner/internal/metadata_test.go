2aa6eb84997e290716a70665914d8f99aa4adf06
Arthur Jamet <arthur.jamet@gmail.com>
2024-08-16 19:38:17 +0200

    Scanner: Test Metadata Merging + sanitization

--- scanner/internal/metadata_test.go
+++ scanner/internal/metadata_test.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -10,13 +11,16 @@ func TestVMergeMetadataCorrectOverride(t *testing.T) {
 	m1 := Metadata{
 		Artist:      "A",
 		AlbumArtist: "",
+		ReleaseDate: time.Time{},
 	}
 	m2 := Metadata{
 		Artist:      "B",
 		AlbumArtist: "B",
+		ReleaseDate: time.Date(2007, 1, 1, 1, 1, 1, 1, time.UTC),
 	}
 	m3 := Merge(m1, m2)
 
 	assert.Equal(t, "A", m3.Artist)
 	assert.Equal(t, "B", m3.AlbumArtist)
+	assert.Equal(t, 2007, m3.ReleaseDate.Year())
 }
