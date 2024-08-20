ae44cd55741737b9a42b1ac49defea883d72f250
Arthur Jamet <arthur.jamet@gmail.com>
2024-08-20 09:15:17 +0200

    Scanner: Check if requesting user is admin

---
+++ scanner/app/context.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Arthi-chaud/Meelo/scanner/internal/config"
+
+type ScannerContext struct {
+	config *config.Config
+}
--- scanner/internal/api/api.go
+++ scanner/internal/api/api.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/Arthi-chaud/Meelo/scanner/internal/config"
+	"github.com/go-playground/validator/v10"
 	"github.com/kpango/glg"
 )
 
@@ -14,11 +16,25 @@ func HealthCheck(config config.Config) error {
 	return err
 }
 
+func GetUserFromAccessToken(config config.Config, accessToken string) (User, error) {
+	config.AccessToken = accessToken
+	res, err := request("GET", "/users/me", nil, config)
+	if err != nil {
+		return User{}, err
+	}
+	var u = User{}
+	err = validate(res, &u)
+	return u, err
+}
+
 func request(method string, url string, body io.Reader, config config.Config) (string, error) {
 	client := &http.Client{}
 	req, _ := http.NewRequest(method, fmt.Sprintf("%s%s", config.ApiUrl, url), body)
 	if body != nil {
 		req.Header.Set("Content-Type", "application/json")
+	}
+	if config.AccessToken != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", config.AccessToken))
 	}
 	// req.Header.Set("x-api-key", config.ApiKey)
 	resp, err := client.Do(req)
@@ -38,4 +54,15 @@ func request(method string, url string, body io.Reader, config config.Config) (s
 		return "", err
 	}
 	return string(b), nil
+}
+
+func validate[T any](res string, obj *T) error {
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	if err := json.Unmarshal([]byte(res), obj); err != nil {
+		return err
+	}
+	if err := validate.Struct(*obj); err != nil {
+		return err
+	}
+	return nil
 }
