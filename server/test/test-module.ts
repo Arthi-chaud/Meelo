bb147d567ff0ed03c5c674f5654a34c47c91d1ca
Arthur Jamet <arthur.jamet@gmail.com>
2024-09-28 16:16:14 +0100

    Server: Fix setup of rabbitmq for tests

--- server/test/test-module.ts
+++ server/test/test-module.ts
@@ -1,9 +1,24 @@
 import { AmqpConnection, RabbitMQModule } from "@golevelup/nestjs-rabbitmq";
-import type { ModuleMetadata } from "@nestjs/common";
+import { Module, type ModuleMetadata } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import { MemoryStoredFile, NestjsFormDataModule } from "nestjs-form-data";
 import { MeiliSearchModule } from "nestjs-meilisearch";
 import { createMock } from "@golevelup/ts-jest";
+import { EventsModule } from "src/events/events.module";
+import { EventsService } from "src/events/events.service";
+
+@Module({
+	imports: [RabbitMQModule.forRoot(RabbitMQModule)],
+	providers: [
+		{
+			provide: AmqpConnection,
+			useValue: createMock<AmqpConnection>(),
+		},
+		EventsService,
+	],
+	exports: [AmqpConnection, EventsService],
+})
+class MockEventsModule {}
 
 export function createTestingModule(metadata: ModuleMetadata) {
 	return Test.createTestingModule({
@@ -12,13 +27,13 @@ export function createTestingModule(metadata: ModuleMetadata) {
 				host: process.env.MEILI_HOST ?? "localhost:7700",
 				apiKey: process.env.MEILI_MASTER_KEY,
 			}),
-			RabbitMQModule.forRoot(RabbitMQModule),
 			NestjsFormDataModule.config({
 				storage: MemoryStoredFile,
 				isGlobal: true,
 				cleanupAfterSuccessHandle: true,
 				cleanupAfterFailedHandle: true,
 			}),
+			EventsModule,
 		),
 		exports: [AmqpConnection, ...(metadata.exports ?? [])],
 		providers: [
@@ -28,5 +43,7 @@ export function createTestingModule(metadata: ModuleMetadata) {
 			},
 			...(metadata.providers ?? []),
 		],
-	});
+	})
+		.overrideModule(EventsModule)
+		.useModule(MockEventsModule);
 }
