b0e6a361bbf20c0c8a84acfdf499dcd9108d21cf
Arthur Jamet <arthur.jamet@gmail.com>
2024-10-13 13:23:57 +0100

    Matcher: Configure API Client + Ping API at Startup

--- matcher/matcher/__main__.py
+++ matcher/matcher/__main__.py
@@ -2,6 +2,7 @@ import pika
 import os
 import logging
 from .models.event import Event
+from .api import API
 
 
 def main():
@@ -20,6 +21,10 @@ def main():
 
     channel.basic_consume(queue="meelo", on_message_callback=callback)
     logging.basicConfig(level=logging.INFO)
+    api_client = API()
+    if not api_client.ping():
+        logging.error("Could not connect to API. Exiting...")
+        exit(1)
     logging.info("Ready to match!")
     channel.start_consuming()
 
