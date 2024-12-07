818b07cb90575fe389a4a81ae8bae4d6641bf828
Arthur Jamet <arthur.jamet@gmail.com>
2024-12-07 17:26:44 +0000

    Matcher: Get rating from metacritic

--- matcher/matcher/providers/metacritic.py
+++ matcher/matcher/providers/metacritic.py
@@ -3,7 +3,9 @@ from typing import Any
 from matcher.settings import MetacriticSettings
 from .base import ArtistSearchResult, BaseProvider, AlbumSearchResult
 from ..models.api.provider import Provider as ApiProviderEntry
-from datetime import date
+import requests
+from bs4 import BeautifulSoup, Tag
+from datetime import date, datetime
 
 
 @dataclass
@@ -16,7 +18,10 @@ class MetacriticProvider(BaseProvider):
         pass
 
     def get_musicbrainz_relation_key(self) -> str | None:
-        return None
+        pass
+
+    def is_musicbrainz_relation(self, rel: Any) -> bool | None:
+        return "metacritic" in rel["target"]
 
     def get_artist_id_from_url(self, artist_url) -> str | None:
         return None
@@ -42,22 +47,54 @@ class MetacriticProvider(BaseProvider):
         pass
 
     def get_album_url_from_id(self, album_id: str) -> str | None:
-        pass
+        return f'https://www.metacritic.com/music/{album_id.replace('music/', '')}'
 
     def get_album_id_from_url(self, album_url) -> str | None:
-        pass
+        return album_url.replace("https://www.metacritic.com/music/", "")
 
     def get_album(self, album_id: str) -> Any | None:
-        pass
+        album_url = self.get_album_url_from_id(album_id)
+        try:
+            html = requests.get(
+                str(album_url),
+                headers={"User-Agent": "Meelo Matcher/0.0.1"},
+            ).text
+            soup = BeautifulSoup(html, "html.parser")
+            return soup
+        except Exception:
+            pass
 
     def get_album_description(self, album: Any, album_url: str) -> str | None:
         pass
+        # Note, MC's description are rarely of satisfactory quality/length
+        # tag: Tag = album
+        # try:
+        #     description = tag.find(
+        #         "span", attrs={"itemprop": "description"}
+        #     ).text.strip()  # pyright: ignore
+        #     return description if len(description) > 0 else None
+        # except Exception:
+        #     pass
 
     def get_album_release_date(self, album: Any, album_url: str) -> date | None:
-        pass
+        tag: Tag = album
+        try:
+            release_date = tag.find(
+                "span", attrs={"itemprop": "datePublished"}
+            ).text.strip()  # pyright: ignore
+            return datetime.strptime(release_date, "%b %d, %Y").date()
+        except Exception:
+            pass
 
     def get_wikidata_album_relation_key(self) -> str | None:
         return "P1712"
 
     def get_album_rating(self, album: Any, album_url: str) -> int | None:
-        pass
+        tag: Tag = album
+        try:
+            raw_value = tag.find("span", attrs={"itemprop": "ratingValue"}).text  # pyright: ignore
+            if not raw_value.isnumeric():
+                return None
+            return int(raw_value)
+        except Exception:
+            pass
