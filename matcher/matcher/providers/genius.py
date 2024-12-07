416b1974027a28b9ab9dbc9bfabbb11fdfa45a0b
Arthur Jamet <arthur.jamet@gmail.com>
2024-12-07 10:11:39 +0000

    Matcher: Search Album and release date With Genius

--- matcher/matcher/providers/genius.py
+++ matcher/matcher/providers/genius.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import logging
 from typing import Any
 import requests
 from .base import ArtistSearchResult, BaseProvider, AlbumSearchResult
@@ -32,9 +33,9 @@ class GeniusProvider(BaseProvider):
             artists = self._fetch("/search/artist", {"q": artist_name})["response"][
                 "sections"
             ][0]["hits"]
+            artist_slug = self._slugify(artist_name)
             for artist in artists:
-                # TODO Use slug
-                if artist["result"]["name"] == artist_name:
+                if self._slugify(artist["result"]["name"]) == artist_slug:
                     return ArtistSearchResult(artist["result"]["name"])
             return None
         except Exception:
@@ -94,26 +95,68 @@ class GeniusProvider(BaseProvider):
     def get_wikidata_artist_relation_key(self) -> str | None:
         return "P2373"
 
+    # TODO Use a real slug
+    def _slugify(self, s: str) -> str:
+        return "".join(s.lower().split())
+
     # Album
     def search_album(
         self, album_name: str, artist_name: str | None
     ) -> AlbumSearchResult | None:
-        pass
+        artist_slug = None if not artist_name else self._slugify(artist_name)
+        album_slug = self._slugify(album_name)
+        try:
+            albums = self._fetch(
+                "/search/album", {"q": f"{album_name} {artist_name or ""}"}
+            )["response"]["sections"][0]["hits"]
+            for album in albums:
+                album = album["result"]
+                if (
+                    artist_slug
+                    and self._slugify(album["artist"]["name"]) != artist_slug
+                ):
+                    continue
+                if self._slugify(album["name"]) == album_slug:
+                    album_url = album["url"]
+                    return AlbumSearchResult(str(self.get_album_id_from_url(album_url)))
+            return None
+        except Exception:
+            return None
 
     def get_album_url_from_id(self, album_id: str) -> str | None:
-        pass
+        return f"https://genius.com/albums/{album_id}"
 
     def get_album_id_from_url(self, album_url) -> str | None:
-        pass
+        return album_url.replace("https://genius.com/albums/", "")
 
     def get_album(self, album_id: str) -> Any | None:
-        pass
+        try:
+            artists = self._fetch("/search/album", {"q": album_id})["response"][
+                "sections"
+            ][0]["hits"]
+            for artist in artists:
+                artist = artist["result"]
+                if not artist["_type"] == "album":
+                    continue
+                if artist["url"] == self.get_album_url_from_id(album_id):
+                    return artist
+            return None
+        except Exception:
+            return None
 
     def get_album_description(self, album: Any, album_url: str) -> str | None:
         pass
 
     def get_album_release_date(self, album: Any, album_url: str) -> date | None:
-        pass
+        try:
+            album_release_date = album["release_date_components"]
+            return date(
+                album_release_date["year"],
+                album_release_date["month"],
+                album_release_date["day"],
+            )
+        except Exception:
+            pass
 
     def get_wikidata_album_relation_key(self) -> str | None:
         return "P6217"
--- matcher/tests/matcher/album.py
+++ matcher/tests/matcher/album.py
@@ -12,12 +12,13 @@ class TestMatchAlbum(unittest.TestCase):
 
     # TODO Test album where rating on allmusic is 0
     # TODO Test compilation
+    # TODO Test album where rating from metacritic
     # TODO Test where an album has the same name as a song
     # TODO Test a compilation album does not get mixed up with a single
 
     def test_get_album(self):
         [matches, date] = match_album(1, "Confessions on a Dancefloor", "Madonna")
-        print(matches, date)
+        print(matches.sources)
         # Release date
         self.assertIsNotNone(date)
         self.assertEqual(date, datetime.date(2005, 11, 11))
@@ -30,26 +31,25 @@ class TestMatchAlbum(unittest.TestCase):
         self.assertTrue("2005" in str(matches.description))
         # ##Sources
         # self.assertEqual(len(matches.sources), 6)
-        # ### Wikipedia
+        ### Wikipedia
         [wiki] = [p for p in matches.sources if "wiki" in p.url]
         self.assertEqual(
             wiki.url, "https://en.wikipedia.org/wiki/Confessions on a Dance Floor"
         )
-        # ### Metacritic
-        # [meta] = [p for p in matches.sources if "metacritic" in p.url]
-        # self.assertEqual(meta.url, "https://www.metacritic.com/person/madonna")
-        # ### Discogs
+        ### Discogs
         [discogs] = [p for p in matches.sources if "discogs" in p.url]
         self.assertEqual(discogs.url, "https://www.discogs.com/master/34205")
-        # ### Genius
-        # [genius] = [p for p in matches.sources if "genius" in p.url]
-        # self.assertEqual(genius.url, "https://genius.com/artists/Madonna")
-        # ### Musicbrainz
+        ### Genius
+        [genius] = [p for p in matches.sources if "genius" in p.url]
+        self.assertEqual(
+            genius.url, "https://genius.com/albums/Madonna/Confessions-on-a-dance-floor"
+        )
+        ### Musicbrainz
         [mb] = [p for p in matches.sources if "musicbrainz" in p.url]
         self.assertEqual(
             mb.url,
             "https://musicbrainz.org/release-group/122677be-e664-362d-95eb-be3ae126ec03",
         )
-        # ### Allmusic
+        ### Allmusic
         [allmusic] = [p for p in matches.sources if "allmusic" in p.url]
         self.assertEqual(allmusic.url, "https://www.allmusic.com/album/mw0000356345")
