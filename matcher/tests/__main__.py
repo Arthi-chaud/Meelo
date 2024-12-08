e40b7496546ab2c65bfacedf24cae92e19769a7e
Arthur Jamet <arthur.jamet@gmail.com>
2024-12-08 09:33:49 +0000

    Matcher: Unit Tests for Metacritic, Wikipedia and Allmusic

--- matcher/tests/__main__.py
+++ matcher/tests/__main__.py
@@ -3,6 +3,10 @@ from .api import *
 from .settings import *
 from .matcher.artist import *
 from .matcher.album import *
+from .providers.metacritic import *
+from .providers.allmusic import *
+from .providers.wikipedia import *
+
 from dotenv import load_dotenv
 
 if __name__ == "__main__":
---
+++ matcher/tests/providers/wikipedia.py
@@ -0,0 +1,27 @@
+import unittest
+from matcher.context import Context
+from matcher.providers.base import BaseProvider
+from tests.matcher.common import MatcherTestUtils
+from matcher.providers.wikipedia import WikipediaProvider
+
+
+class TestWikipedia(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        MatcherTestUtils.setup_context()
+
+    def test_get_album_description(self):
+        provider: BaseProvider = Context().get().get_provider(WikipediaProvider)  # pyright: ignore
+        album = provider.get_album("Do You Like My Tight Sweater?")
+        self.assertIsNotNone(album)
+        description = provider.get_album_description(album, "")
+        self.assertIsNotNone(description)
+        self.assertIn("first album", description)
+
+    def test_get_artist_description(self):
+        provider: BaseProvider = Context().get().get_provider(WikipediaProvider)  # pyright: ignore
+        artist = provider.get_album("Siobhán Donaghy")
+        self.assertIsNotNone(artist)
+        description = provider.get_artist_description(artist, "")
+        self.assertIsNotNone(description)
+        self.assertIn("Sugababes", description)
