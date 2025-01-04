b57e6d2b68b63dd9275ce5f4b2450db213be1505
Arthur Jamet <arthur.jamet@gmail.com>
2025-01-04 15:52:41 +0000

    Prisma: Rename video table + add master track field to video

--- server/prisma/migrations/20250102092150_new_video_model/migration.sql
+++ server/prisma/migrations/20250102092150_new_video_model/migration.sql
@@ -1,41 +1,58 @@
 -- CreateEnum
-CREATE TYPE "video-types" AS ENUM ('MusicVideo', 'LyricsVideo', 'Live', 'BehindTheScenes', 'Interview', 'Advert', 'PhotoGallery', 'Documentary', 'Other');
+CREATE TYPE "video-types" AS ENUM (
+    'MusicVideo',
+    'LyricsVideo',
+    'Live',
+    'BehindTheScenes',
+    'Interview',
+    'Advert',
+    'PhotoGallery',
+    'Documentary',
+    'Other'
+);
 
 -- DropForeignKey
-ALTER TABLE "tracks" DROP CONSTRAINT "tracks_songId_fkey";
+ALTER TABLE "tracks"
+    DROP CONSTRAINT "tracks_songId_fkey";
 
 -- AlterTable
-ALTER TABLE "tracks" ADD COLUMN     "videoId" INTEGER,
-ALTER COLUMN "songId" DROP NOT NULL;
+ALTER TABLE "tracks"
+    ADD COLUMN "videoId" INTEGER,
+    ALTER COLUMN "songId" DROP NOT NULL;
 
 -- CreateTable
-CREATE TABLE "Video" (
-    "id" SERIAL NOT NULL,
-    "slug" TEXT NOT NULL,
-    "nameSlug" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "artistId" INTEGER NOT NULL,
-    "songId" INTEGER,
-    "groupId" INTEGER,
+CREATE TABLE "videos" (
+    "id" serial NOT NULL,
+    "slug" text NOT NULL,
+    "nameSlug" text NOT NULL,
+    "name" text NOT NULL,
+    "artistId" integer NOT NULL,
+    "songId" integer,
+    "groupId" integer,
     "type" "video-types" NOT NULL,
-
-    CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "videos_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Video_slug_key" ON "Video"("slug");
+CREATE UNIQUE INDEX "videos_slug_key" ON "videos" ("slug");
 
 -- AddForeignKey
-ALTER TABLE "Video" ADD CONSTRAINT "Video_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "artists"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "videos"
+    ADD CONSTRAINT "videos_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "artists" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Video" ADD CONSTRAINT "Video_songId_fkey" FOREIGN KEY ("songId") REFERENCES "songs"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "videos"
+    ADD CONSTRAINT "videos_songId_fkey" FOREIGN KEY ("songId") REFERENCES "songs" ("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Video" ADD CONSTRAINT "Video_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "song_groups"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "videos"
+    ADD CONSTRAINT "videos_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "song_groups" ("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "tracks" ADD CONSTRAINT "tracks_songId_fkey" FOREIGN KEY ("songId") REFERENCES "songs"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "tracks"
+    ADD CONSTRAINT "tracks_songId_fkey" FOREIGN KEY ("songId") REFERENCES "songs" ("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "tracks" ADD CONSTRAINT "tracks_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "tracks"
+    ADD CONSTRAINT "tracks_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "videos" ("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
