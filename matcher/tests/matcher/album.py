c902e8cf92828db4a4829234aeaf30758d482952
Arthur Jamet <arthur.jamet@gmail.com>
2025-03-08 11:39:41 +0000

    Matcher: Use classes instead of tuples for match results

--- matcher/tests/matcher/album.py
+++ matcher/tests/matcher/album.py
@@ -12,155 +12,149 @@ class TestMatchAlbum(unittest.TestCase):
         return MatcherTestUtils.setup_context()
 
     def test_get_album(self):
-        [matches, date, type, genres] = match_album(
-            1, "Confessions on a Dancefloor", "Madonna", AlbumType.STUDIO
-        )
+        res = match_album(1, "Confessions on a Dancefloor", "Madonna", AlbumType.STUDIO)
         # Type
-        self.assertIsNone(type)
+        self.assertIsNone(res.album_type)
         # Genres
-        self.assertEqual(len(genres), 7)
-        self.assertIn("Pop", genres)
-        self.assertIn("Dance-Pop", genres)
-        self.assertIn("Synth-Pop", genres)
-        self.assertIn("Disco", genres)
-        self.assertIn("House", genres)
-        self.assertIn("Electronic", genres)
-        self.assertIn("Euro House", genres)
+        self.assertEqual(len(res.genres), 7)
+        self.assertIn("Pop", res.genres)
+        self.assertIn("Dance-Pop", res.genres)
+        self.assertIn("Synth-Pop", res.genres)
+        self.assertIn("Disco", res.genres)
+        self.assertIn("House", res.genres)
+        self.assertIn("Electronic", res.genres)
+        self.assertIn("Euro House", res.genres)
         # Rating
-        self.assertIsNotNone(matches.rating)
-        self.assertEqual(matches.rating, 70)
+        self.assertIsNotNone(res.metadata.rating)
+        self.assertEqual(res.metadata.rating, 70)
         # Release date
-        self.assertIsNotNone(date)
-        self.assertEqual(date, datetime.date(2005, 11, 11))
+        self.assertIsNotNone(res.release_date)
+        self.assertEqual(res.release_date, datetime.date(2005, 11, 11))
         # Matches
-        self.assertIsNotNone(matches)
-        self.assertEqual(matches.album_id, 1)
+        self.assertIsNotNone(res.metadata)
+        self.assertEqual(res.metadata.album_id, 1)
         ##Description
-        self.assertIsNotNone(matches.description)
-        self.assertTrue("Confessions" in str(matches.description))
-        self.assertTrue("2005" in str(matches.description))
+        self.assertIsNotNone(res.metadata.description)
+        self.assertTrue("Confessions" in str(res.metadata.description))
+        self.assertTrue("2005" in str(res.metadata.description))
         ##Sources
-        self.assertGreaterEqual(len(matches.sources), 5)
+        self.assertGreaterEqual(len(res.metadata.sources), 5)
         ### Wikipedia
-        [wiki] = [p for p in matches.sources if "wiki" in p.url]
+        [wiki] = [p for p in res.metadata.sources if "wiki" in p.url]
         self.assertEqual(
             wiki.url, "https://en.wikipedia.org/wiki/Confessions on a Dance Floor"
         )
         ### Metacritic
-        [meta] = [p for p in matches.sources if "metacritic" in p.url]
+        [meta] = [p for p in res.metadata.sources if "metacritic" in p.url]
         self.assertEqual(
             meta.url,
             "https://www.metacritic.com/music/confessions-on-a-dance-floor/madonna",
         )
         ### Discogs
-        [discogs] = [p for p in matches.sources if "discogs" in p.url]
+        [discogs] = [p for p in res.metadata.sources if "discogs" in p.url]
         self.assertEqual(discogs.url, "https://www.discogs.com/master/34205")
         ### Genius
         if not MatcherTestUtils.is_ci():
-            [genius] = [p for p in matches.sources if "genius" in p.url]
+            [genius] = [p for p in res.metadata.sources if "genius" in p.url]
             self.assertEqual(
                 genius.url,
                 "https://genius.com/albums/Madonna/Confessions-on-a-dance-floor",
             )
         ### Musicbrainz
-        [mb] = [p for p in matches.sources if "musicbrainz" in p.url]
+        [mb] = [p for p in res.metadata.sources if "musicbrainz" in p.url]
         self.assertEqual(
             mb.url,
             "https://musicbrainz.org/release-group/122677be-e664-362d-95eb-be3ae126ec03",
         )
         ### Allmusic
-        [allmusic] = [p for p in matches.sources if "allmusic" in p.url]
+        [allmusic] = [p for p in res.metadata.sources if "allmusic" in p.url]
         self.assertEqual(allmusic.url, "https://www.allmusic.com/album/mw0000356345")
 
     def test_get_album2(self):
-        [matches, date, type, genres] = match_album(
+        res = match_album(
             1, "The Tortured Poets Department", "Taylor Swift", AlbumType.STUDIO
         )
         # Type
-        self.assertIsNone(type)
+        self.assertIsNone(res.album_type)
         # Rating
-        self.assertIsNotNone(matches.rating)
-        self.assertEqual(matches.rating, 60)
+        self.assertIsNotNone(res.metadata.rating)
+        self.assertEqual(res.metadata.rating, 60)
         # Release date
-        self.assertIsNotNone(date)
-        self.assertEqual(date, datetime.date(2024, 4, 19))
+        self.assertIsNotNone(res.release_date)
+        self.assertEqual(res.release_date, datetime.date(2024, 4, 19))
         # Matches
-        self.assertIsNotNone(matches)
-        self.assertEqual(matches.album_id, 1)
+        self.assertIsNotNone(res.metadata)
+        self.assertEqual(res.metadata.album_id, 1)
         # ##Description
-        self.assertIsNotNone(matches.description)
-        self.assertTrue("eleventh studio album" in str(matches.description))
+        self.assertIsNotNone(res.metadata.description)
+        self.assertTrue("eleventh studio album" in str(res.metadata.description))
         ### Metacritic
-        [meta] = [p for p in matches.sources if "metacritic" in p.url]
+        [meta] = [p for p in res.metadata.sources if "metacritic" in p.url]
         self.assertEqual(
             meta.url,
             "https://www.metacritic.com/music/the-tortured-poets-department/taylor-swift",
         )
         ### Wikipedia
-        [wiki] = [p for p in matches.sources if "wiki" in p.url]
+        [wiki] = [p for p in res.metadata.sources if "wiki" in p.url]
         self.assertEqual(
             wiki.url, "https://en.wikipedia.org/wiki/The Tortured Poets Department"
         )
         ### Discogs
-        [discogs] = [p for p in matches.sources if "discogs" in p.url]
+        [discogs] = [p for p in res.metadata.sources if "discogs" in p.url]
         self.assertEqual(discogs.url, "https://www.discogs.com/master/3461018")
         ### Genius
-        [genius] = [p for p in matches.sources if "genius" in p.url]
+        [genius] = [p for p in res.metadata.sources if "genius" in p.url]
         self.assertEqual(
             genius.url,
             "https://genius.com/albums/Taylor-swift/The-tortured-poets-department",
         )
         ### Musicbrainz
-        [mb] = [p for p in matches.sources if "musicbrainz" in p.url]
+        [mb] = [p for p in res.metadata.sources if "musicbrainz" in p.url]
         self.assertEqual(
             mb.url,
             "https://musicbrainz.org/release-group/71c3eed6-466c-4aef-ad12-65a36d19467a",
         )
         ### Allmusic
-        [allmusic] = [p for p in matches.sources if "allmusic" in p.url]
+        [allmusic] = [p for p in res.metadata.sources if "allmusic" in p.url]
         self.assertEqual(allmusic.url, "https://www.allmusic.com/album/mw0004210541")
 
     def test_get_album_no_rating(self):
-        [matches, date, type, genres] = match_album(
-            1, "Aéromusical", "Superbus", AlbumType.STUDIO
-        )
+        res = match_album(1, "Aéromusical", "Superbus", AlbumType.STUDIO)
         # Rating
-        self.assertIsNone(matches.rating)
+        self.assertIsNone(res.metadata.rating)
         # Release date
-        self.assertIsNotNone(date)
-        self.assertEqual(date.month, 3)
+        self.assertIsNotNone(res.release_date)
+        self.assertEqual(res.release_date.month, 3)
         # Matches
-        self.assertIsNotNone(matches)
+        self.assertIsNotNone(res.metadata)
         ##Description
-        self.assertIsNotNone(matches.description)
-        self.assertTrue("Aéromusical" in matches.description)
+        self.assertIsNotNone(res.metadata.description)
+        self.assertTrue("Aéromusical" in res.metadata.description)
         ### Wikipedia
-        [wiki] = [p for p in matches.sources if "wiki" in p.url]
+        [wiki] = [p for p in res.metadata.sources if "wiki" in p.url]
         self.assertEqual(wiki.url, "https://en.wikipedia.org/wiki/Aéromusical")
         ### Discogs
-        [discogs] = [p for p in matches.sources if "discogs" in p.url]
+        [discogs] = [p for p in res.metadata.sources if "discogs" in p.url]
         self.assertEqual(discogs.url, "https://www.discogs.com/master/426003")
         ### Genius
-        [genius] = [p for p in matches.sources if "genius" in p.url]
+        [genius] = [p for p in res.metadata.sources if "genius" in p.url]
         self.assertEqual(genius.url, "https://genius.com/albums/Superbus/Aeromusical")
         ### Musicbrainz
-        [mb] = [p for p in matches.sources if "musicbrainz" in p.url]
+        [mb] = [p for p in res.metadata.sources if "musicbrainz" in p.url]
         self.assertEqual(
             mb.url,
             "https://musicbrainz.org/release-group/5666a1be-2156-3e94-847b-7bd5cc8b5b93",
         )
         ### Allmusic
-        [allmusic] = [p for p in matches.sources if "allmusic" in p.url]
+        [allmusic] = [p for p in res.metadata.sources if "allmusic" in p.url]
         self.assertEqual(allmusic.url, "https://www.allmusic.com/album/mw0000770491")
 
     def test_get_album_ignore_genres(self):
         # Setup
         context = Context.get()
         context.settings.push_genres = False
-        [matches, date, type, genres] = match_album(
-            1, "Confessions on a Dancefloor", "Madonna", AlbumType.STUDIO
-        )
+        res = match_album(1, "Confessions on a Dancefloor", "Madonna", AlbumType.STUDIO)
         # Teardown
         context.settings.push_genres = True
         # Genres
-        self.assertEqual(len(genres), 0)
+        self.assertEqual(len(res.genres), 0)
--- matcher/tests/matcher/artist.py
+++ matcher/tests/matcher/artist.py
@@ -10,101 +10,101 @@ class TestMatchArtist(unittest.TestCase):
         return MatcherTestUtils.setup_context()
 
     def test_get_artist(self):
-        [matches, imageUrl] = match_artist(1, "Madonna")
+        res = match_artist(1, "Madonna")
         # Illustration
-        self.assertIsNotNone(imageUrl)
-        imageUrl = str(imageUrl)
-        self.assertTrue("discogs" in imageUrl)
+        self.assertIsNotNone(res.illustration_url)
+        res.illustration_url = str(res.illustration_url)
+        self.assertTrue("discogs" in res.illustration_url)
         # Matches
-        self.assertIsNotNone(matches)
-        self.assertEqual(matches.artist_id, 1)
+        self.assertIsNotNone(res.metadata)
+        self.assertEqual(res.metadata.artist_id, 1)
         ##Description
-        self.assertIsNotNone(matches.description)
-        self.assertTrue("Madonna" in str(matches.description))
+        self.assertIsNotNone(res.metadata.description)
+        self.assertTrue("Madonna" in str(res.metadata.description))
         ##Sources
-        self.assertEqual(len(matches.sources), 6)
+        self.assertEqual(len(res.metadata.sources), 6)
         ### Wikipedia
-        [wiki] = [p for p in matches.sources if "wiki" in p.url]
+        [wiki] = [p for p in res.metadata.sources if "wiki" in p.url]
         self.assertEqual(wiki.url, "https://en.wikipedia.org/wiki/Madonna")
         ### Metacritic
-        [meta] = [p for p in matches.sources if "metacritic" in p.url]
+        [meta] = [p for p in res.metadata.sources if "metacritic" in p.url]
         self.assertEqual(meta.url, "https://www.metacritic.com/person/madonna")
         ### Discogs
-        [discogs] = [p for p in matches.sources if "discogs" in p.url]
+        [discogs] = [p for p in res.metadata.sources if "discogs" in p.url]
         self.assertEqual(discogs.url, "https://www.discogs.com/artist/3424607")
         ### Genius
-        [genius] = [p for p in matches.sources if "genius" in p.url]
+        [genius] = [p for p in res.metadata.sources if "genius" in p.url]
         self.assertEqual(genius.url, "https://genius.com/artists/Madonna")
         ### Musicbrainz
-        [mb] = [p for p in matches.sources if "musicbrainz" in p.url]
+        [mb] = [p for p in res.metadata.sources if "musicbrainz" in p.url]
         self.assertEqual(
             mb.url,
             "https://musicbrainz.org/artist/79239441-bfd5-4981-a70c-55c3f15c1287",
         )
         ### Allmusic
-        [allmusic] = [p for p in matches.sources if "allmusic" in p.url]
+        [allmusic] = [p for p in res.metadata.sources if "allmusic" in p.url]
         self.assertEqual(allmusic.url, "https://www.allmusic.com/artist/mn0000237205")
 
     def test_get_artist_with_special_char(self):
-        [matches, imageUrl] = match_artist(1, "P!nk")
+        res = match_artist(1, "P!nk")
 
         # Illustration
-        self.assertIsNotNone(imageUrl)
-        imageUrl = str(imageUrl)
-        self.assertTrue("discogs" in imageUrl)
+        self.assertIsNotNone(res.illustration_url)
+        res.illustration_url = str(res.illustration_url)
+        self.assertTrue("discogs" in res.illustration_url)
         # Matches
-        self.assertIsNotNone(matches)
-        self.assertEqual(matches.artist_id, 1)
+        self.assertIsNotNone(res.metadata)
+        self.assertEqual(res.metadata.artist_id, 1)
         ##Description
-        self.assertIsNotNone(matches.description)
-        self.assertTrue("American singer" in str(matches.description))
+        self.assertIsNotNone(res.metadata.description)
+        self.assertTrue("American singer" in str(res.metadata.description))
         ##Sources
-        self.assertEqual(len(matches.sources), 6)
+        self.assertEqual(len(res.metadata.sources), 6)
         ### Wikipedia
-        [wiki] = [p for p in matches.sources if "wiki" in p.url]
+        [wiki] = [p for p in res.metadata.sources if "wiki" in p.url]
         self.assertEqual(wiki.url, "https://en.wikipedia.org/wiki/Pink (singer)")
         ### Metacritic
-        [meta] = [p for p in matches.sources if "metacritic" in p.url]
+        [meta] = [p for p in res.metadata.sources if "metacritic" in p.url]
         self.assertEqual(meta.url, "https://www.metacritic.com/person/p!nk")
         ### Discogs
-        [discogs] = [p for p in matches.sources if "discogs" in p.url]
+        [discogs] = [p for p in res.metadata.sources if "discogs" in p.url]
         self.assertEqual(discogs.url, "https://www.discogs.com/artist/1525636")
         ### Genius
-        [genius] = [p for p in matches.sources if "genius" in p.url]
+        [genius] = [p for p in res.metadata.sources if "genius" in p.url]
         self.assertEqual(genius.url, "https://genius.com/artists/P-nk")
         ### Musicbrainz
-        [mb] = [p for p in matches.sources if "musicbrainz" in p.url]
+        [mb] = [p for p in res.metadata.sources if "musicbrainz" in p.url]
         self.assertEqual(
             mb.url,
             "https://musicbrainz.org/artist/f4d5cc07-3bc9-4836-9b15-88a08359bc63",
         )
         ### Allmusic
-        [allmusic] = [p for p in matches.sources if "allmusic" in p.url]
+        [allmusic] = [p for p in res.metadata.sources if "allmusic" in p.url]
         self.assertEqual(allmusic.url, "https://www.allmusic.com/artist/mn0001878899")
 
     def test_get_artist_with_placeholder_image(self):
-        [matches, imageUrl] = match_artist(1, "Peplab")
+        res = match_artist(1, "Peplab")
         # Illustration
-        self.assertIsNotNone(imageUrl)
-        imageUrl = str(imageUrl)
+        self.assertIsNotNone(res.illustration_url)
+        res.illustration_url = str(res.illustration_url)
         # We want to get Discogs' image, not genius' as it's a placeholder
-        self.assertFalse("genius" in imageUrl)
-        self.assertTrue("discogs" in imageUrl)
+        self.assertFalse("genius" in res.illustration_url)
+        self.assertTrue("discogs" in res.illustration_url)
         # Matches
-        self.assertIsNotNone(matches)
-        self.assertEqual(matches.artist_id, 1)
+        self.assertIsNotNone(res.metadata)
+        self.assertEqual(res.metadata.artist_id, 1)
         # ##Sources
-        self.assertTrue(len(matches.sources) >= 2)
+        self.assertTrue(len(res.metadata.sources) >= 2)
         ### Discogs
-        [discogs] = [p for p in matches.sources if "discogs" in p.url]
+        [discogs] = [p for p in res.metadata.sources if "discogs" in p.url]
         self.assertEqual(discogs.url, "https://www.discogs.com/artist/9663")
         ### Genius
         ### CI fails here (thanks cloudflare) because we do a search for this provider
         if not MatcherTestUtils.is_ci():
-            [genius] = [p for p in matches.sources if "genius" in p.url]
+            [genius] = [p for p in res.metadata.sources if "genius" in p.url]
             self.assertEqual(genius.url, "https://genius.com/artists/Peplab")
         ### Musicbrainz
-        [mb] = [p for p in matches.sources if "musicbrainz" in p.url]
+        [mb] = [p for p in res.metadata.sources if "musicbrainz" in p.url]
         self.assertEqual(
             mb.url,
             "https://musicbrainz.org/artist/ccccf1ba-c503-48d7-8c8c-f7c80c3347d4",
