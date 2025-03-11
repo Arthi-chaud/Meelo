df3646a5b1232ea106c56ba2a4a1027c7ad3d33e
Matthieu LAURENT <matthieu.laurent69@proton.me>
2025-03-11 22:57:18 +0100

    Use the embeded video illustration as thumbnail if one exists. This falls back to the existing implementation of using a frame from the video if no embeded illustration is present.

--- scanner/internal/tasks/illustration.go
+++ scanner/internal/tasks/illustration.go
@@ -1,6 +1,7 @@
 package tasks
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"os"
@@ -9,9 +10,26 @@ import (
 	"github.com/Arthi-chaud/Meelo/scanner/internal/api"
 	"github.com/Arthi-chaud/Meelo/scanner/internal/config"
 	"github.com/Arthi-chaud/Meelo/scanner/internal/illustration"
+	"gopkg.in/vansante/go-ffprobe.v2"
 )
 
 func SaveThumbnail(t ThumbnailTask, c config.Config) error {
+	// If the video has an embedded illustration, use that as the thumbnail
+	ctx, cancelFn := context.WithCancel(context.Background())
+	defer cancelFn()
+	
+	probeData, err := ffprobe.ProbeURL(ctx, t.FilePath)
+	if err == nil {
+		streamIndex := illustration.GetEmbeddedIllustrationStreamIndex(*probeData)
+		if streamIndex >= 0 {
+			thumbnailbytes, err := illustration.ExtractEmbeddedIllustration(t.FilePath, streamIndex)
+			if err == nil {
+				return api.PostIllustration(c, t.TrackId, api.Thumbnail, thumbnailbytes)
+			}
+		}
+	}
+	
+	// If there is no embedded illustration, we instead extract a frame from the video
 	thumbnailPosition := int64(t.TrackDuration / 2)
 	if t.TrackDuration == 0 {
 		t.TrackDuration = 5 // this is abitrary. If the scan os path only, we do not get the duration.
