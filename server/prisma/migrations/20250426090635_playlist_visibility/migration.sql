d22ac5dbae04b19a1d43e8ab907f9dbe31008966
Arthur Jamet <arthur.jamet@gmail.com>
2025-04-27 19:43:44 +0100

    Server: Fix Playlist migration

--- server/prisma/migrations/20250426090635_playlist_visibility/migration.sql
+++ server/prisma/migrations/20250426090635_playlist_visibility/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE "playlists"
     ADD COLUMN "allowChanges" boolean NOT NULL DEFAULT FALSE,
     ADD COLUMN "isPublic" boolean NOT NULL DEFAULT TRUE,
-    ADD COLUMN "ownerId" integer NOT NULL;
+    ADD COLUMN "ownerId" integer;
 
 UPDATE
     "playlists"
@@ -25,5 +25,6 @@ ALTER TABLE "playlists"
 
 ALTER TABLE "playlists"
     ALTER COLUMN "allowChanges" DROP DEFAULT,
-    ALTER COLUMN "isPublic" DROP DEFAULT;
+    ALTER COLUMN "isPublic" DROP DEFAULT,
+    ALTER COLUMN "ownerId" SET NOT NULL;
 
