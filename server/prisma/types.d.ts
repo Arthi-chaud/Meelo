bbb75aaf8a3b0db3a6f3cb3175c380612b05621a
Arthur Jamet <arthur.jamet@gmail.com>
2025-05-19 11:19:39 +0100

    Prisma: Add Scrobbler Table

--- server/prisma/types.d.ts
+++ server/prisma/types.d.ts
@@ -1,5 +1,9 @@
 declare global {
 	namespace PrismaJson {
 		type SyncedLyrics = [{ timestamp: number; content: string }];
+		type LastFMData = { sessionToken: string };
+		type ListenBrainzData = { userToken: string };
 	}
 }
+
+export {};
