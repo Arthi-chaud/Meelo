7b9bca4bb2c282b4ba6b2b04a784e048c8140ae8
Arthur Jamet <arthur.jamet@gmail.com>
2025-06-06 18:16:43 +0100

    Front: Mobile: Init

---
+++ front/apps/mobile/app/(tabs)/index.tsx
@@ -0,0 +1,54 @@
+/*
+ * Meelo is a music server and application to enjoy your personal music files anywhere, anytime you want.
+ * Copyright (C) 2023
+ *
+ * Meelo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Meelo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// import { Text } from "primitives/src/text";
+import { useTranslation } from "react-i18next";
+import { Button, Text, View } from "react-native";
+import { useColorScheme } from "~/hooks/color-scheme";
+
+export default function Root() {
+	const { t } = useTranslation();
+	const [colorScheme, setColorScheme] = useColorScheme();
+
+	return (
+		<View>
+			<Button
+				title="Toggle"
+				onPress={() => {
+					setColorScheme(colorScheme === "light" ? "dark" : "light");
+				}}
+			/>
+			{(
+				[
+					"h1",
+					"h2",
+					"h3",
+					"h4",
+					"h5",
+					"h6",
+					"body",
+					"subtitle",
+				] as const
+			).map((s) => (
+				<Text key={s}>{s}</Text>
+			))}
+			<Text>{t("browsing.sections.musicVideos")}</Text>
+			<Button title="AA" />
+		</View>
+	);
+}
---
+++ front/apps/mobile/app/_layout.tsx
@@ -0,0 +1,78 @@
+/*
+ * Meelo is a music server and application to enjoy your personal music files anywhere, anytime you want.
+ * Copyright (C) 2023
+ *
+ * Meelo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Meelo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {
+	Rubik_300Light,
+	Rubik_400Regular,
+	Rubik_500Medium,
+	Rubik_600SemiBold,
+	Rubik_700Bold,
+	Rubik_800ExtraBold,
+	Rubik_900Black,
+} from "@expo-google-fonts/rubik";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import i18next from "i18next";
+import { useEffect } from "react";
+import { initReactI18next } from "react-i18next";
+import "intl-pluralrules";
+import resources from "../../../translations";
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+	duration: 1000,
+	fade: true,
+});
+
+export default function RootLayout() {
+	const [loaded, error] = useFonts({
+		Rubik_300Light,
+		Rubik_400Regular,
+		Rubik_500Medium,
+		Rubik_600SemiBold,
+		Rubik_700Bold,
+		Rubik_800ExtraBold,
+		Rubik_900Black,
+	});
+
+	useEffect(() => {
+		i18next.use(initReactI18next).init({
+			interpolation: {
+				escapeValue: false,
+			},
+			returnEmptyString: false,
+			fallbackLng: "en",
+			lng: "en", // TODO Choose using local storage
+			resources,
+		});
+	});
+
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded) {
+		return null;
+	}
+
+	return <Stack />;
+}
