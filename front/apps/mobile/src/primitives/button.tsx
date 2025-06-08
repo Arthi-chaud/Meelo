c99893ec31d27b7457ad7c05607e71502ab70e9c
Arthur Jamet <arthur.jamet@gmail.com>
2025-06-08 19:03:37 +0100

    Front: Mobile: Button: Add leading icon

--- front/apps/mobile/src/primitives/button.tsx
+++ front/apps/mobile/src/primitives/button.tsx
@@ -16,19 +16,27 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import type { Icon } from "@/ui/icons";
+import type { IconProps } from "iconsax-react-nativejs";
 import { useCallback } from "react";
 import {
 	Pressable,
 	type ButtonProps as RNButtonProps,
 	Text,
 	type TextProps,
+	View,
 } from "react-native";
 import Animated, {
 	useAnimatedStyle,
 	useSharedValue,
 	withSpring,
 } from "react-native-reanimated";
-import { StyleSheet, type UnistylesVariants } from "react-native-unistyles";
+import type { SvgProps } from "react-native-svg";
+import {
+	StyleSheet,
+	type UnistylesVariants,
+	withUnistyles,
+} from "react-native-unistyles";
 
 // TODO Make the button animation cooler/dynamic
 
@@ -37,10 +45,20 @@ type ButtonProps = UnistylesVariants<typeof styles> & {
 	buttonProps?: RNButtonProps;
 	onPress: RNButtonProps["onPress"];
 	title: RNButtonProps["title"];
-};
+} & (
+		| {
+				leadingIconStyle?: Omit<IconProps, keyof SvgProps>;
+				leadingIcon: Icon;
+		  }
+		| { leadingIconStyle?: never; leadingIcon?: never }
+	);
 
+// Note: For the 'fitContent' variant to work, you may need to tweak the parent so that:
+// - The button is in a row
+// - The button is align to the center (?)
 export const Button = (props: ButtonProps) => {
 	const scale = useSharedValue<number>(1);
+	const Leading = withUnistyles(props.leadingIcon ?? View);
 	const handlePress = useCallback(() => {
 		scale.value = 0.97;
 	}, []);
@@ -63,6 +81,12 @@ export const Button = (props: ButtonProps) => {
 				onPressIn={handlePress}
 				onPressOut={handleRelease}
 			>
+				{props.leadingIcon ? (
+					<Leading
+						{...(props.leadingIconStyle as any)}
+						style={[styles.label as any, styles.icon]}
+					/>
+				) : undefined}
 				<Text style={styles.label}>{props.title}</Text>
 			</Pressable>
 		</Animated.View>
@@ -85,6 +109,8 @@ const styles = StyleSheet.create((theme) => ({
 			width: {
 				fill: {
 					width: "100%",
+					display: "flex",
+					justifyContent: "center",
 				},
 				fitContent: {
 					display: "flex",
@@ -97,8 +123,14 @@ const styles = StyleSheet.create((theme) => ({
 	// Separate this to make sure the entire surface is clickable
 	content: {
 		paddingVertical: theme.gap(1),
-		paddingHorizontal: theme.gap(2.5),
+		paddingHorizontal: theme.gap(2),
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		gap: theme.gap(1.5),
 	},
+	icon: { marginBottom: 2 },
 	label: {
 		...theme.fontStyles.medium,
 		fontSize: theme.fontSize.rem(1.5),
