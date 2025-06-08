6ef8988a699311c61821920f6ab6279226259774
Arthur Jamet <arthur.jamet@gmail.com>
2025-06-08 19:02:55 +0100

    Front: Cast icons type to make them typecheck w/ react native

--- front/packages/ui/src/icons.tsx
+++ front/packages/ui/src/icons.tsx
@@ -18,101 +18,102 @@
 
 import * as Iconsax from "iconsax-react";
 
+export type Icon = React.FunctionComponent<IconProps>;
 export type IconProps = Iconsax.IconProps;
 
-export const TranscodeIcon = Iconsax.Polyswarm;
-export const PlaylistIcon = Iconsax.MusicFilter;
-export const LibraryIcon = Iconsax.MusicLibrary2;
-export const ReleaseIcon = Iconsax.Cd;
-export const AlbumIcon = Iconsax.MusicDashboard;
-export const ArtistIcon = Iconsax.Profile2User;
-export const SongIcon = Iconsax.Musicnote;
-export const GenreIcon = Iconsax.Speaker;
-export const RadioIcon = Iconsax.Radio;
-export const PlayerIcon = Iconsax.PlayCircle;
-export const CompilationIcon = Iconsax.Category;
-export const TrackIcon = Iconsax.Musicnote;
-export const LyricsIcon = Iconsax.MessageText;
-export const VideoIcon = Iconsax.VideoSquare;
-export const RelatedTracksIcon = Iconsax.Category;
-export const CloseIcon = Iconsax.CloseCircle;
-export const EditIcon = Iconsax.Edit2;
-export const LogoutIcon = Iconsax.Logout;
-export const DarkMode = Iconsax.Sun1;
-export const ProviderIcon = Iconsax.Global;
-export const LightMode = (props: IconProps) => (
+export const TranscodeIcon: Icon = Iconsax.Polyswarm;
+export const PlaylistIcon: Icon = Iconsax.MusicFilter;
+export const LibraryIcon: Icon = Iconsax.MusicLibrary2;
+export const ReleaseIcon: Icon = Iconsax.Cd;
+export const AlbumIcon: Icon = Iconsax.MusicDashboard;
+export const ArtistIcon: Icon = Iconsax.Profile2User;
+export const SongIcon: Icon = Iconsax.Musicnote;
+export const GenreIcon: Icon = Iconsax.Speaker;
+export const RadioIcon: Icon = Iconsax.Radio;
+export const PlayerIcon: Icon = Iconsax.PlayCircle;
+export const CompilationIcon: Icon = Iconsax.Category;
+export const TrackIcon: Icon = Iconsax.Musicnote;
+export const LyricsIcon: Icon = Iconsax.MessageText;
+export const VideoIcon: Icon = Iconsax.VideoSquare;
+export const RelatedTracksIcon: Icon = Iconsax.Category;
+export const CloseIcon: Icon = Iconsax.CloseCircle;
+export const EditIcon: Icon = Iconsax.Edit2;
+export const LogoutIcon: Icon = Iconsax.Logout;
+export const DarkMode: Icon = Iconsax.Sun1;
+export const ProviderIcon: Icon = Iconsax.Global;
+export const LightMode: Icon = (props: IconProps) => (
 	<Iconsax.Sun1 variant="Bold" {...props} />
 );
-export const AutoMode = Iconsax.Autobrightness;
-export const TranslateIcon = Iconsax.Translate;
-export const SettingsIcon = Iconsax.Setting2;
-export const SearchIcon = Iconsax.SearchNormal1;
-export const UpdateIllustrationIcon = Iconsax.Image;
-export const BurgerIcon = Iconsax.HambergerMenu;
-export const AddIcon = Iconsax.AddCircle;
-export const DeleteIcon = Iconsax.Trash;
-export const ShuffleIcon = Iconsax.Shuffle;
-export const InfoIcon = (props: IconProps) => (
+export const AutoMode: Icon = Iconsax.Autobrightness;
+export const TranslateIcon: Icon = Iconsax.Translate;
+export const SettingsIcon: Icon = Iconsax.Setting2;
+export const SearchIcon: Icon = Iconsax.SearchNormal1;
+export const UpdateIllustrationIcon: Icon = Iconsax.Image;
+export const BurgerIcon: Icon = Iconsax.HambergerMenu;
+export const AddIcon: Icon = Iconsax.AddCircle;
+export const DeleteIcon: Icon = Iconsax.Trash;
+export const ShuffleIcon: Icon = Iconsax.Shuffle;
+export const InfoIcon: Icon = (props: IconProps) => (
 	<Iconsax.InfoCircle style={{ rotate: "180deg" }} {...props} />
 );
-export const AddToPlaylistIcon = Iconsax.MusicSquareAdd;
-export const AddItemToPlaylistIcon = Iconsax.MusicSquareAdd;
-export const PlayNextIcon = Iconsax.MusicSquareAdd;
-export const PlayAfterIcon = Iconsax.MusicSquareAdd;
-export const HomeIcon = Iconsax.Home2;
-export const ExpandLessIcon = Iconsax.ArrowUp2;
-export const ExpandMoreIcon = Iconsax.ArrowDown2;
-export const ArchiveIcon = Iconsax.ArchiveBox;
-export const DownloadIcon = Iconsax.DirectInbox;
-export const ScanIcon = Iconsax.ArrowRotateRight;
-export const CleanIcon = Iconsax.Broom;
-export const MetadataRefreshIcon = Iconsax.Convertshape2;
-export const MoreIcon = Iconsax.ArrowRight2;
-export const MasterIcon = Iconsax.Star1;
-export const StarIcon = Iconsax.Star1;
-export const MovingStarIcon = Iconsax.Star;
-export const ShareIcon = Iconsax.Share;
-export const CheckIcon = Iconsax.TickCircle;
-export const ContextualMenuIcon = (props: IconProps) => (
+export const AddToPlaylistIcon: Icon = Iconsax.MusicSquareAdd;
+export const AddItemToPlaylistIcon: Icon = Iconsax.MusicSquareAdd;
+export const PlayNextIcon: Icon = Iconsax.MusicSquareAdd;
+export const PlayAfterIcon: Icon = Iconsax.MusicSquareAdd;
+export const HomeIcon: Icon = Iconsax.Home2;
+export const ExpandLessIcon: Icon = Iconsax.ArrowUp2;
+export const ExpandMoreIcon: Icon = Iconsax.ArrowDown2;
+export const ArchiveIcon: Icon = Iconsax.ArchiveBox;
+export const DownloadIcon: Icon = Iconsax.DirectInbox;
+export const ScanIcon: Icon = Iconsax.ArrowRotateRight;
+export const CleanIcon: Icon = Iconsax.Broom;
+export const MetadataRefreshIcon: Icon = Iconsax.Convertshape2;
+export const MoreIcon: Icon = Iconsax.ArrowRight2;
+export const MasterIcon: Icon = Iconsax.Star1;
+export const StarIcon: Icon = Iconsax.Star1;
+export const MovingStarIcon: Icon = Iconsax.Star;
+export const ShareIcon: Icon = Iconsax.Share;
+export const CheckIcon: Icon = Iconsax.TickCircle;
+export const ContextualMenuIcon: Icon = (props: IconProps) => (
 	<Iconsax.More
 		{...props}
 		style={{ rotate: "90deg", ...props.style }}
 		size={18}
 	/>
 );
-export const UpgradeIcon = Iconsax.Star;
-export const GridIcon = Iconsax.Category;
-export const ListIcon = Iconsax.TextalignJustifycenter;
-export const AscIcon = Iconsax.ArrowUp;
-export const DescIcon = Iconsax.ArrowDown;
-export const GoBackTopIcon = Iconsax.ArrowUp;
-export const DragHandleIcon = Iconsax.HambergerMenu;
+export const UpgradeIcon: Icon = Iconsax.Star;
+export const GridIcon: Icon = Iconsax.Category;
+export const ListIcon: Icon = Iconsax.TextalignJustifycenter;
+export const AscIcon: Icon = Iconsax.ArrowUp;
+export const DescIcon: Icon = Iconsax.ArrowDown;
+export const GoBackTopIcon: Icon = Iconsax.ArrowUp;
+export const DragHandleIcon: Icon = Iconsax.HambergerMenu;
 
-export const PlayIcon = Iconsax.ArrowRight3;
-export const PauseIcon = Iconsax.Pause;
-export const RewindIcon = Iconsax.Previous;
-export const ForwardIcon = Iconsax.Next;
-export const FullscreenIcon = Iconsax.Maximize4;
-export const PlusIcon = Iconsax.Add;
-export const MinusIcon = Iconsax.Minus;
-export const DoneIcon = Iconsax.TickSquare;
+export const PlayIcon: Icon = Iconsax.ArrowRight3;
+export const PauseIcon: Icon = Iconsax.Pause;
+export const RewindIcon: Icon = Iconsax.Previous;
+export const ForwardIcon: Icon = Iconsax.Next;
+export const FullscreenIcon: Icon = Iconsax.Maximize4;
+export const PlusIcon: Icon = Iconsax.Add;
+export const MinusIcon: Icon = Iconsax.Minus;
+export const DoneIcon: Icon = Iconsax.TickSquare;
 
-export const EmptyStateIcon = Iconsax.SearchStatus1;
+export const EmptyStateIcon: Icon = Iconsax.SearchStatus1;
 
-export const MergeIcon = Iconsax.Hierarchy;
+export const MergeIcon: Icon = Iconsax.Hierarchy;
 
-export const OpenExternalIcon = Iconsax.ExportCircle;
-export const WarningIcon = Iconsax.Warning2;
-export const BookIcon = Iconsax.Book1;
+export const OpenExternalIcon: Icon = Iconsax.ExportCircle;
+export const WarningIcon: Icon = Iconsax.Warning2;
+export const BookIcon: Icon = Iconsax.Book1;
 // Icons for song type
-export const SongTypeOriginalIcon = Iconsax.Music;
-export const SongTypeRemixIcon = Iconsax.Repeat;
-export const SongTypeLiveIcon = Iconsax.Microphone2;
-export const SongTypeAcousticIcon = Iconsax.VoiceSquare;
-export const SongTypeInstrumentalIcon = Iconsax.Musicnote;
-export const SongTypeEditIcon = Iconsax.Cut;
-export const SongTypeCleanIcon = Iconsax.TickCircle;
-export const SongTypeDemoIcon = Iconsax.ReceiptEdit;
-export const SongTypeAcapellaIcon = Iconsax.VoiceCricle;
-export const SongTypeMedleyIcon = Iconsax.Layer;
-export const SongTypeNonMusicIcon = Iconsax.VolumeSlash;
+export const SongTypeOriginalIcon: Icon = Iconsax.Music;
+export const SongTypeRemixIcon: Icon = Iconsax.Repeat;
+export const SongTypeLiveIcon: Icon = Iconsax.Microphone2;
+export const SongTypeAcousticIcon: Icon = Iconsax.VoiceSquare;
+export const SongTypeInstrumentalIcon: Icon = Iconsax.Musicnote;
+export const SongTypeEditIcon: Icon = Iconsax.Cut;
+export const SongTypeCleanIcon: Icon = Iconsax.TickCircle;
+export const SongTypeDemoIcon: Icon = Iconsax.ReceiptEdit;
+export const SongTypeAcapellaIcon: Icon = Iconsax.VoiceCricle;
+export const SongTypeMedleyIcon: Icon = Iconsax.Layer;
+export const SongTypeNonMusicIcon: Icon = Iconsax.VolumeSlash;
