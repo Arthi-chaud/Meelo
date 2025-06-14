ae0358e4b14658374579f59947c8bcc0c4ed2345
Arthur Jamet <arthur.jamet@gmail.com>
2025-06-14 12:11:45 +0100

    Front: Mobile: Auth Screen Layout

--- front/apps/mobile/src/app/auth.tsx
+++ front/apps/mobile/src/app/auth.tsx
@@ -16,8 +16,79 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Text } from "~/primitives/text";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { View } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
+import { MeeloBanner } from "~/components/meelo_banner";
+import { Button } from "~/primitives/button";
+import { Divider } from "~/primitives/divider";
+import { TextInput } from "~/primitives/text_input";
+
+const styles = StyleSheet.create((theme) => ({
+	root: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		flex: 1,
+		paddingVertical: theme.gap(8),
+		justifyContent: "space-evenly",
+	},
+	banner: {
+		flex: 1,
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "flex-end",
+	},
+	formContainer: {
+		flex: 2,
+		display: "flex",
+		width: "100%",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		gap: theme.gap(3),
+	},
+}));
+
+//TODO Handle empty fields
+//TODO On press, push to API
+//TODO On authed, update atom
 
 export default function AuthenticationScreen() {
-	return <Text>Login Screen</Text>;
+	const { t } = useTranslation();
+	const [formType, setFormType] = useState<"login" | "signup">("login");
+	return (
+		<View style={styles.root}>
+			<MeeloBanner style={styles.banner} />
+			<View style={styles.formContainer}>
+				<TextInput placeholder={t("form.auth.username")} />
+				<TextInput placeholder={t("form.auth.password")} />
+				{formType === "signup" && (
+					<TextInput
+						placeholder={t("form.auth.confirmPasswordField")}
+					/>
+				)}
+				<Button
+					onPress={() => {}}
+					title={t(
+						formType === "login"
+							? "auth.loginButton"
+							: "auth.signupButton",
+					)}
+				/>
+				<Divider h withInsets />
+				<Button
+					variant="outlined"
+					onPress={() => {
+						setFormType(formType === "login" ? "signup" : "login");
+					}}
+					title={t(
+						formType === "login" ? "auth.signup" : "auth.signin",
+					)}
+				/>
+			</View>
+		</View>
+	);
 }
