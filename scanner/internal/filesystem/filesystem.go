be85753ed0122f73b15b1ff8061471fe418d130e
Arthur Jamet <arthur.jamet@gmail.com>
2025-06-20 14:48:46 +0100

    Scanner: Consider '.ignore' files a-la jellyfin

--- scanner/internal/filesystem/filesystem.go
+++ scanner/internal/filesystem/filesystem.go
@@ -5,6 +5,10 @@ import (
 	"path"
 )
 
+const IgnoreFileName = ".ignore"
+
+// Walks through the file system from the given root
+// Does not return content of directories that contain a '.ignore' file
 func GetAllFilesInDirectory(dir string) ([]string, error) {
 	files := []string{}
 	entries, err := os.ReadDir(dir)
@@ -14,6 +18,9 @@ func GetAllFilesInDirectory(dir string) ([]string, error) {
 	}
 
 	for _, e := range entries {
+		if e.Name() == IgnoreFileName {
+			return []string{}, nil
+		}
 		entryPath := path.Join(dir, e.Name())
 		i, err := os.Stat(entryPath)
 		if err != nil {
