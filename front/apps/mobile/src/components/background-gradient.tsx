7b555dd5ef1c92051397865164ce57e87bc5791b
Arthur Jamet <arthur.jamet@gmail.com>
2025-07-08 10:57:09 +0100

    Front: Mobile: background gradient: smooth transition

--- front/apps/mobile/src/components/background-gradient.tsx
+++ front/apps/mobile/src/components/background-gradient.tsx
@@ -2,15 +2,26 @@ import type { InfiniteQuery } from "@/api/query";
 import type Illustration from "@/models/illustration";
 import type { IllustratedResource } from "@/models/illustration";
 import type Resource from "@/models/resource";
-import { LinearGradient } from "expo-linear-gradient";
+import { generateArray } from "@/utils/gen-list";
+import {
+	Canvas,
+	RadialGradient,
+	Rect,
+	type RectProps,
+	vec,
+} from "@shopify/react-native-skia";
 import { useFocusEffect } from "expo-router";
 import { atom, useAtom, useAtomValue } from "jotai";
 import { useEffect, useMemo } from "react";
-import { StyleSheet } from "react-native-unistyles";
+import {
+	useDerivedValue,
+	useSharedValue,
+	withTiming,
+} from "react-native-reanimated";
+import { StyleSheet, withUnistyles } from "react-native-unistyles";
 import { useInfiniteQuery } from "~/api";
 
 //TODO Check it looks good in dark mode
-//TODO Animation on transition
 
 export const keyIllustrationAtom = atom<Illustration | null>(null);
 
@@ -45,31 +56,111 @@ export const useSetKeyIllustrationFromInfiniteQuery = <
 	});
 };
 
+const transitionDuration = 300;
+
 export const BackgroundGradient = () => {
 	const keyIllustration = useAtomValue(keyIllustrationAtom);
-	const sortedColors = useMemo(
-		() => keyIllustration?.colors.sort().reverse(),
-		[keyIllustration],
+	const currentColor1 = useSharedValue("transparent");
+	const currentColor2 = useSharedValue("transparent");
+	const currentColor3 = useSharedValue("transparent");
+	const currentColor4 = useSharedValue("transparent");
+	const currentColor5 = useSharedValue("transparent");
+
+	useEffect(() => {
+		if (keyIllustration) {
+			currentColor1.value = withTiming(keyIllustration.colors[0], {
+				duration: transitionDuration,
+			});
+			currentColor2.value = withTiming(keyIllustration.colors[1], {
+				duration: transitionDuration,
+			});
+			currentColor3.value = withTiming(keyIllustration.colors[2], {
+				duration: transitionDuration,
+			});
+			currentColor4.value = withTiming(keyIllustration.colors[3], {
+				duration: transitionDuration,
+			});
+			currentColor5.value = withTiming(keyIllustration.colors[4], {
+				duration: transitionDuration,
+			});
+		}
+	}, [keyIllustration]);
+	const gradientColor1 = useDerivedValue(() => {
+		return [currentColor1.value, "transparent"];
+	}, []);
+	const gradientColor2 = useDerivedValue(() => {
+		return [currentColor2.value, "transparent"];
+	}, []);
+	const gradientColor3 = useDerivedValue(() => {
+		return [currentColor3.value, "transparent"];
+	}, []);
+	const gradientColor4 = useDerivedValue(() => {
+		return [currentColor4.value, "transparent"];
+	}, []);
+	const gradientColor5 = useDerivedValue(() => {
+		return [currentColor5.value, "transparent"];
+	}, []);
+	const gradientColors = useMemo(
+		() => [
+			gradientColor1,
+			gradientColor2,
+			gradientColor3,
+			gradientColor4,
+			gradientColor5,
+		],
+		[],
 	);
-	if (sortedColors?.length !== 5) {
-		return null;
-	}
 	return (
-		<LinearGradient
-			// Background Linear Gradient
-			colors={sortedColors as any}
-			style={styles.gradient}
-		/>
+		<Canvas style={styles.canvas}>
+			{generateArray(5, null).map((_, index) => {
+				const cxRatio =
+					index === 0 || index === 3 ? 0.1 : index === 4 ? 0 : 0.9;
+				const cyRatio = index < 2 ? 0.9 : index < 4 ? 0.1 : 1;
+
+				return (
+					<FullScreenRect key={index}>
+						<GradientEllipse
+							uniProps={(_, rt) => ({
+								c: vec(
+									rt.screen.width * cxRatio,
+									rt.screen.height * cyRatio,
+								),
+							})}
+							colors={gradientColors[index]}
+						/>
+					</FullScreenRect>
+				);
+			})}
+		</Canvas>
 	);
 };
 
+const GradientEllipse = withUnistyles(RadialGradient, (_, rt) => {
+	const r = Math.max(rt.screen.width, rt.screen.height) * 0.55;
+	return {
+		r,
+	};
+});
+
+const FullScreenRect = withUnistyles(
+	Rect,
+	(_theme, rt) =>
+		({
+			x: 0,
+			y: 0,
+			width: rt.screen.width,
+			height: rt.screen.height,
+		}) as RectProps,
+);
+
 const styles = StyleSheet.create((theme) => ({
-	gradient: {
+	canvas: {
 		position: "absolute",
 		left: 0,
 		// zIndex: -1, //TODO
 		right: 0,
 		top: 0,
+		flex: 1,
 		backgroundColor: theme.colors.background,
 		opacity: theme.name === "light" ? 0.5 : 0.4,
 		height: "100%",
