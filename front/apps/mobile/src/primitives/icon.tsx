17788fa198eaa368fb559efab925d648c4505a89
Arthur Jamet <arthur.jamet@gmail.com>
2025-07-17 09:42:35 +0100

    Front: Mobile: Icon Primitive: allow setting size

--- front/apps/mobile/src/primitives/icon.tsx
+++ front/apps/mobile/src/primitives/icon.tsx
@@ -1,7 +1,7 @@
 import type { IconProps, Icon as IconType } from "@/ui/icons";
 import { withUnistyles } from "react-native-unistyles";
 
-type IconStyle = { color: string } & IconProps["style"];
+type IconStyle = { color: string; size?: number } & IconProps["style"];
 
 type Props = {
 	icon: IconType;
@@ -11,6 +11,9 @@ type Props = {
 
 export const Icon = ({ icon, style, variant }: Props) => {
 	const Icon = withUnistyles(icon, (theme) => ({
+		size: Array.isArray(style)
+			? style.find(({ size }) => size)?.size
+			: style?.size,
 		color: Array.isArray(style)
 			? style.find(({ color }) => color)?.color
 			: (style?.color ?? theme.colors.text.primary),
