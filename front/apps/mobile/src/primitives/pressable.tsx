089c91a230c571bac6ede8e44f83f9ca0031f4ad
Arthur Jamet <arthur.jamet@gmail.com>
2025-07-19 10:18:00 +0100

    Front: Mobile: Allow disabling pressable

--- front/apps/mobile/src/primitives/pressable.tsx
+++ front/apps/mobile/src/primitives/pressable.tsx
@@ -5,13 +5,15 @@ import { StyleSheet } from "react-native-unistyles";
 type Props = {
 	onPress: () => void;
 	children: React.ReactNode;
+	disabled?: boolean;
 	style?: ComponentProps<typeof TouchableOpacity>["style"];
 };
 
 // Wrapper around TouchableOpacity
-export const Pressable = ({ onPress, children, style }: Props) => {
+export const Pressable = ({ onPress, children, style, disabled }: Props) => {
 	return (
 		<TouchableOpacity
+			disabled={disabled}
 			touchSoundDisabled
 			style={[styles.root, style]}
 			onPress={onPress}
