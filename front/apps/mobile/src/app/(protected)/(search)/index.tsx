e94d62fd66bf55de769370d6f93e59599ec11f85
Arthur Jamet <arthur.jamet@gmail.com>
2025-07-22 09:25:04 +0100

    Front: Mobile: Search page with search history

--- front/apps/mobile/src/app/(protected)/(search)/index.tsx
+++ front/apps/mobile/src/app/(protected)/(search)/index.tsx
@@ -16,13 +16,127 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { SafeView } from "~/components/safe-view";
-import { Text } from "~/primitives/text";
+import { getSearchHistory, searchAll } from "@/api/queries";
+import {
+	type InfiniteQuery,
+	type Query,
+	toInfiniteQuery,
+	transformPage,
+} from "@/api/query";
+import type Illustration from "@/models/illustration";
+import type Resource from "@/models/resource";
+import type { SaveSearchItem, SearchResult } from "@/models/search";
+import { useMutation } from "@tanstack/react-query";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { View } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
+import { useAPI, useQueryClient } from "~/api";
+import { InfiniteView } from "~/components/infinite/view";
+import { SearchResultItem } from "~/components/list-item/resource/search-result";
+import { TextInput } from "~/primitives/text_input";
+
+//TODO Allow filtering by type (artist, album, etc.)
 
 export default function SearchView() {
+	const timeoutRef = useRef<number>(null);
+	const api = useAPI();
+	const queryClient = useQueryClient();
+	const [searchValue, setSearchValue] = useState("");
+	const debounceSearch = useCallback((searchValue: string) => {
+		if (timeoutRef.current) clearTimeout(timeoutRef.current);
+		timeoutRef.current = setTimeout(() => {
+			setSearchValue(searchValue);
+		}, 500);
+	}, []);
+	const query = useMemo(() => {
+		if (searchValue.trim() === "") {
+			return searchResultQueryToResourceQuery(getSearchHistory());
+		}
+		return searchResultQueryToResourceQuery(searchAll(searchValue));
+	}, [searchValue]);
+
+	const saveSearch = useMutation({
+		mutationFn: async (item: SearchResult) => {
+			try {
+				const dto: SaveSearchItem = item.artist
+					? { artistId: item.artist.id }
+					: item.album
+						? { albumId: item.album.id }
+						: item.song
+							? { songId: item.song.id }
+							: { videoId: item.video.id };
+				await api.saveSearchHistoryEntry(dto);
+				// Sometimes, it refreshes to fast, and shifts the history
+				// before openning a page (for artists) is done
+				setTimeout(() => {
+					queryClient.client.invalidateQueries({
+						queryKey: getSearchHistory().key,
+					});
+				}, 500);
+			} catch (error) {
+				// biome-ignore lint/suspicious/noConsole: <explanation>
+				console.error(error);
+			}
+		},
+	});
+
 	return (
-		<SafeView>
-			<Text>Search</Text>
-		</SafeView>
+		<InfiniteView
+			header={
+				<View style={styles.searchHeader}>
+					<TextInput
+						containerStyle={{ backgroundColor: "transparent" }}
+						onChangeText={debounceSearch}
+					/>
+				</View>
+			}
+			// NOTE: Since the original type of the query is SearchResult, it does not have an id,
+			// that's why it complains
+			query={
+				query as unknown as InfiniteQuery<
+					Resource,
+					IllustratedSearchResultWithId
+				>
+			}
+			controls={{}}
+			render={(item) => (
+				<SearchResultItem
+					searchResult={item}
+					onPress={() => item && saveSearch.mutate(item)}
+				/>
+			)}
+			layout="list"
+		/>
 	);
 }
+
+type IllustratedSearchResultWithId = {
+	id: number;
+	illustration: Illustration | null;
+} & SearchResult;
+
+const searchResultQueryToResourceQuery = (query: Query<SearchResult[]>) => {
+	return transformPage(toInfiniteQuery(query), (item) => {
+		return {
+			...item,
+			id: item.song
+				? `song-${item.song.id}`
+				: item.album
+					? `album-${item.album.id}`
+					: item.video
+						? `video-${item.video.id}`
+						: `artist-${item.artist.id}`,
+			illustration: (item.album ?? item.artist ?? item.song ?? item.video)
+				?.illustration,
+		};
+	});
+};
+
+const styles = StyleSheet.create((theme) => ({
+	searchHeader: {
+		paddingTop: theme.gap(2),
+		//To balance with the default top padding from root view style
+		paddingBottom: theme.gap(3),
+		alignItems: "center",
+	},
+}));
---
+++ front/apps/mobile/src/components/list-item/resource/search-result.tsx
@@ -0,0 +1,61 @@
+import type { SearchResult } from "@/models/search";
+import { useTranslation } from "react-i18next";
+import { View } from "react-native";
+import { AlbumItem } from "./album";
+import { ArtistItem } from "./artist";
+import { SongItem } from "./song";
+import { VideoItem } from "./video";
+
+export const SearchResultItem = ({
+	searchResult,
+	onPress,
+}: { searchResult: SearchResult | undefined; onPress: () => void }) => {
+	const { t } = useTranslation();
+	if (!searchResult || searchResult.song) {
+		return (
+			<SongItem
+				song={searchResult?.song}
+				illustrationProps={{ simpleColorPlaceholder: true }}
+				subtitle="artists"
+				onPress={onPress}
+				formatSubtitle={(s) => `${t("models.song")} • ${s}`}
+			/>
+		);
+	}
+	if (searchResult.album) {
+		return (
+			<AlbumItem
+				album={searchResult.album}
+				illustrationProps={{ simpleColorPlaceholder: true }}
+				subtitle="artistName"
+				onPress={onPress}
+				formatSubtitle={(s) => `${t("models.album")} • ${s}`}
+			/>
+		);
+	}
+	if (searchResult.artist) {
+		return (
+			<ArtistItem
+				artist={searchResult.artist}
+				illustrationProps={{ simpleColorPlaceholder: true }}
+				onPress={onPress}
+			/>
+		);
+	}
+	if (searchResult.video) {
+		return (
+			<VideoItem
+				video={searchResult.video}
+				illustrationProps={{ simpleColorPlaceholder: true }}
+				onPress={onPress}
+				subtitle="artistName"
+				formatSubtitle={(s) => `${t("models.video")} • ${s}`}
+			/>
+		);
+	}
+	// biome-ignore lint/suspicious/noConsole: <explanation>
+	console.error("Unexpected search result");
+	// biome-ignore lint/suspicious/noConsole: <explanation>
+	console.error(searchResult);
+	return <View />;
+};
