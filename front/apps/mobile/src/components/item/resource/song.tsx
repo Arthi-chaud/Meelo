514eaa74836f0504384b9e832e337b9b72dfd055
Arthur Jamet <arthur.jamet@gmail.com>
2025-07-30 16:24:35 +0100

    Front: Mobile: Song: remove tmp context menu

--- front/apps/mobile/src/components/item/resource/song.tsx
+++ front/apps/mobile/src/components/item/resource/song.tsx
@@ -1,5 +1,4 @@
 import type { SongWithRelations } from "@/models/song";
-import { PlayNextIcon } from "@/ui/icons";
 import formatArtists from "@/utils/format-artists";
 import { type ComponentProps, useMemo } from "react";
 import { ListItem } from "../list-item";
@@ -38,23 +37,6 @@ export const SongItem = ({
 		<ListItem
 			title={song?.name}
 			subtitle={formattedSubtitle}
-			contextMenu={{
-				header: {
-					illustration: song?.illustration ?? null,
-					title: song?.name,
-					subtitle: song
-						? formatArtists(song.artist, song.featuring)
-						: null,
-				},
-				items: [
-					[
-						{
-							label: "actions.playback.playNext",
-							icon: PlayNextIcon,
-						},
-					],
-				],
-			}}
 			onPress={() => {
 				onPress?.();
 			}} // TODO Launch playback
