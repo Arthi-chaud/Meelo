17408db2c447f1d913f55cee394d737755c8701d
Arthur Jamet <arthur.jamet@gmail.com>
2025-08-13 16:55:48 +0100

    Front: Mobile: Chip: Fix color and add icon prop

--- front/apps/mobile/src/components/chip.tsx
+++ front/apps/mobile/src/components/chip.tsx
@@ -1,6 +1,8 @@
 import { type Href, useRouter } from "expo-router";
 import { useCallback } from "react";
 import { StyleSheet } from "react-native-unistyles";
+import type { Icon as IconType } from "@/ui/icons";
+import { Icon } from "~/primitives/icon";
 import { Pressable } from "~/primitives/pressable";
 import { LoadableText } from "./loadable_text";
 
@@ -8,10 +10,11 @@ type Props = {
 	title: string | undefined;
 	onPress?: () => void;
 	href?: Href;
+	icon?: IconType;
 	filled?: boolean;
 };
 
-export const Chip = ({ title, onPress, href, filled }: Props) => {
+export const Chip = ({ title, onPress, href, filled, icon }: Props) => {
 	styles.useVariants({ filled: filled ?? false });
 	const router = useRouter();
 	const pressCallback = useCallback(() => {
@@ -23,6 +26,7 @@ export const Chip = ({ title, onPress, href, filled }: Props) => {
 
 	return (
 		<Pressable onPress={pressCallback} style={styles.chip}>
+			{icon && <Icon icon={icon} style={styles.chipIcon} />}
 			<LoadableText
 				content={title}
 				skeletonWidth={5}
@@ -36,20 +40,34 @@ export const Chip = ({ title, onPress, href, filled }: Props) => {
 const styles = StyleSheet.create((theme) => ({
 	chip: {
 		borderRadius: theme.borderRadius * 3,
+		flexDirection: "row",
+		gap: theme.gap(1),
 		justifyContent: "center",
+		alignItems: "center",
 		variants: {
 			filled: {
 				true: {
-					backgroundColor: theme.colors.text.secondary,
+					backgroundColor: theme.colors.text.primary,
 				},
 				false: {
 					borderWidth: 1,
-					borderColor: theme.colors.text.secondary,
+					borderColor: theme.colors.text.primary,
 				},
 			},
 		},
 		paddingVertical: theme.gap(0.5),
 		paddingHorizontal: theme.gap(1.5),
+	},
+	chipIcon: {
+		size: theme.fontSize.rem(1),
+		variants: {
+			filled: {
+				true: {
+					color: theme.colors.text.onAccentSurface,
+				},
+				false: {},
+			},
+		},
 	},
 	chipText: {
 		fontSize: theme.fontSize.rem(0.8),
