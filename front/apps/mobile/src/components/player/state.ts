5a49d7bf677ec6adfcb296ba1b8989fc2bc046aa
Arthur Jamet <arthur.jamet@gmail.com>
2025-08-16 17:01:36 +0100

    Front: Mobile: setup player context

--- front/apps/mobile/src/components/player/state.ts
+++ front/apps/mobile/src/components/player/state.ts
@@ -4,3 +4,13 @@ import { cursorAtom, playlistAtom, type TrackState } from "@/state/player";
 export const currentTrackAtom = atom(
 	(get): TrackState | null => get(playlistAtom)[get(cursorAtom)],
 );
+
+export const playAtom = atom(null, (_, set) => set(_isPlaying, true));
+export const pauseAtom = atom(null, (_, set) => set(_isPlaying, false));
+export const isPlayingAtom = atom((get) => get(_isPlaying));
+// Duration is in seconds
+export const durationAtom = atom<null | number>(null);
+// Progress is in seconds
+export const progressAtom = atom(0);
+
+const _isPlaying = atom(false);
