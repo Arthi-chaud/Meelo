ed1937c4009eeefc9a24c9cd15c36fc79ea2d8e9
Arthur Jamet <arthur.jamet@gmail.com>
2025-08-31 12:06:29 +0100

    Front: Mobile: 'create playlist' action + add action to /playlists

--- front/apps/mobile/src/app/(protected)/(home,browse,search)/playlists/index.tsx
+++ front/apps/mobile/src/app/(protected)/(home,browse,search)/playlists/index.tsx
@@ -1,21 +1,25 @@
 import { getPlaylists } from "@/api/queries";
 import { PlaylistSortingKeys } from "@/models/playlist";
+import {
+	CreatePlaylist,
+	useCreatePlaylistFormModal,
+} from "~/actions/playlist/create-update";
 import { useSortControl } from "~/components/infinite/controls/sort";
 import { InfiniteView } from "~/components/infinite/view";
 import { PlaylistItem } from "~/components/item/resource/playlist";
-
-//TODO Action to create playlist
 
 export default function PlaylistsView() {
 	const [{ sort, order }, sortControl] = useSortControl({
 		sortingKeys: PlaylistSortingKeys,
 		translate: (s) => `browsing.controls.sort.${s}`,
 	});
+	const { openFormModal } = useCreatePlaylistFormModal();
 
 	return (
 		<InfiniteView
 			layout={"list"}
 			controls={{
+				actions: [CreatePlaylist(openFormModal)],
 				sort: sortControl,
 			}}
 			query={getPlaylists(
