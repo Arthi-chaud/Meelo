dee5b9e956cbf5bfb079344462d36b5482e5369f
Arthur Jamet <arthur.jamet@gmail.com>
2025-09-01 18:13:03 +0100

    Front: Mobile: Playlists View: add layout toggle

--- front/apps/mobile/src/app/(protected)/(home,browse,search)/playlists/index.tsx
+++ front/apps/mobile/src/app/(protected)/(home,browse,search)/playlists/index.tsx
@@ -4,30 +4,40 @@ import {
 	CreatePlaylist,
 	useCreatePlaylistFormModal,
 } from "~/actions/playlist/create-update";
+import { useLayoutControl } from "~/components/infinite/controls/layout";
 import { useSortControl } from "~/components/infinite/controls/sort";
 import { InfiniteView } from "~/components/infinite/view";
-import { PlaylistItem } from "~/components/item/resource/playlist";
+import {
+	PlaylistItem,
+	PlaylistTile,
+} from "~/components/item/resource/playlist";
 
 export default function PlaylistsView() {
 	const [{ sort, order }, sortControl] = useSortControl({
 		sortingKeys: PlaylistSortingKeys,
 		translate: (s) => `browsing.controls.sort.${s}`,
 	});
+	const [{ layout }, layoutControl] = useLayoutControl({
+		defaultLayout: "list",
+		enableToggle: true,
+	});
 	const { openFormModal } = useCreatePlaylistFormModal();
+	const Item = layout === "list" ? PlaylistItem : PlaylistTile;
 
 	return (
 		<InfiniteView
-			layout={"list"}
+			layout={layout}
 			controls={{
 				actions: [CreatePlaylist(openFormModal)],
 				sort: sortControl,
+				layout: layoutControl,
 			}}
 			query={getPlaylists(
 				{},
 				{ sortBy: sort ?? "name", order: order ?? "asc" },
 				["illustration"],
 			)}
-			render={(playlist) => <PlaylistItem playlist={playlist} />}
+			render={(playlist) => <Item playlist={playlist} />}
 		/>
 	);
 }
