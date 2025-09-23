51c6ca8712d84cdea65a88f1da47eeb49415e892
Arthur Jamet <arthur.jamet@gmail.com>
2025-09-23 08:55:25 +0200

    Matcher: Setup FastAPI + use async for Rabbitmq client

--- matcher/matcher/__init__.py
+++ matcher/matcher/__init__.py
@@ -1,10 +1,12 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 import pika
 import os
-import sys
 import logging
-import signal
-
+from pika.adapters.asyncio_connection import AsyncioConnection
 from pika.adapters.blocking_connection import BlockingChannel
+from pika.channel import Channel
+from matcher import router
 from matcher.bootstrap import bootstrap_context
 from matcher.context import Context
 from matcher.matcher.album import match_and_post_album
@@ -17,7 +19,7 @@ channel: BlockingChannel | None = None
 queue_name = "meelo"
 
 
-def consume(ch: BlockingChannel, method, prop, body):
+def consume(ch: Channel, method, prop, body):
     event = Event.from_json(body)
     delivery_tag = method.delivery_tag
     logging.info(f"Received event: {event} (P={prop.priority})")
@@ -39,36 +41,41 @@ def consume(ch: BlockingChannel, method, prop, body):
             pass
 
 
-def main():
-    global channel
+def on_channel_open(channel: Channel):
+    channel.queue_declare(
+        queue=queue_name, durable=True, arguments={"x-max-priority": 5}
+    )
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue_name, on_message_callback=consume)
+    bootstrap_context()
+    logging.info(f"Version: {Context.get().settings.version}")
+    logging.info("Ready to match!")
+
+
+@asynccontextmanager
+async def lifespan(_):
     logging.basicConfig(level=logging.INFO)
     rabbitUrl = os.environ.get("RABBITMQ_URL")
     if not rabbitUrl:
         logging.error("Missing env var 'RABBITMQ_URL'")
         exit(1)
     connectionParams = pika.URLParameters(rabbitUrl)
-    connection = pika.BlockingConnection(connectionParams)
-    channel = connection.channel()
-    channel.queue_declare(
-        queue=queue_name, durable=True, arguments={"x-max-priority": 5}
+    AsyncioConnection(
+        connectionParams,
+        on_open_callback=lambda conn: conn.channel(on_open_callback=on_channel_open),
     )
-    bootstrap_context()
-
-    logging.info(f"Version: {Context.get().settings.version}")
-    logging.info("Ready to match!")
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue_name, on_message_callback=consume)
-    channel.start_consuming()
-
-
-def terminate(signal, term):
+    yield
     logging.info("Shutting Down Matcher...")
     if channel:
         channel.close()
-    sys.exit(0)
 
 
-# From https://www.rabbitmq.com/tutorials/tutorial-one-python
-if __name__ == "__main__":
-    signal.signal(signal.SIGTERM, terminate)
-    main()
+app = FastAPI(
+    title="Meelo's Matcher Matcher APU",
+    description="The matcher is in charge of downloading external metadata (lyrics, images, genres) from providers (e.g. Genius, Wikipedia, etc.)",
+    openapi_tags=[{"name": "Endpoints"}],
+    swagger_ui_parameters={"syntaxHighlight": True},
+    lifespan=lifespan,
+    docs_url="/swagger",
+)
+app.include_router(router.router)
