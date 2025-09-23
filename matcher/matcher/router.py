74f29a36c28fedbf6c1a302e0ba379a472878c7c
Arthur Jamet <arthur.jamet@gmail.com>
2025-09-23 09:10:29 +0200

    Matcher: Index endpoint return build version

--- matcher/matcher/router.py
+++ matcher/matcher/router.py
@@ -1,8 +1,16 @@
+import os
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
 
 
+class StatusResponse(BaseModel):
+    message: str
+    version: str
+
+
 @router.get("/", tags=["Endpoints"])
-def hello_world():
-    return '{"message": "Hello World"}'
+async def status() -> StatusResponse:
+    buildVersion = os.environ["VERSION"] or "unknown"
+    return StatusResponse(message="Matcher is alive.", version=buildVersion)
