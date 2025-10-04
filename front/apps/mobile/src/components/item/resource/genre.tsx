9dc59280d406672247a3723fc79f69b2ba299a0b
Arthur Jamet <arthur.jamet@gmail.com>
2025-10-04 11:04:16 +0100

    Mobile: Genre Context Menu

--- front/apps/mobile/src/components/item/resource/genre.tsx
+++ front/apps/mobile/src/components/item/resource/genre.tsx
@@ -1,16 +1,20 @@
 import type Genre from "@/models/genre";
-import { GenreIcon } from "@/ui/icons";
+import { useContextMenu } from "~/components/context-menu";
+import { useGenreContextMenu } from "~/components/context-menu/resource/genre";
 import { ListItem } from "../list-item";
 
 type Props = { genre: Genre | undefined };
 
 export const GenreItem = ({ genre }: Props) => {
+	const ctxMenu = useGenreContextMenu(genre);
+	const { openContextMenu } = useContextMenu(ctxMenu);
 	return (
 		<ListItem
 			noIllustration
 			title={genre?.name}
 			subtitle={null}
-			onPress={() => {}} // TODO
+			onPress={openContextMenu}
+			contextMenu={ctxMenu}
 		/>
 	);
 };
