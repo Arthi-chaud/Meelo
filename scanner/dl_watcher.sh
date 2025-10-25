6ee3262064a823184246d824bdf2cdac85546ad9
Arthur Jamet <arthur.jamet@gmail.com>
2025-10-25 12:01:47 +0100

    Scanner: Install C fs watcher

---
+++ scanner/dl_watcher.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+REPO=https://github.com/e-dant/watcher
+TAG=0.13.8
+URL="$REPO/archive/refs/tags/$TAG.zip"
+TAR_OUT_DIR=/tmp/watcher-$TAG
+DEST=internal/watcher
+
+rm -rf "$DEST"/*
+
+curl -L "$URL" | tar xz 
+mv watcher-$TAG $TAR_OUT_DIR
+cp  $TAR_OUT_DIR/watcher-c/src/*.cpp "$DEST"
+mkdir "$DEST"/wtr
+cp $TAR_OUT_DIR/watcher-c/include/wtr/* $TAR_OUT_DIR/include/wtr/* "$DEST"/wtr 
+
+rm -rf /tmp/watcher.zip /tmp/watcher
