a1bb399c5b9e848a261096b96d9e859962c2faba
Arthur Jamet <arthur.jamet@gmail.com>
2025-11-05 09:36:42 +0000

    Scanner: Fix DL script, setup env in CI and docker

--- scanner/dl_watcher.sh
+++ scanner/dl_watcher.sh
@@ -2,16 +2,15 @@
 
 REPO=https://github.com/e-dant/watcher
 TAG=0.13.8
-URL="$REPO/archive/refs/tags/$TAG.zip"
-TAR_OUT_DIR=/tmp/watcher-$TAG
+URL="$REPO/archive/refs/tags/$TAG.tar.gz"
+TAR_OUT_DIR=./watcher-$TAG
 DEST=app/
 
-rm -rf "$DEST"/*
+rm -rf "$DEST"/wtr "$TAR_OUT_DIR"
 
 curl -L "$URL" | tar xz 
-mv watcher-$TAG $TAR_OUT_DIR
 cp  $TAR_OUT_DIR/watcher-c/src/*.cpp "$DEST"
 mkdir "$DEST"/wtr
 cp $TAR_OUT_DIR/watcher-c/include/wtr/* $TAR_OUT_DIR/include/wtr/* "$DEST"/wtr 
 
-rm -rf /tmp/watcher.zip /tmp/watcher
+rm -rf "$TAR_OUT_DIR"
