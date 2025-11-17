fbff69e2cb88e19ca20c5abc5656b88b955ac10e
Arthur Jamet <arthur.jamet@gmail.com>
2025-11-17 10:18:37 +0000

    Mobile: Select server from auth page

--- front/apps/mobile/src/app/auth.tsx
+++ front/apps/mobile/src/app/auth.tsx
@@ -17,18 +17,27 @@
  */
 
 import { useRouter } from "expo-router";
-import { useSetAtom } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
+import { Fragment, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native-unistyles";
+import { Toast } from "toastify-react-native";
 import { getCurrentUserStatus } from "@/api/queries";
-import { AddIcon } from "@/ui/icons";
+import { AddIcon, MoreIcon } from "@/ui/icons";
 import { getAPI_ } from "~/api";
+import { InstanceButton } from "~/components/instance-button";
 import { useLoginForm } from "~/components/login-form";
 import { Banner } from "~/components/meelo";
 import { Button } from "~/primitives/button";
-import { currentInstanceAtom } from "~/state/user";
+import { Divider } from "~/primitives/divider";
+import {
+	currentInstanceAtom,
+	deleteOtherInstanceAtom,
+	type MeeloInstance,
+	otherInstancesAtom,
+} from "~/state/user";
 
 const styles = StyleSheet.create((theme, rt) => ({
 	root: {
@@ -55,13 +64,22 @@ const styles = StyleSheet.create((theme, rt) => ({
 		alignItems: "center",
 		gap: theme.gap(3),
 	},
+	instanceButtons: {
+		flexDirection: "column",
+		gap: theme.gap(1),
+		width: "100%",
+		justifyContent: "center",
+		paddingHorizontal: theme.gap(3),
+	},
 }));
 
 export default function AuthenticationScreen() {
 	const safeAreaStyle = useSafeAreaInsets();
 	const { t } = useTranslation();
 	const router = useRouter();
+	const otherInstances = useAtomValue(otherInstancesAtom);
 	const setCurrentInstance = useSetAtom(currentInstanceAtom);
+	const deleteOtherInstance = useSetAtom(deleteOtherInstanceAtom);
 	const { openLoginForm } = useLoginForm({
 		onLogin: async ({ instanceUrl, token }) => {
 			const api = getAPI_(token, instanceUrl);
@@ -74,9 +92,52 @@ export default function AuthenticationScreen() {
 			router.replace("/");
 		},
 	});
+	const signInToOtherInstance = useCallback(
+		(instance: MeeloInstance) => {
+			const api = getAPI_(instance.accessToken, instance.url);
+
+			getCurrentUserStatus()
+				.exec(api)()
+				.then(() => {
+					setCurrentInstance(instance);
+					deleteOtherInstance(instance);
+					router.replace("/");
+				})
+				.catch((e) => {
+					Toast.error(
+						`${t("toasts.auth.signinFailed")}: ${e.message ?? e.error ?? JSON.stringify(e)}`,
+					);
+				});
+		},
+		[setCurrentInstance, router],
+	);
+	const signInAction = useCallback(
+		(instance: MeeloInstance) => {
+			return {
+				onPress: () => signInToOtherInstance(instance),
+				icon: MoreIcon,
+			};
+		},
+		[signInToOtherInstance],
+	);
 	return (
 		<View style={[styles.root, safeAreaStyle]}>
 			<Banner style={styles.banner} />
+			{otherInstances.length && (
+				<View style={styles.instanceButtons}>
+					{otherInstances.map((instance, idx) => (
+						<Fragment key={idx}>
+							<InstanceButton
+								key={idx}
+								instance={instance}
+								enabled
+								actions={[signInAction(instance)]}
+							/>
+							<Divider h />
+						</Fragment>
+					))}
+				</View>
+			)}
 			<View style={styles.formContainer}>
 				<Button
 					title={t("actions.connectToNewServer")}
@@ -84,7 +145,7 @@ export default function AuthenticationScreen() {
 					onPress={openLoginForm}
 				/>
 			</View>
-			<View />
+			{!otherInstances.length && <View />}
 			{/* to have a 1/3 page-height footer */}
 		</View>
 	);
