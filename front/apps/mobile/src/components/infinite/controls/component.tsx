e72dedd1e27ddd74c80e72521e98b34b0ac0a178
Arthur Jamet <arthur.jamet@gmail.com>
2025-11-26 10:30:07 +0000

    Mobile: Infinite view controls: Group actions into nowrap row

--- front/apps/mobile/src/components/infinite/controls/component.tsx
+++ front/apps/mobile/src/components/infinite/controls/component.tsx
@@ -31,111 +31,125 @@ export const Controls = <S extends string>({
 		}),
 	);
 	return (
-		<View style={styles.root}>
-			{filters?.map((filter, idx) => (
-				<SelectModalButton
-					header={t("browsing.controls.filter.header")}
-					key={idx}
-					closeOnSelect
-					buttonProps={{
-						title: t(filter.buttonLabel),
-						icon: filter.buttonIcon as any,
-						width: "fitContent",
-						size: "small",
-						iconPosition: "right",
-					}}
-					values={filter.values ?? []}
-					isSelected={(item) =>
-						filter.multipleChoice
-							? filter.selected.includes(item)
-							: filter.selected === item
-					}
-					formatItem={(item) => t(filter.formatItem(item))}
-					onSelect={(selected) => {
-						if (filter.multipleChoice) {
-							const selectedKeys = filter.selected.includes(
-								selected,
-							)
-								? filter.selected.filter((k) => k !== selected)
-								: [selected, ...filter.selected];
-							filter.onUpdate(selectedKeys);
-						} else {
-							filter.onUpdate(
-								filter.selected === selected ? null : selected,
-							);
+		<View style={styles.row}>
+			<View style={styles.row}>
+				{filters?.map((filter, idx) => (
+					<SelectModalButton
+						header={t("browsing.controls.filter.header")}
+						key={idx}
+						closeOnSelect
+						buttonProps={{
+							title: t(filter.buttonLabel),
+							icon: filter.buttonIcon as any,
+							width: "fitContent",
+							size: "small",
+							iconPosition: "right",
+						}}
+						values={filter.values ?? []}
+						isSelected={(item) =>
+							filter.multipleChoice
+								? filter.selected.includes(item)
+								: filter.selected === item
 						}
-					}}
-				/>
-			))}
-			{sort && (
-				<SelectModalButton
-					header={t("browsing.controls.sortBy")}
-					closeOnSelect
-					values={sort.sortingKeys}
-					formatItem={(item) => t(sort.formatItem(item))}
-					buttonProps={{
-						title: t(sort.formatItem(sort.selected.sort)),
-						width: "fitContent",
-						size: "small",
-						icon:
-							sort.selected.order === "asc" ? AscIcon : DescIcon,
-						iconPosition: "left",
-					}}
-					isSelected={(item) => sort.selected.sort === item}
-					checkIcon={OrderIcon}
-					onSelect={(selected: S) => {
-						if (selected === sort.selected.sort) {
-							sort.onUpdate({
-								sort: selected,
-								order:
-									sort.selected.order === "asc"
-										? "desc"
-										: "asc",
+						formatItem={(item) => t(filter.formatItem(item))}
+						onSelect={(selected) => {
+							if (filter.multipleChoice) {
+								const selectedKeys = filter.selected.includes(
+									selected,
+								)
+									? filter.selected.filter(
+											(k) => k !== selected,
+										)
+									: [selected, ...filter.selected];
+								filter.onUpdate(selectedKeys);
+							} else {
+								filter.onUpdate(
+									filter.selected === selected
+										? null
+										: selected,
+								);
+							}
+						}}
+					/>
+				))}
+				{sort && (
+					<SelectModalButton
+						header={t("browsing.controls.sortBy")}
+						closeOnSelect
+						values={sort.sortingKeys}
+						formatItem={(item) => t(sort.formatItem(item))}
+						buttonProps={{
+							title: t(sort.formatItem(sort.selected.sort)),
+							width: "fitContent",
+							size: "small",
+							icon:
+								sort.selected.order === "asc"
+									? AscIcon
+									: DescIcon,
+							iconPosition: "left",
+						}}
+						isSelected={(item) => sort.selected.sort === item}
+						checkIcon={OrderIcon}
+						onSelect={(selected: S) => {
+							if (selected === sort.selected.sort) {
+								sort.onUpdate({
+									sort: selected,
+									order:
+										sort.selected.order === "asc"
+											? "desc"
+											: "asc",
+								});
+							} else {
+								sort.onUpdate({ sort: selected, order: "asc" });
+							}
+						}}
+					/>
+				)}
+				{layout && (
+					<Button
+						size="small"
+						width="fitContent"
+						icon={layout.layout === "list" ? GridIcon : ListIcon}
+						containerStyle={styles.button}
+						onPress={() => {
+							layout.onUpdate({
+								layout:
+									layout.layout === "list" ? "grid" : "list",
+								itemSize: layout.itemSize,
 							});
-						} else {
-							sort.onUpdate({ sort: selected, order: "asc" });
-						}
-					}}
-				/>
-			)}
-			{layout && (
-				<Button
-					size="small"
-					width="fitContent"
-					icon={layout.layout === "list" ? GridIcon : ListIcon}
-					containerStyle={styles.button}
-					onPress={() => {
-						layout.onUpdate({
-							layout: layout.layout === "list" ? "grid" : "list",
-							itemSize: layout.itemSize,
-						});
-					}}
-				/>
-			)}
-			{actions?.map((action, idx) => (
-				<Button
-					size="small"
-					width="fitContent"
-					key={idx}
-					icon={action.icon}
-					title={t(action.label)}
-					onPress={() => {
-						action.onPress?.();
-					}}
-				/>
-			))}
+						}}
+					/>
+				)}
+			</View>
+			<View style={[styles.row, styles.unbreakableRow]}>
+				{actions?.map((action, idx) => (
+					<Button
+						size="small"
+						width="fitContent"
+						key={idx}
+						icon={action.icon}
+						title={t(action.label)}
+						onPress={() => {
+							action.onPress?.();
+						}}
+					/>
+				))}
+			</View>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create((theme) => ({
-	root: {
+	row: {
 		display: "flex",
 		flexDirection: "row",
-		flexWrap: "wrap",
 		alignItems: "center",
 		justifyContent: "center",
+		flexWrap: "wrap",
 		gap: theme.gap(1),
+	},
+	unbreakableRow: {
+		flexWrap: "nowrap",
 	},
 	button: { elevation: 10 },
 }));
