afb71c81eb6115f8d3d9fdb05ba5f82af8c7edad
Arthur Jamet <arthur.jamet@gmail.com>
2025-12-18 21:09:54 +0000

    Web: Remove unused ts-expect-error

--- front/apps/web/src/components/blurhash.tsx
+++ front/apps/web/src/components/blurhash.tsx
@@ -52,7 +52,6 @@ const Blurhash = ({ blurhash, ...props }: BlurhashProps) => {
 	return (
 		<Box suppressHydrationWarning {...containerProps}>
 			{blurhash && (
-				// @ts-expect-error
 				<RBlurhash
 					hash={blurhash}
 					style={{ width: "100%", height: "100%" }}
