7a795e0f09b688095469f95fea796bdde7080a23
Arthur Jamet <arthur.jamet@gmail.com>
2026-02-03 18:14:24 +0000

    Mobile: Validate cache object at startup

--- front/apps/mobile/src/state/download.ts
+++ front/apps/mobile/src/state/download.ts
@@ -1,22 +1,34 @@
 import { atom } from "jotai";
+import * as yup from "yup";
 import { storage } from "~/utils/storage";
 
-export type Downloads = { downloadedFiles: DownloadedFile[] };
-export type DownloadedFile = {
-	fileId: number;
-	downloadDate: Date;
-	instanceUrl: string;
-	localPath: string;
-};
+const DownloadedFile = yup.object({
+	fileId: yup.number().required(),
+	downloadDate: yup.date().required(),
+	instanceUrl: yup.string().required(),
+	localPath: yup.string().required(),
+});
+const Downloads = yup.object({
+	downloadedFiles: yup.array(DownloadedFile.required()).required(),
+});
+
+export type Downloads = yup.InferType<typeof Downloads>;
+export type DownloadedFile = yup.InferType<typeof DownloadedFile>;
 
 const DownloadsKey = "cache";
 
 const getPersistedDownloads = (): Downloads => {
+	const stringDls = storage.getString(DownloadsKey);
+	if (!stringDls) {
+		return { downloadedFiles: [] };
+	}
 	try {
-		//TODO Validate
-		//TODO Check that all the files are still available
-		return JSON.parse(storage.getString(DownloadsKey)!);
-	} catch {
+		const dls = Downloads.validateSync(JSON.parse(stringDls));
+
+		return dls;
+	} catch (e) {
+		// biome-ignore lint/suspicious/noConsole: For debug
+		console.warn(`An error occured when hydrating cache: ${e}`);
 		return { downloadedFiles: [] };
 	}
 };
