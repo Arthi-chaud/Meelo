abb1cf0eff7fb1701cbccfd44b3067465585251e
Arthur Jamet <60505370+Arthi-chaud@users.noreply.github.com>
2026-04-02 08:42:50 +0100

    Matcher: Setup venv using nix (#1629)

--- matcher/shell.nix
+++ matcher/shell.nix
@@ -1,10 +1,28 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.mkShell {
-  packages = with pkgs; [
+pkgs.mkShell rec {
+  venvDir = "./.venv";
+  buildInputs = with pkgs; [
     python314
+    python314Packages.venvShellHook
     ruff
     pyright
   ];
+  # SRC: https://nixos.org/manual/nixpkgs/stable/#python
+  shellHook = ''
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    if [ ! -d "${venvDir}" ]; then
+      echo "Creating new venv environment in path: '${venvDir}'"
+      ${pkgs.python3Packages.python.interpreter} -m venv "${venvDir}"
+    fi
+
+    PYTHONPATH=$PWD/${venvDir}/${pkgs.python3Packages.python.sitePackages}/:$PYTHONPATH
+    source "${venvDir}/bin/activate"
+    pip install -r requirements.txt -qqq # Hiding verbose logs
+  '';
+  postShellHook = ''
+    unset SOURCE_DATE_EPOCH
+  '';
 }
