1d6f6a9deadebb356977aba8fae6ec75e667f086
Arthur Jamet <arthur.jamet@gmail.com>
2026-06-29 08:03:03 +0200

    Mobile Fix tab resest when switching instance

--- front/apps/mobile/src/pages/settings/hooks.ts
+++ front/apps/mobile/src/pages/settings/hooks.ts
@@ -3,23 +3,23 @@ import { useSetAtom } from "jotai";
 import { useCallback } from "react";
 import { emptyPlaylistAtom } from "@/state/player";
 import { useQueryClient } from "~/api";
-
 export const useResetAllTabs = () => {
 	const navigation = useNavigation();
 
 	return useCallback(() => {
-		const st = navigation.getState();
+		const st = navigation.getParent()?.getState();
 		if (!st) {
 			return;
 		}
-		navigation.reset({
-			...st,
-			history: [],
-			routes: st.routes.map((r) => ({
-				...r,
-				state: undefined,
-			})),
-		} as any);
+
+		st.routes.forEach((route: any) => {
+			// https://github.com/expo/expo/blob/dfc7d35b20fc8829d42a49d6da2adb8e8f03b790/packages/expo-router/src/react-navigation/bottom-tabs/views/BottomTabView.tsx#L106
+			// NOTE: It will issue a warning in dev, but it works so shrug
+			navigation.dispatch({
+				type: "POP_TO_TOP",
+				target: route.state?.key ?? route.key,
+			});
+		});
 	}, [navigation]);
 };
 
