618ba26c9b59e187faeb6ce4fef26aca6cc5ccd6
Arthur Jamet <arthur.jamet@gmail.com>
2026-07-13 14:54:43 +0100

    Mobile: Handle Label search result

--- front/apps/mobile/src/components/item/resource/label.tsx
+++ front/apps/mobile/src/components/item/resource/label.tsx
@@ -1,20 +1,40 @@
-import type { LabelWithRelations } from "@/models/label";
+import type { Area } from "@/models/area";
+import type Label from "@/models/label";
+import { LabelIcon } from "@/ui/icons";
 import { Chip } from "~/components/chip";
 import { useContextMenu } from "~/components/context-menu";
 import { useLabelContextMenu } from "~/components/context-menu/resource/label";
 import { ListItem } from "../list-item";
 
-type Props = { label: LabelWithRelations<"area"> | undefined };
+type Props = {
+	label: (Label & { area?: Area | null }) | undefined;
+	withLeadingIcon?: boolean;
+	subtitle?: string;
+	onPress?: () => void;
+};
 
-export const LabelItem = ({ label }: Props) => {
+export const LabelItem = ({
+	label,
+	subtitle,
+	withLeadingIcon,
+	onPress,
+}: Props) => {
 	const ctxMenu = useLabelContextMenu(label);
 	const { openContextMenu } = useContextMenu(ctxMenu);
 	return (
 		<ListItem
-			leading={null}
+			{...(withLeadingIcon
+				? {
+						illustration: null,
+						illustrationProps: { fallbackIcon: LabelIcon },
+					}
+				: { leading: null })}
 			title={label?.name}
-			subtitle={null}
-			onPress={openContextMenu}
+			subtitle={subtitle ?? null}
+			onPress={() => {
+				onPress?.();
+				openContextMenu();
+			}}
 			contextMenu={ctxMenu}
 		/>
 	);
