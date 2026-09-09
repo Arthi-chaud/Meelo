ff88d863e6dc4218f60c3dbba1220257d1566d17
Arthur Jamet <arthur.jamet@gmail.com>
2026-09-09 12:09:31 +0100

    Server: Update tsconfig for nestjs 12

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 	oxc: false,
 	test: {
 		globals: true,
-		root: "./",
+		root: "./src",
 		watch: false,
 		testTimeout: 60000,
 		env: loadEnv("", process.cwd(), ""),
