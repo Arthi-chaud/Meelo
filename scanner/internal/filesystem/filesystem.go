620ebeb04115b515c93985c9778b2142a77bd636
Arthur Jamet <arthur.jamet@gmail.com>
2026-09-11 10:57:39 +0100

    Scanner: Do not abort scan when getting subdirectory fails

--- scanner/internal/filesystem/filesystem.go
+++ scanner/internal/filesystem/filesystem.go
@@ -3,12 +3,19 @@ package filesystem
 import (
 	"os"
 	"path"
+
+	"github.com/rs/zerolog/log"
 )
 
 const IgnoreFileName = ".ignore"
 
 // Walks through the file system from the given root
 // Does not return content of directories that contain a '.ignore' file
+//
+// It ignores hidden files/directories (unix)
+//
+// It only fails if the root dir cannot be opened.
+// Stats/ReadDir failures on children will be logged.
 func GetAllFilesInDirectory(dir string) ([]string, error) {
 	files := []string{}
 	entries, err := os.ReadDir(dir)
@@ -17,19 +24,31 @@ func GetAllFilesInDirectory(dir string) ([]string, error) {
 		return []string{}, err
 	}
 
-	for _, e := range entries {
-		if e.Name() == IgnoreFileName {
+	for _, entry := range entries {
+		entryName := entry.Name()
+		if entryName == IgnoreFileName {
 			return []string{}, nil
 		}
-		entryPath := path.Join(dir, e.Name())
+		if len(entryName) > 1 && entryName[0] == '.' {
+			continue
+		}
+		entryPath := path.Join(dir, entryName)
 		i, err := os.Stat(entryPath)
 		if err != nil {
-			return []string{}, err
+			log.Warn().
+				Str("file", path.Base(entryPath)).
+				Msg("Could not run 'stat' on file or directory")
+			log.Trace().Msg(err.Error())
+			continue
 		}
 		if i.IsDir() {
 			filesInDir, err := GetAllFilesInDirectory(entryPath)
 			if err != nil {
-				return []string{}, err
+				log.Warn().
+					Str("file", path.Base(entryPath)).
+					Msg("Failed to get files in directory")
+				log.Trace().Msg(err.Error())
+				continue
 			}
 			files = append(files, filesInDir...)
 		} else {
